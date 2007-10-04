5595a996234cbe610f9681dbe8ed26880decd94a
Aaron Brashears <aaronb@lindenlab.com>
2007-10-04 23:19:43 +0000

    Result of svn merge -r71162:71205 svn+ssh://svn/svn/linden/branches/new-license into release. only changes files which are not deployed or the comments section of code.

--- indra/lib/python/indra/__init__.py
+++ indra/lib/python/indra/__init__.py
@@ -2,6 +2,19 @@
 @file __init__.py
 @brief Initialization file for the indra module.
 
+$LicenseInfo:firstyear=2006&license=internal$
+
 Copyright (c) 2006-2007, Linden Research, Inc.
-$License$
+
+The following source code is PROPRIETARY AND CONFIDENTIAL. Use of
+this source code is governed by the Linden Lab Source Code Disclosure
+Agreement ("Agreement") previously entered between you and Linden
+Lab. By accessing, using, copying, modifying or distributing this
+software, you acknowledge that you have been informed of your
+obligations under the Agreement and agree to abide by those obligations.
+
+ALL LINDEN LAB SOURCE CODE IS PROVIDED "AS IS." LINDEN LAB MAKES NO
+WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
+COMPLETENESS OR PERFORMANCE.
+$/LicenseInfo$
 """
