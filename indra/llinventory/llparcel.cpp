ef0eb8dbcf3980a057ad0864efa1c8e1575a8428
Josh Bell <josh@lindenlab.com>
2007-11-01 22:23:39 +0000

    svn merge -r 72356:72879 svn+ssh://svn.lindenlab.com/svn/linden/branches/Branch_1-18-3-Server --> release
    Backport fixes from 1.18.3 Server

--- indra/llinventory/llparcel.cpp
+++ indra/llinventory/llparcel.cpp
@@ -1616,7 +1616,7 @@ void LLParcel::startSale(const LLUUID& buyer_id, BOOL is_buyer_group)
 	mSaleTimerExpires.setTimerExpirySec(DEFAULT_USEC_SALE_TIMEOUT / SEC_TO_MICROSEC);
 	mStatus = OS_LEASE_PENDING;
 	mClaimDate = time(NULL);
-	mAuctionID = 0;
+	setAuctionID(0);
 	// clear the autoreturn whenever land changes hands
 	setCleanOtherTime(0);
 }
@@ -1651,6 +1651,7 @@ void LLParcel::completeSale(U32& type, U8& flags,
 	// Purchased parcels are assumed to no longer be for sale.
 	// Otherwise someone can snipe the sale.
 	setForSale(FALSE);
+	setAuctionID(0);
 
 	// Turn off show directory, since it's a recurring fee that
 	// the buyer may not want.
@@ -1676,6 +1677,7 @@ void LLParcel::clearSale()
 	}
 	mAuthBuyerID.setNull();
 	setForSale(FALSE);
+	setAuctionID(0);
 	setPreviousOwnerID(LLUUID::null);
 	setPreviouslyGroupOwned(FALSE);
 	setSellWithObjects(FALSE);
