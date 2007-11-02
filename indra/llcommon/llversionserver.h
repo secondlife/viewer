1e7917fa8965ab3691ee868961ea7cc36b6cab5c
Leyla Farazha <leyla@lindenlab.com>
2007-11-02 21:26:41 +0000

    Committing viewer-auth-server into release svn merge -r72818:72835 svn+ssh://svn/svn/linden/branches/viewer-auth-server-2

--- indra/llcommon/llversionserver.h
+++ indra/llcommon/llversionserver.h
@@ -35,7 +35,7 @@
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 18;
 const S32 LL_VERSION_PATCH = 3;
-const S32 LL_VERSION_BUILD = 71877;
+const S32 LL_VERSION_BUILD = 72163;
 
 const char * const LL_CHANNEL = "Second Life Server";
 
