943db8fc6bc210baf78265a987e0b2758b31f824
Eric Tulla <tulla@lindenlab.com>
2008-08-08 22:14:53 +0000

    Added the latest directx sdk release (June 2008) to the list that cmake searches for. -Reviewed by bos

--- indra/cmake/DirectX.cmake
+++ indra/cmake/DirectX.cmake
@@ -3,6 +3,7 @@
 if (VIEWER AND WINDOWS)
   find_path(DIRECTX_INCLUDE_DIR dxdiag.h
             "$ENV{DXSDK_DIR}/Include"
+            "C:/Program Files/Microsoft DirectX SDK (June 2008)/Include"
             "C:/Program Files/Microsoft DirectX SDK (March 2008)/Include"
             "C:/Program Files/Microsoft DirectX SDK (November 2007)/Include"
             "C:/Program Files/Microsoft DirectX SDK (August 2007)/Include"
@@ -21,6 +22,7 @@ if (VIEWER AND WINDOWS)
 
   find_path(DIRECTX_LIBRARY_DIR dxguid.lib
             "$ENV{DXSDK_DIR}/Lib/x86"
+            "C:/Program Files/Microsoft DirectX SDK (June 2008)/Lib/x86"
             "C:/Program Files/Microsoft DirectX SDK (March 2008)/Lib/x86"
             "C:/Program Files/Microsoft DirectX SDK (November 2007)/Lib/x86"
             "C:/Program Files/Microsoft DirectX SDK (August 2007)/Lib/x86"
