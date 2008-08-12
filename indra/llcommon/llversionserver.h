93328402371424df7a26eefc6b6866a76a4f308d
Christian Goetze <cg@lindenlab.com>
2008-08-12 17:52:55 +0000

    svn -c 94141 merge svn+ssh://svn.lindenlab.com/svn/linden/branches/viewer-merge-1

--- indra/llcommon/llversionserver.h
+++ indra/llcommon/llversionserver.h
@@ -35,7 +35,7 @@
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 24;
 const S32 LL_VERSION_PATCH = 0;
-const S32 LL_VERSION_BUILD = 93453;
+const S32 LL_VERSION_BUILD = 94081;
 
 const char * const LL_CHANNEL = "Second Life Server";
 
