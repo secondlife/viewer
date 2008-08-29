eb65aca9f8aaa1ce67a221dcfecce240f1bb59a5
Bryan O'Sullivan <bos@lindenlab.com>
2008-08-29 21:26:27 +0000

    Use target_link_libraries for llaudio

--- indra/cmake/LLAudio.cmake
+++ indra/cmake/LLAudio.cmake
@@ -6,10 +6,4 @@ set(LLAUDIO_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llaudio
     )
 
-set(LLAUDIO_LIBRARIES
-    llaudio
-    ${VORBISENC_LIBRARIES}
-    ${VORBISFILE_LIBRARIES}
-    ${VORBIS_LIBRARIES}
-    ${OGG_LIBRARIES}
-    )
+set(LLAUDIO_LIBRARIES llaudio)
