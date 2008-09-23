d0c70d11cb06eaa94900f95c16ae923911b6b565
Dave Kaprielian <sabin@lindenlab.com>
2008-09-23 23:15:14 +0000

    merge -r97452:97484 svn+ssh://svn.lindenlab.com/svn/linden/branches/deploy-refactor-2

--- indra/llcommon/llversionserver.h
+++ indra/llcommon/llversionserver.h
@@ -35,7 +35,7 @@
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 24;
 const S32 LL_VERSION_PATCH = 6;
-const S32 LL_VERSION_BUILD = 0;
+const S32 LL_VERSION_BUILD = 94536;
 
 const char * const LL_CHANNEL = "Second Life Server";
 
