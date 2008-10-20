d0e62db34ebda571db3ad78ca138275c4e84e47f
Kyle Ambroff <ambroff@lindenlab.com>
2008-10-20 01:23:26 +0000

    bump build number to 100,000. w00t.

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -35,7 +35,7 @@
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 21;
 const S32 LL_VERSION_PATCH = 5;
-const S32 LL_VERSION_BUILD = 97417;
+const S32 LL_VERSION_BUILD = 100000;
 
 const char * const LL_CHANNEL = "Second Life Release";
 
