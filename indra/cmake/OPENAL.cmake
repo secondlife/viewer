bf9166cb78f0855b6f760b2feac67e1f1313b240
Christian Goetze <cg@lindenlab.com>
2008-12-21 20:54:07 +0000

    Carry through andrew's rename of openal to openal-soft into cmake, see if that helps

--- indra/cmake/OPENAL.cmake
+++ indra/cmake/OPENAL.cmake
@@ -12,10 +12,10 @@ if (OPENAL)
   if (STANDALONE)
     include(FindPkgConfig)
     include(FindOpenAL)
-    pkg_check_modules(OPENAL_LIB REQUIRED openal)
+    pkg_check_modules(OPENAL_LIB REQUIRED openal-soft)
     pkg_check_modules(FREEALUT_LIB REQUIRED freealut)
   else (STANDALONE)
-    use_prebuilt_binary(openal)
+    use_prebuilt_binary(openal-soft)
   endif (STANDALONE)
   set(OPENAL_LIBRARIES 
     openal
