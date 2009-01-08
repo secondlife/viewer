3a5dd71ad60275d0567ceef5ef5f3405c57ba722
Ramzi Ramey <ramzi@lindenlab.com>
2009-01-08 00:07:15 +0000

    setting viewer to report 1.23.0, because code in trunk is well beyond 1.22 now and closer to what the 1.23 viewer will be

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -34,8 +34,8 @@
 #define LL_LLVERSIONVIEWER_H
 
 const S32 LL_VERSION_MAJOR = 1;
-const S32 LL_VERSION_MINOR = 22;
-const S32 LL_VERSION_PATCH = 4;
+const S32 LL_VERSION_MINOR = 23;
+const S32 LL_VERSION_PATCH = 0;
 const S32 LL_VERSION_BUILD = 0;
 
 const char * const LL_CHANNEL = "Second Life Release";
