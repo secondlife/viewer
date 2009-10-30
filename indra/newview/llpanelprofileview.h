73efd4802b8f02f86599e6b7ab7220b9f9fe2dc0
Mike Antipov <mantipov@productengine.com>
2009-10-30 19:30:23 +0200

    Implemented major task EXT-2022 (Show profile online status for non-friend avatar too.)  - imlemented like in viewer 1.23 behavior: show Online status according to avatar properties requested from server.
    --HG--
    branch : product-engine

--- indra/newview/llpanelprofileview.h
+++ indra/newview/llpanelprofileview.h
@@ -49,6 +49,7 @@ class LLPanelProfileView : public LLPanelProfile
 {
 	LOG_CLASS(LLPanelProfileView);
 	friend class LLUICtrlFactory;
+	friend class AvatarStatusObserver;
 
 public:
 
@@ -65,8 +66,9 @@ public:
 protected:
 
 	void onBackBtnClick();
-	bool isGrantedToSeeOnlineStatus();
-	void updateOnlineStatus();
+	bool isGrantedToSeeOnlineStatus(); // deprecated after EXT-2022 is implemented
+	void updateOnlineStatus(); // deprecated after EXT-2022 is implemented
+	void processOnlineStatus(bool online);
 
 private:
 	// LLCacheName will call this function when avatar name is loaded from server.
@@ -78,6 +80,8 @@ private:
 		BOOL is_group);
 
 	LLTextBox* mStatusText;
+	AvatarStatusObserver* mAvatarStatusObserver;
+	bool mAvatarIsOnline;
 };
 
 #endif //LL_LLPANELPROFILEVIEW_H
