6098706ae532131a4df890988dc209961ff52a33
Nyx (Neal Orman) <nyx@lindenlab.com>
2009-11-16 17:24:05 -0500

    EXT-2445 appearance editor doesn't show correct previews
    This system is in dire need of a rewrite, but the bug reported is fairly simple.
    The visual params in the avatar are no longer authoritative, the real values are
    stored in the wearable itself. The appearance editor was manipulating the values
    in the avatar object, resulting in no change. Updated the preview windows to use
    the parameter objects stored in the wearable.
    
    Code reviewed by Bigpapi
    
    --HG--
    branch : avatar-pipeline

--- indra/newview/lltoolmorph.cpp
+++ indra/newview/lltoolmorph.cpp
@@ -146,8 +146,8 @@ void LLVisualParamHint::preRender(BOOL clear_depth)
 {
 	LLVOAvatarSelf* avatarp = gAgent.getAvatarObject();
 
-	mLastParamWeight = avatarp->getVisualParamWeight(mVisualParam);
-	avatarp->setVisualParamWeight(mVisualParam, mVisualParamWeight);
+	mLastParamWeight = mVisualParam->getWeight();
+	mVisualParam->setWeight(mVisualParamWeight, FALSE);
 	avatarp->setVisualParamWeight("Blink_Left", 0.f);
 	avatarp->setVisualParamWeight("Blink_Right", 0.f);
 	avatarp->updateComposites();
@@ -242,7 +242,7 @@ BOOL LLVisualParamHint::render()
 		gGL.setSceneBlendType(LLRender::BT_ALPHA);
 		gGL.setAlphaRejectSettings(LLRender::CF_DEFAULT);
 	}
-	avatarp->setVisualParamWeight(mVisualParam, mLastParamWeight);
+	mVisualParam->setWeight(mLastParamWeight, FALSE);
 	gGL.color4f(1,1,1,1);
 	mGLTexturep->setGLTextureCreated(true);
 	return TRUE;
