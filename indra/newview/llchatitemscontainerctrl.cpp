a46794bcff15b70ab4e5e04ce12d118579b72245
James Cook <james@lindenlab.com>
2009-11-18 14:27:58 -0800

    EXT-2579 Fix memory leak/bloat due to /me IRC-style emotes. Pending review with Richard/Leyla

--- indra/newview/llchatitemscontainerctrl.cpp
+++ indra/newview/llchatitemscontainerctrl.cpp
@@ -188,7 +188,17 @@ void LLNearbyChatToastPanel::init(LLSD& notification)
 		}
 		mText = mText.substr(3);
 		style_params.font.style = "UNDERLINE";
+#define INFINITE_REFLOW_BUG 0
+#if INFINITE_REFLOW_BUG
+		// This causes LLTextBase::reflow() to infinite loop until the viewer
+		// runs out of memory, throws a bad_alloc exception from std::vector
+		// in mLineInfoList, and the main loop catches it and continues.
+		// It appears to be caused by addText() adding a line separator in the
+		// middle of a line.  See EXT-2579, EXT-1949
 		msg_text->addText(mText,style_params);
+#else
+		msg_text->appendText(mText, FALSE, style_params);
+#endif
 	}
 	else 
 	{
