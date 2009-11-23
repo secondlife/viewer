f6f9e63091d82f10fb76778d993ae7d2087a1452
Tofu Linden <tofu.linden@lindenlab.com>
2009-11-23 16:10:37 +0000

    incomplete but almost-working volume pulldown, except it's not a pulldown.

--- indra/newview/llstatusbar.cpp
+++ indra/newview/llstatusbar.cpp
@@ -509,6 +509,9 @@ static void onClickVolume(void* data)
 	// toggle the master mute setting
 	BOOL mute_audio = gSavedSettings.getBOOL("MuteAudio");
 	gSavedSettings.setBOOL("MuteAudio", !mute_audio);
+	
+	// toggle the master volume pull-down
+	LLFloaterReg::showInstance("volume_pulldown"); //tmp
 }
 
 // sets the static variables necessary for the date
