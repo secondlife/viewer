9d941cf1193eaaf333dd1c9427256786b1bc60c8
Mike Antipov <mantipov@productengine.com>
2009-11-25 14:52:39 +0200

    Work on major sub-task EXT-2790 (Complete Voice Control Panel (floater) started by Lynx (LLVoiceControlPanel))  -- Implemented speakers list for local chat.
    --HG--
    branch : product-engine

--- indra/newview/llcallfloater.h
+++ indra/newview/llcallfloater.h
@@ -36,6 +36,10 @@
 
 #include "llfloater.h"
 
+class LLAvatarList;
+class LLParticipantList;
+class LLSpeakerMgr;
+
 /**
  * The Voice Control Panel is an ambient window summoned by clicking the flyout chevron on the Speak button.
  * It can be torn-off and freely positioned onscreen.
@@ -53,7 +57,13 @@ public:
 	LLCallFloater();
 	~LLCallFloater();
 
+	/*virtual*/ BOOL postBuild();
 
+
+private:
+	LLSpeakerMgr* mSpeakerManager;
+	LLParticipantList* mPaticipants;
+	LLAvatarList* mAvatarList;
 };
 
 
