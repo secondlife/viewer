37c7e4059cab2a7152e730ca5d68530f14f8f51e
Lynx Linden <lynx@lindenlab.com>
2009-12-07 18:22:52 +0000

    DEV-43439: Allow Home side panel URL to be customized.
    The URL for the Home side panel is now specified via a new
    "HomeSidePanelURL" string in app_settings/settings.xml. This
    string supports the following substitutions:
    
    CHANNEL = the channel name for the viewer
    VERSION = the full version string for the viewer
    LANGUAGE = the current language set in the viewer
    AUTH_KEY = the authentication key (see below)
    
    The authentication key is an optional string that is read from the
    dictionary of strings returned by login.cgi. If login.cgi returns a
    key called "home_sidetray_token", then the value of that key is used
    for the AUTH_KEY substitution. This lets the server provide a piece of
    blind data that can be passed to the Home panel web page to support
    authentication. The viewer does not interpret the token in any way.
    
    This change adds a new module, llviewerhome.{cpp|h} to contain the
    model functionality, used by the view module, llpanelhome.{cpp|h}.

---
+++ indra/newview/llviewerhome.cpp
@@ -0,0 +1,79 @@
+/** 
+ * @file llviewerhome.cpp
+ * @brief Model (non-View) component for the web-based Home side panel
+ * @author Martin Reddy
+ *
+ * $LicenseInfo:firstyear=2009&license=viewergpl$
+ * 
+ * Copyright (c) 2009, Linden Research, Inc.
+ * 
+ * Second Life Viewer Source Code
+ * The source code in this file ("Source Code") is provided by Linden Lab
+ * to you under the terms of the GNU General Public License, version 2.0
+ * ("GPL"), unless you have obtained a separate licensing agreement
+ * ("Other License"), formally executed by you and Linden Lab.  Terms of
+ * the GPL can be found in doc/GPL-license.txt in this distribution, or
+ * online at http://secondlifegrid.net/programs/open_source/licensing/gplv2
+ * 
+ * There are special exceptions to the terms and conditions of the GPL as
+ * it is applied to this Source Code. View the full text of the exception
+ * in the file doc/FLOSS-exception.txt in this software distribution, or
+ * online at
+ * http://secondlifegrid.net/programs/open_source/licensing/flossexception
+ * 
+ * By copying, modifying or distributing this software, you acknowledge
+ * that you have read and understood your obligations described above,
+ * and agree to abide by those obligations.
+ * 
+ * ALL LINDEN LAB SOURCE CODE IS PROVIDED "AS IS." LINDEN LAB MAKES NO
+ * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
+ * COMPLETENESS OR PERFORMANCE.
+ * $/LicenseInfo$
+ */
+
+#include "llviewerprecompiledheaders.h"
+#include "llviewerhome.h"
+
+#include "lllogininstance.h"
+#include "llui.h"
+#include "lluri.h"
+#include "llsd.h"
+#include "llversionviewer.h"
+#include "llviewercontrol.h"
+
+//static
+std::string LLViewerHome::getHomeURL()
+{	
+	// Return the URL to display in the Home side tray. We read
+	// this value from settings.xml and support various substitutions
+
+	// *TODO: We should put this version pattern in a central place; this and near
+	// equivalents are replicated in other code - what's a good location?
+	std::ostringstream version;
+	version << LL_VERSION_MAJOR << "."
+	<< LL_VERSION_MINOR << "."
+	<< LL_VERSION_PATCH << "."
+	<< LL_VERSION_BUILD;
+
+	LLSD substitution;
+	substitution["VERSION"] = version.str();
+	substitution["CHANNEL"] = LLURI::escape(gSavedSettings.getString("VersionChannelName"));
+	substitution["LANGUAGE"] = LLUI::getLanguage();
+	substitution["AUTH_KEY"] = LLURI::escape(getAuthKey());
+
+	std::string homeURL = gSavedSettings.getString("HomeSidePanelURL");
+	LLStringUtil::format(homeURL, substitution);
+		
+	return homeURL;	
+}
+
+//static
+std::string LLViewerHome::getAuthKey()
+{
+	// return the value of the (optional) auth token returned by login.cgi
+	// this lets the server provide an authentication token that we can
+	// blindly pass to the Home web page for it to perform authentication.
+	static const std::string authKeyName("home_sidetray_token");
+	return LLLoginInstance::getInstance()->getResponse(authKeyName);
+}
+
