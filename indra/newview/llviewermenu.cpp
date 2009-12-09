a65473957f16f9cfba8ed10b87218d65d64519ef
James Cook <james@lindenlab.com>
2009-12-08 16:28:34 -0800

    Back out EXT-2827 Unable to delete object when select face of object May cause right-click delete to fail - testing that Backed out changeset: 1f8e1e73f9f4

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -2422,12 +2422,6 @@ class LLObjectEnableReportAbuse : public view_listener_t
 	bool handleEvent(const LLSD& userdata)
 	{
 		bool new_value = LLSelectMgr::getInstance()->getSelection()->getObjectCount() != 0;
-/*		// all the faces needs to be selected
-		if(LLSelectMgr::getInstance()->getSelection()->contains(LLSelectMgr::getInstance()->getSelection()->getPrimaryObject(),SELECT_ALL_TES ))
-		{
-			new_value = true;
-		}
- */
 		return new_value;
 	}
 };
@@ -2816,7 +2810,6 @@ BOOL enable_has_attachments(void*)
 bool enable_object_mute()
 {
 	LLViewerObject* object = LLSelectMgr::getInstance()->getSelection()->getPrimaryObject();
-	
 	bool new_value = (object != NULL);
 	if (new_value)
 	{
@@ -2829,19 +2822,6 @@ bool enable_object_mute()
 			BOOL is_self = avatar->isSelf();
 			new_value = !is_linden && !is_self;
 		}
-		else
-		{
-			if( LLSelectMgr::getInstance()->getSelection()->contains(object,SELECT_ALL_TES ))
-			{
-				new_value = true;
-			}		
-			else 
-			{
-				new_value = false;
-			}
-
-		}
-
 	}
 	return new_value;
 }
