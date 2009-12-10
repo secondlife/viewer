48ba1af15e149e111592b1e9bfbc8cce4c15d5c4
Leyla Farazha <leyla@lindenlab.com>
2009-12-10 11:30:20 -0800

    EXT-3047 	 "Block" item in "more verbs" menu of mini inspector isn't disabled when resident is blocked EXT-3358 	 Cannot toggle Grid pulldown on login screen reviewed by Richard

--- indra/newview/llpanellogin.cpp
+++ indra/newview/llpanellogin.cpp
@@ -671,12 +671,8 @@ void LLPanelLogin::refreshLocation( bool force_visible )
 	sInstance->childSetVisible("start_location_combo", show_start);
 	sInstance->childSetVisible("start_location_text", show_start);
 
-#if LL_RELEASE_FOR_DOWNLOAD
 	BOOL show_server = gSavedSettings.getBOOL("ForceShowGrid");
 	sInstance->childSetVisible("server_combo", show_server);
-#else
-	sInstance->childSetVisible("server_combo", TRUE);
-#endif
 
 #endif
 }
