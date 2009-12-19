06019288c181ad986e854969214a5c6ec3ba69b2
Steve Bennetts <steve@lindenlab.com>
2009-12-18 17:14:53 -0800

    HTTP Texture tuning: * Changed download rate update frequency * Updated network bandwidth preferences to be 100 Kbps - 10 Mbps instead of 50 Kbps - 1.5 MBps

--- indra/newview/llviewerstats.cpp
+++ indra/newview/llviewerstats.cpp
@@ -656,9 +656,9 @@ void update_statistics(U32 frame_count)
 	gObjectBits = 0;
 //	gDecodedBits = 0;
 
-	// Only update texture stats ones per second so that they are less noisy
+	// Only update texture stats periodically so that they are less noisy
 	{
-		static const F32 texture_stats_freq = 1.f;
+		static const F32 texture_stats_freq = 10.f;
 		static LLFrameTimer texture_stats_timer;
 		if (texture_stats_timer.getElapsedTimeF32() >= texture_stats_freq)
 		{
