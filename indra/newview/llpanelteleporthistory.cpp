36a9e88158eefa19ea4dac10383f2d85b6d53dc8
Eugene Mutavchi <emutavchi@productengine.com>
2009-12-24 20:02:20 +0200

    Fixed low bug EXT-1165(Teleport History stays in "filtered" state after resetting filter)
    --HG--
    branch : product-engine

--- indra/newview/llpanelteleporthistory.cpp
+++ indra/newview/llpanelteleporthistory.cpp
@@ -472,11 +472,8 @@ void LLTeleportHistoryPanel::draw()
 // virtual
 void LLTeleportHistoryPanel::onSearchEdit(const std::string& string)
 {
-	if (sFilterSubString != string)
-	{
-		sFilterSubString = string;
-		showTeleportHistory();
-	}
+	sFilterSubString = string;
+	showTeleportHistory();
 }
 
 // virtual
