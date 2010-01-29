ec5dbb60e71302a77f68cc604e7dfd52bcaa926a
Lynx Linden <lynx@lindenlab.com>
2010-01-29 13:36:39 +0000

    EXT-4763: Revert my previous fix.
    We don't want to universally disabled URL highlighting for all toast
    alerts, e.g., the report abuse notification has URLs that we want
    users to be able to click on. I have a better plan...

--- indra/newview/lltoastalertpanel.cpp
+++ indra/newview/lltoastalertpanel.cpp
@@ -170,7 +170,6 @@ LLToastAlertPanel::LLToastAlertPanel( LLNotificationPtr notification, bool modal
 	params.tab_stop(false);
 	params.wrap(true);
 	params.follows.flags(FOLLOWS_LEFT | FOLLOWS_TOP);
-	params.allow_html(false);
 
 	LLTextBox * msg_box = LLUICtrlFactory::create<LLTextBox> (params);
 	// Compute max allowable height for the dialog text, so we can allocate
