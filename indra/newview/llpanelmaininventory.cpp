cf119b522f0a5f5b8b28bc56ea1611d342fd1283
Tofu Linden <tofu.linden@lindenlab.com>
2010-02-15 17:34:06 +0000

    CID-137
    Checker: NULL_RETURNS
    Function: LLPanelMainInventory::onCustomAction(const LLSD &)
    File: /indra/newview/llpanelmaininventory.cpp

--- indra/newview/llpanelmaininventory.cpp
+++ indra/newview/llpanelmaininventory.cpp
@@ -1003,7 +1003,10 @@ void LLPanelMainInventory::onCustomAction(const LLSD& userdata)
 		}
 		const LLUUID item_id = current_item->getListener()->getUUID();
 		LLViewerInventoryItem *item = gInventory.getItem(item_id);
-		item->regenerateLink();
+		if (item)
+		{
+			item->regenerateLink();
+		}
 		active_panel->setSelection(item_id, TAKE_FOCUS_NO);
 	}
 	if (command_name == "find_original")
