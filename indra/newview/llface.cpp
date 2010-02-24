245fe3edc6fa7333c16efcc90a4a52db8450a828
Palmer Truelson <palmer@lindenlab.com>
2010-02-23 23:02:20 -0800

    Fix compiler complaints about signed/unsigned mismatch

--- indra/newview/llface.cpp
+++ indra/newview/llface.cpp
@@ -1065,14 +1065,14 @@ BOOL LLFace::getGeometryVolume(const LLVolume& volume,
 		mVertexBuffer->getIndexStrider(indicesp, mIndicesIndex);
 		if (LLPipeline::sUseTriStrips)
 		{
-			for (U32 i = 0; i < num_indices; i++)
+			for (U32 i = 0; i < (U32) num_indices; i++)
 			{
 				*indicesp++ = vf.mTriStrip[i] + index_offset;
 			}
 		}
 		else
 		{
-			for (U32 i = 0; i < num_indices; i++)
+			for (U32 i = 0; i < (U32) num_indices; i++)
 			{
 				*indicesp++ = vf.mIndices[i] + index_offset;
 			}
