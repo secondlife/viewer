0b3099fbca36be1012ddaeaf1e6cd5673caa1aca
Aimee Linden <aimee@lindenlab.com>
2010-03-23 14:45:01 +0000

    Minimal fix for EXT-6508, restore voice volume when unmuting. Ignore incoming volume level of zero from SLVoice. (May also fix EXT-5625, though unconfirmed, removes obsolete hack that was obfuscating that issue).

--- indra/newview/llinspectavatar.cpp
+++ indra/newview/llinspectavatar.cpp
@@ -564,7 +564,6 @@ void LLInspectAvatar::updateVolumeSlider()
 		LLUICtrl* volume_slider = getChild<LLUICtrl>("volume_slider");
 		volume_slider->setEnabled( !is_muted );
 
-		const F32 DEFAULT_VOLUME = 0.5f;
 		F32 volume;
 		if (is_muted)
 		{
@@ -575,13 +574,6 @@ void LLInspectAvatar::updateVolumeSlider()
 		{
 			// actual volume
 			volume = gVoiceClient->getUserVolume(mAvatarID);
-
-			// *HACK: Voice client doesn't have any data until user actually
-			// says something.
-			if (volume == 0.f)
-			{
-				volume = DEFAULT_VOLUME;
-			}
 		}
 		volume_slider->setValue( (F64)volume );
 	}
