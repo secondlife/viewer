7e76f26407c6760464b5928b7c6cb8c800a5883f
Mike Antipov <mantipov@productengine.com>
2010-04-12 15:38:01 +0300

    Fixed normal bug EXT-6660 (Just logged on resident is displayed as connected to group voice chat in the VCP)
    Reason.
     VCP floater stores voice states of its participants. If invited in group voice chat avatar goes offline and then online LLAvatarListItem representing it will display it as JOINED.
    
    Fix: added force setting of voice state in VCP for added participants.
    
    Reviewed by Vadim at https://codereview.productengine.com/secondlife/r/218/
    
    --HG--
    branch : product-engine

--- indra/newview/llcallfloater.cpp
+++ indra/newview/llcallfloater.cpp
@@ -601,10 +601,13 @@ void LLCallFloater::updateNotInVoiceParticipantState(LLAvatarListItem* item)
 			}
 		}
 		break;
-	case STATE_INVITED:
 	case STATE_LEFT:
 		// nothing to do. These states should not be changed.
 		break;
+	case STATE_INVITED:
+		// If avatar was invited into group chat and went offline it is still exists in mSpeakerStateMap
+		// If it goes online it will be rendered as JOINED via LAvatarListItem.
+		// Lets update its visual representation. See EXT-6660
 	case STATE_UNKNOWN:
 		// If an avatarID is not found in a speakers list from VoiceClient and
 		// a panel with this ID has an UNKNOWN status this means that this person
