11ab373c5d4ea5476330f0140f60bec0280d3e84
Dave Parks <davep@lindenlab.com>
2010-04-12 23:15:42 -0500

    Tracking down ATI deferred bugs WIP (transplanted from 76dacfb89b155fd1e6698d7affc81fb13ee4fb33)

--- indra/newview/app_settings/shaders/class1/deferred/avatarShadowF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/avatarShadowF.glsl
@@ -10,7 +10,7 @@ uniform sampler2D diffuseMap;
 
 void main() 
 {
-	gl_FragColor = vec4(1,1,1,gl_Color.a * texture2D(diffuseMap, gl_TexCoord[0].xy).a);
-	//gl_FragColor = vec4(1,1,1,1);
+	//gl_FragColor = vec4(1,1,1,gl_Color.a * texture2D(diffuseMap, gl_TexCoord[0].xy).a);
+	gl_FragColor = vec4(1,1,1,1);
 }
 
