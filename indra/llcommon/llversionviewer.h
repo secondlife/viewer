628b143566b81e6029d9bd35f0dc76b79db9cdf8
Kent Quirk <q@lindenlab.com>
2010-04-14 15:42:50 -0400

    update version number

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -35,7 +35,7 @@
 
 const S32 LL_VERSION_MAJOR = 2;
 const S32 LL_VERSION_MINOR = 0;
-const S32 LL_VERSION_PATCH = 0;
+const S32 LL_VERSION_PATCH = 1;
 const S32 LL_VERSION_BUILD = 200030;
 
 const char * const LL_CHANNEL = "Second Life Developer";
