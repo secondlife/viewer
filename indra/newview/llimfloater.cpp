7a65a9430e1904f9c9b23d6f75a567e76a962312
Vadim Savchuk <vsavchuk@productengine.com>
2010-04-14 18:36:11 +0300

    Fixed low priority bug EXT-6781 (Text edit field should be in focus after IM button has been pressed).
    Explicitly focus the input editor whenever an IM tab gets visible.
    
    Added a check for the IM tab to have focus and only then pass the focus to the input entry.
    The check is needed for the fix not to re-introduce EXT-5387 (Private IM Window Steals Focus from Nearby Chat).
    
    Reviewed by Alex: https://codereview.productengine.com/secondlife/r/224/
    
    --HG--
    branch : product-engine

--- indra/newview/llimfloater.cpp
+++ indra/newview/llimfloater.cpp
@@ -499,8 +499,8 @@ void LLIMFloater::setVisible(BOOL visible)
 	{
 		//only if floater was construced and initialized from xml
 		updateMessages();
-		//prevent steal focus when IM opened in multitab mode
-		if (!isChatMultiTab())
+		//prevent stealing focus when opening a background IM tab (EXT-5387, checking focus for EXT-6781)
+		if (!isChatMultiTab() || hasFocus())
 		{
 			mInputEditor->setFocus(TRUE);
 		}
