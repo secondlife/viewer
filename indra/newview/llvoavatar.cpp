d69084d5d86d551adbc9721276aa60959b7d7899
Eugene Mutavchi <emutavchi@productengine.com>
2010-04-16 19:28:37 +0300

    Reverted fix of EXT-1655 (because it causes EXT-6835)
    --HG--
    branch : product-engine

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -5609,8 +5609,6 @@ void LLVOAvatar::sitDown(BOOL bSitting)
 //-----------------------------------------------------------------------------
 void LLVOAvatar::sitOnObject(LLViewerObject *sit_object)
 {
-	sitDown(TRUE);
-
 	if (isSelf())
 	{
 		// Might be first sit
@@ -5643,6 +5641,7 @@ void LLVOAvatar::sitOnObject(LLViewerObject *sit_object)
 	mDrawable->mXform.setRotation(mDrawable->getWorldRotation() * inv_obj_rot);
 
 	gPipeline.markMoved(mDrawable, TRUE);
+	sitDown(TRUE);
 	mRoot.getXform()->setParent(&sit_object->mDrawable->mXform); // LLVOAvatar::sitOnObject
 	mRoot.setPosition(getPosition());
 	mRoot.updateWorldMatrixChildren();
