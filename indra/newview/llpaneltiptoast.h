d57e8ecc7e119ca6930499ee270f438fc5ec415a
Alexei Arabadji <aarabadji@productengine.com>
2010-04-19 17:21:16 +0300

    fixed EXT-6848 Avoid creation of dummy objects in LLOnlineStatusToast and LLPanelGenericTip classes. * decoupled tip toast panel related logic from class LLTast; * moved documentation comment of  LLPanelGenericTip constructor from .cpp to .h file; * corrected name attribute in panel_generic_tip.xml;
    reviewed by Mike Antipov and Vadim Savchuk at https://codereview.productengine.com/secondlife/r/230/
    
    --HG--
    branch : product-engine

--- indra/newview/llpaneltiptoast.h
+++ indra/newview/llpaneltiptoast.h
@@ -1,12 +1,11 @@
-/** 
- * @file llpanelgenerictip.h
- * @brief Represents a generic panel for a notifytip notifications. As example:
- * "SystemMessageTip", "Cancelled", "UploadWebSnapshotDone".
+/**
+ * @file llpaneltiptoast.h
+ * @brief Represents a base class of tip toast panels.
  *
  * $LicenseInfo:firstyear=2010&license=viewergpl$
- * 
+ *
  * Copyright (c) 2010, Linden Research, Inc.
- * 
+ *
  * Second Life Viewer Source Code
  * The source code in this file ("Source Code") is provided by Linden Lab
  * to you under the terms of the GNU General Public License, version 2.0
@@ -14,44 +13,45 @@
  * ("Other License"), formally executed by you and Linden Lab.  Terms of
  * the GPL can be found in doc/GPL-license.txt in this distribution, or
  * online at http://secondlifegrid.net/programs/open_source/licensing/gplv2
- * 
+ *
  * There are special exceptions to the terms and conditions of the GPL as
  * it is applied to this Source Code. View the full text of the exception
  * in the file doc/FLOSS-exception.txt in this software distribution, or
  * online at
  * http://secondlifegrid.net/programs/open_source/licensing/flossexception
- * 
+ *
  * By copying, modifying or distributing this software, you acknowledge
  * that you have read and understood your obligations described above,
  * and agree to abide by those obligations.
- * 
+ *
  * ALL LINDEN LAB SOURCE CODE IS PROVIDED "AS IS." LINDEN LAB MAKES NO
  * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
  * COMPLETENESS OR PERFORMANCE.
  * $/LicenseInfo$
  */
 
-#ifndef LL_PANELGENERICTIP_H
-#define LL_PANELGENERICTIP_H
 
-#include "llpaneltiptoast.h"
+#include "lltoastpanel.h"
+
+#ifndef LL_PANELTOASTTIP_H
+#define LL_PANELTOASTTIP_H
 
 /**
- * Represents tip toast panel that contains only one child element - message text.
- * This panel can be used for different cases of tip notifications.
+ * Base class for tip toast panels.
+ *
+ * Tip toast panels are required to have text message box named as 'message'.
  */
-class LLPanelGenericTip: public LLPanelTipToast
+class LLPanelTipToast : public LLToastPanel
 {
-	// disallow instantiation of this class
+	LOG_CLASS(LLPanelTipToast);
+public:
+	LLPanelTipToast(const LLNotificationPtr& notification): LLToastPanel(notification) {}
+	virtual BOOL postBuild();
 private:
-	// grant privileges to instantiate this class to LLToastPanel
-	friend class LLToastPanel;
-	/**
-	 * Generic toast tip panel.
-	 * This is particular case of toast panel that decoupled from LLToastNotifyPanel.
-	 * From now LLToastNotifyPanel is deprecated and will be removed after all  panel
-	 * types are represented in separate classes.
-	 */
-	LLPanelGenericTip(const LLNotificationPtr& notification);
+	void onMessageTextClick();
+	void onPanelClick(S32 x, S32 y, MASK mask);
+
+	LLUICtrl* mMessageText;
 };
-#endif /* LL_PANELGENERICTIP_H */
+
+#endif /* LL_PANELTOASTTIP_H */
