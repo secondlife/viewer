f972cc4be952abc943f9d289533c9951ce4dc839
Tofu Linden <tofu.linden@lindenlab.com>
2010-04-23 13:37:03 +0100

    Restore a bit of the non-deferred renderer's sun/moon waterglow in deferred rendering.  This also adds a slight pinch of glow to shiny sun-spots.  So be it. (transplanted from 5a0d9e5b5cfb2ecd96685f0275ab8e999ab86263)

--- indra/newview/app_settings/shaders/class2/deferred/softenLightF.glsl
+++ indra/newview/app_settings/shaders/class2/deferred/softenLightF.glsl
@@ -269,6 +269,7 @@ void main()
 	vec2 scol_ambocc = texture2DRect(lightMap, vary_fragcoord.xy).rg;
 	float scol = max(scol_ambocc.r, diffuse.a); 
 	float ambocc = scol_ambocc.g;
+	float glowresult = 0.0;
 	
 	calcAtmospherics(pos.xyz, ambocc);
 	
@@ -284,6 +285,7 @@ void main()
 		vec3 refnormpersp = normalize(reflect(pos.xyz, norm.xyz));
 		float sa = dot(refnormpersp, vary_light.xyz);
 		vec3 dumbshiny = vary_SunlitColor*scol_ambocc.r*texture2D(lightFunc, vec2(sa, spec.a)).a;
+		glowresult = 0.08 * dot(dumbshiny.rgb, spec.rgb);
 
 		// screen-space cheap fakey reflection map
 		//
@@ -334,5 +336,5 @@ void main()
 	col = scaleSoftClip(col);
 		
 	gl_FragColor.rgb = col;
-	gl_FragColor.a = 0.0;
+	gl_FragColor.a = glowresult;
 }
