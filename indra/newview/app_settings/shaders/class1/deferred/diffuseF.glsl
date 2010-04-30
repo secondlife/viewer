41afe80984ee6d0363032193ea4a5d3eb867b3b1
Tofu Linden <tofu.linden@lindenlab.com>
2010-04-30 10:40:27 +0100

    more tweakery for EXT-7149 objects with white color and varied texture do not appear shiny with deferred rendering (transplanted from 36680184ec50ecf5fff92ee33db711b7512d923a)

--- indra/newview/app_settings/shaders/class1/deferred/diffuseF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/diffuseF.glsl
@@ -13,7 +13,7 @@ void main()
 {
 	vec3 col = gl_Color.rgb * texture2D(diffuseMap, gl_TexCoord[0].xy).rgb;
 	gl_FragData[0] = vec4(col, 0.0);
-	gl_FragData[1] = vec4(gl_Color.aaa*1.5, gl_Color.a); // spec
+	gl_FragData[1] = gl_Color.aaaa; // spec
 	//gl_FragData[1] = vec4(vec3(gl_Color.a), gl_Color.a+(1.0-gl_Color.a)*gl_Color.a); // spec - from former class3 - maybe better, but not so well tested
 	gl_FragData[2] = vec4(normalize(vary_normal)*0.5+0.5, 0.0);
 }
