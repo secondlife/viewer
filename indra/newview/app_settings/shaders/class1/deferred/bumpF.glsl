a74859da17b92def264be49925df680bbfd6169d
Tofu Linden <tofu.linden@lindenlab.com>
2010-04-30 11:58:45 +0100

    expand EXT-7149 fix to include bumpyshiny.

--- indra/newview/app_settings/shaders/class1/deferred/bumpF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/bumpF.glsl
@@ -14,14 +14,15 @@ varying vec3 vary_mat2;
 
 void main() 
 {
-	vec3 col = texture2D(diffuseMap, gl_TexCoord[0].xy).rgb;
+	vec3 col = gl_Color.rgb * texture2D(diffuseMap, gl_TexCoord[0].xy).rgb;
 	vec3 norm = texture2D(bumpMap, gl_TexCoord[0].xy).rgb * 2.0 - 1.0;
 
 	vec3 tnorm = vec3(dot(norm,vary_mat0),
-					  dot(norm,vary_mat1),
-					  dot(norm,vary_mat2));
+			  dot(norm,vary_mat1),
+			  dot(norm,vary_mat2));
 						
-	gl_FragData[0] = vec4(gl_Color.rgb*col, 0.0);
-	gl_FragData[1] = vec4(col*gl_Color.a, gl_Color.a);
+	gl_FragData[0] = vec4(col, 0.0);
+	gl_FragData[1] = gl_Color.aaaa; // spec
+	//gl_FragData[1] = vec4(vec3(gl_Color.a), gl_Color.a+(1.0-gl_Color.a)*gl_Color.a); // spec - from former class3 - maybe better, but not so well tested
 	gl_FragData[2] = vec4(normalize(tnorm)*0.5+0.5, 0.0);
 }
