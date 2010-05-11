d03f4c2a312b1a4d8f33cd171a958670f508da70
Dave Parks <davep@lindenlab.com>
2010-05-11 00:57:30 -0500

    EXT-7225 Fix for busted projector shadows under certain settings. (transplanted from 63218c79aa5670331235d3fde25de4a595991460)

--- indra/newview/app_settings/shaders/class2/deferred/sunLightF.glsl
+++ indra/newview/app_settings/shaders/class2/deferred/sunLightF.glsl
@@ -77,6 +77,7 @@ float pcfShadow(sampler2DRectShadow shadowMap, vec4 stc, float scl)
 
 float pcfShadow(sampler2DShadow shadowMap, vec4 stc, float scl)
 {
+	stc.z += spot_shadow_offset;
 	stc.xyz /= stc.w;
 	stc.z += spot_shadow_bias*scl;
 	
@@ -182,7 +183,7 @@ void main()
 	gl_FragColor[0] = shadow;
 	gl_FragColor[1] = 1.0;
 	
-	spos.xyz = shadow_pos+offset*spot_shadow_offset;
+	spos = vec4(shadow_pos, 1.0);
 	
 	//spotlight shadow 1
 	vec4 lpos = shadow_matrix[4]*spos;
