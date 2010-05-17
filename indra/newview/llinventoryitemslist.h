d8b989a87b94fd851404b42094e88c0a0c213ac8
Sergei Litovchuk <slitovchuk@productengine.com>
2010-05-17 18:57:03 +0300

    EXT-7158 ADDITIONAL FIX Optimization and implementation fix - Fixed issue with filter not applied to outfit newly added while filtering is active. - Optimization of refreshing outfit list when filter is applied - list is not refreshed on every list visibility change.
    Reviewed by Mike Antipov at https://codereview.productengine.com/secondlife/r/394/
    
    --HG--
    branch : product-engine

--- indra/newview/llinventoryitemslist.h
+++ indra/newview/llinventoryitemslist.h
@@ -222,6 +222,12 @@ public:
 	bool getNeedsRefresh(){ return mNeedsRefresh; }
 
 	/**
+	 * Sets the flag indicating that the list needs to be refreshed even if it is
+	 * not currently visible.
+	 */
+	void setForceRefresh(bool force_refresh){ mForceRefresh = force_refresh; }
+
+	/**
 	 * Idle routine used to refresh the list regardless of the current list
 	 * visibility, unlike draw() which is called only for the visible list.
 	 * This is needed for example to filter items of the list hidden by closed
@@ -259,7 +265,7 @@ private:
 					 // Will be used in refresh() to determine added and removed ids
 	bool mNeedsRefresh;
 
-	bool mPrevVisibility;
+	bool mForceRefresh;
 
 	commit_signal_t mRefreshCompleteSignal;
 };
