fa06293a4c637b31094a8c6907982851d4d0b464
brad kittenbrink <brad@lindenlab.com>
2010-05-21 11:48:07 -0700

    Added call to use_prebuilt_binary for google-breakpad so it actually gets installed by install.py.

---
+++ indra/cmake/GoogleBreakpad.cmake
@@ -0,0 +1,11 @@
+# -*- cmake -*-
+include(Prebuilt)
+
+if (STANDALONE)
+  MESSAGE(FATAL_ERROR "*TODO standalone support for google breakad is unimplemented")
+  # *TODO - implement this include(FindGoogleBreakpad)
+else (STANDALONE)
+  use_prebuilt_binary(google_breakpad)
+  set(BREAKPAD_EXCEPTION_HANDLER_LIBRARIES exception_handler)
+endif (STANDALONE)
+
