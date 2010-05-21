82468016519150242eef436dd46fb14a9a037d3a
Loren Shih <seraph@lindenlab.com>
2010-05-21 13:34:21 -0400

    EXT-7439 FIXED reduce size of avatar textures floater
    Texture elements are now smaller so the floater doesn't consume the entire viewer window space.

--- indra/newview/lltexturectrl.cpp
+++ indra/newview/lltexturectrl.cpp
@@ -1304,7 +1304,7 @@ void LLTextureCtrl::draw()
 							 LLColor4::white, LLFontGL::LEFT, LLFontGL::BASELINE, LLFontGL::DROP_SHADOW);
 
 			v_offset += 12;
-			tdesc = llformat("  ID  : %s...", (mImageAssetID.asString().substr(0,10)).c_str());
+			tdesc = llformat("  ID  : %s...", (mImageAssetID.asString().substr(0,7)).c_str());
 			font->renderUTF8(tdesc, 0, llfloor(interior.mLeft+3), llfloor(interior.mTop-v_offset),
 							 LLColor4::white, LLFontGL::LEFT, LLFontGL::BASELINE, LLFontGL::DROP_SHADOW);
 		}
