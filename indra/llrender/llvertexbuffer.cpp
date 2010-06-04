a4ce5c9eaa0d69ff7acf5fb710f0b9a601fb2a75
Dave Parks <davep@lindenlab.com>
2010-06-04 00:09:52 -0500

    Fix for useVBOs being busted when sEnableVBOs gets flipped.

--- indra/llrender/llvertexbuffer.cpp
+++ indra/llrender/llvertexbuffer.cpp
@@ -900,7 +900,7 @@ BOOL LLVertexBuffer::useVBOs() const
 		return FALSE;
 	}
 #endif
-	return sEnableVBOs;
+	return TRUE;
 }
 
 //----------------------------------------------------------------------------
@@ -1317,7 +1317,7 @@ void LLVertexBuffer::setBuffer(U32 data_mask)
 	{		
 		if (mGLBuffer)
 		{
-			if (sEnableVBOs && sVBOActive)
+			if (useVBOs() && sVBOActive)
 			{
 				glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
 				sBindCount++;
@@ -1329,7 +1329,7 @@ void LLVertexBuffer::setBuffer(U32 data_mask)
 				setup = TRUE; // ... or a client memory pointer changed
 			}
 		}
-		if (sEnableVBOs && mGLIndices && sIBOActive)
+		if (useVBOs() && mGLIndices && sIBOActive)
 		{
 			/*if (sMapped)
 			{
