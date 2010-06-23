d5fb82283997f50d81af7dbf364783a5961b75d5
Sergei Litovchuk <slitovchuk@productengine.com>
2010-06-23 17:29:37 +0300

    Linux build fix.
    --HG--
    branch : product-engine

--- indra/newview/llappearancemgr.h
+++ indra/newview/llappearancemgr.h
@@ -34,10 +34,12 @@
 #define LL_LLAPPEARANCEMGR_H
 
 #include "llsingleton.h"
+
+#include "llagentwearables.h"
+#include "llcallbacklist.h"
 #include "llinventorymodel.h"
 #include "llinventoryobserver.h"
 #include "llviewerinventory.h"
-#include "llcallbacklist.h"
 
 class LLWearable;
 class LLWearableHoldingPattern;
