a880243f34a77549478e76c0606d69c953928e45
Mike Antipov <mantipov@productengine.com>
2010-07-15 15:26:13 +0300

    EXT-8374 FIXED Added possibility to localize "None" action value for Animation and Sound Actions. Also changed hardcoded "until animations are done" value for Wait with label from the appropriate Floater checkbox.
    Reviewed by Vadim Savchuk at https://codereview.productengine.com/secondlife/r/762/
    
    --HG--
    branch : product-engine

--- indra/llcharacter/llmultigesture.cpp
+++ indra/llcharacter/llmultigesture.cpp
@@ -293,7 +293,7 @@ BOOL LLGestureStepAnimation::deserialize(LLDataPacker& dp)
 	dp.unpackU32(mFlags, "flags");
 	return TRUE;
 }
-// *TODO: Translate
+// *NOTE: result is translated in LLPreviewGesture::getLabel()
 std::vector<std::string> LLGestureStepAnimation::getLabel() const 
 {
 	std::vector<std::string> strings;
@@ -367,7 +367,7 @@ BOOL LLGestureStepSound::deserialize(LLDataPacker& dp)
 	dp.unpackU32(mFlags, "flags");
 	return TRUE;
 }
-// *TODO: Translate
+// *NOTE: result is translated in LLPreviewGesture::getLabel()
 std::vector<std::string> LLGestureStepSound::getLabel() const
 {
 	std::vector<std::string> strings;
@@ -425,7 +425,7 @@ BOOL LLGestureStepChat::deserialize(LLDataPacker& dp)
 	dp.unpackU32(mFlags, "flags");
 	return TRUE;
 }
-// *TODO: Translate
+// *NOTE: result is translated in LLPreviewGesture::getLabel()
 std::vector<std::string> LLGestureStepChat::getLabel() const
 {
 	std::vector<std::string> strings;
@@ -479,7 +479,7 @@ BOOL LLGestureStepWait::deserialize(LLDataPacker& dp)
 	dp.unpackU32(mFlags, "flags");
 	return TRUE;
 }
-// *TODO: Translate
+// *NOTE: result is translated in LLPreviewGesture::getLabel()
 std::vector<std::string> LLGestureStepWait::getLabel() const
 {
 	std::vector<std::string> strings;
