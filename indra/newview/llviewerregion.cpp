19d1d57a658db911b36099c3b5dbc94465e4bbdb
brad kittenbrink <brad@lindenlab.com>
2010-07-29 19:49:13 -0700

    Adding a simple region debug console.

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -1533,6 +1533,7 @@ void LLViewerRegion::setSeedCapability(const std::string& url)
 	capabilityNames.append("SendUserReport");
 	capabilityNames.append("SendUserReportWithScreenshot");
 	capabilityNames.append("ServerReleaseNotes");
+	capabilityNames.append("SimConsole");
 	capabilityNames.append("SimulatorFeatures");
 	capabilityNames.append("StartGroupProposal");
 	capabilityNames.append("TextureStats");
