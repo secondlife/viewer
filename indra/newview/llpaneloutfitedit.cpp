07f668fe2b135f96b74e0999d052b4608e223b68
Tofu Linden <tofu.linden@lindenlab.com>
2010-08-05 13:31:19 +0100

    fix a merge conflict I forgot to commit earlier.

--- indra/newview/llpaneloutfitedit.cpp
+++ indra/newview/llpaneloutfitedit.cpp
@@ -1321,7 +1321,15 @@ void LLPanelOutfitEdit::getSelectedItemsUUID(uuid_vec_t& uuid_list)
 
 void LLPanelOutfitEdit::onCOFChanged()
 {
+	//the panel is only updated when is visible to a user
+
+	// BAP - this check has to be removed because otherwise item name
+	// changes made when the panel is not visible will not be
+	// propagated to the panel.
+	// if (!isInVisibleChain()) return;
+
 	update();
+}
 
 void LLPanelOutfitEdit::updateWearablesPanelVerbButtons()
 {
@@ -1346,11 +1354,6 @@ void LLPanelOutfitEdit::saveListSelection()
 		std::set<LLUUID> selected_ids = mInventoryItemsPanel->getRootFolder()->getSelectionList();
 
 		if(!selected_ids.size()) return;
-
-	// BAP - this check has to be removed because otherwise item name
-	// changes made when the panel is not visible will not be
-	// propagated to the panel.
-	// if (!isInVisibleChain()) return;
 
 		for (std::set<LLUUID>::const_iterator item_id = selected_ids.begin(); item_id != selected_ids.end(); ++item_id)
 		{
