e41525cac444ecacb96f18178de21b844c4fbe45
Dave Parks <davep@lindenlab.com>
2010-08-26 17:06:34 -0500

    Fix for crash when rendering translation handles.

--- indra/llrender/llrender.cpp
+++ indra/llrender/llrender.cpp
@@ -901,7 +901,7 @@ LLVector3 LLRender::getUITranslation()
 {
 	if (mUIOffset.empty())
 	{
-		llerrs << "UI offset stack empty." << llendl;
+		return LLVector3(0,0,0);
 	}
 	return mUIOffset.back();
 }
@@ -910,7 +910,7 @@ LLVector3 LLRender::getUIScale()
 {
 	if (mUIScale.empty())
 	{
-		llerrs << "UI scale stack empty." << llendl;
+		return LLVector3(1,1,1);
 	}
 	return mUIScale.back();
 }
