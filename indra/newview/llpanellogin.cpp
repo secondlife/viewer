b4fc6681ca787fdffcf43c7243bca7b20dbadff4
Leyla Farazha <leyla@lindenlab.com>
2010-09-03 13:50:57 -0700

    DEV-53068 ADITI login page broken on Viewer 2

--- indra/newview/llpanellogin.cpp
+++ indra/newview/llpanellogin.cpp
@@ -834,7 +834,7 @@ void LLPanelLogin::loadLoginPage()
 	curl_free(curl_version);
 
 	// Grid
-	char* curl_grid = curl_escape(LLGridManager::getInstance()->getGridLoginID().c_str(), 0);
+	char* curl_grid = curl_escape(LLGridManager::getInstance()->getGridLabel().c_str(), 0);
 	oStr << "&grid=" << curl_grid;
 	curl_free(curl_grid);
 	gViewerWindow->setMenuBackgroundColor(false, !LLGridManager::getInstance()->isInProductionGrid());
