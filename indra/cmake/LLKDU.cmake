dac53830f1a67c8657ced9c39eccedbadf149bd9
Merov Linden <merov@lindenlab.com>
2010-10-27 23:40:35 -0700

    STORM-104 : make kdu statically linked, suppress the need for llkdu

--- indra/cmake/LLKDU.cmake
+++ indra/cmake/LLKDU.cmake
@@ -1,7 +1,24 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+# USE_KDU can be set when launching cmake or develop.py as an option using the argument -DUSE_KDU:BOOL=ON
+# When building using proprietary binaries though (i.e. having access to LL private servers), we always build with KDU
 if (INSTALL_PROPRIETARY AND NOT STANDALONE)
-  use_prebuilt_binary(kdu)
-  set(LLKDU_LIBRARY llkdu)
+  set(USE_KDU ON)
 endif (INSTALL_PROPRIETARY AND NOT STANDALONE)
+
+if (USE_KDU)
+  use_prebuilt_binary(kdu)
+  if (WINDOWS)
+    set(KDU_LIBRARY debug kdu_cored optimized kdu_core)
+  else (WINDOWS)
+    set(KDU_LIBRARY kdu)
+  endif (WINDOWS)
+
+  set(KDU_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
+
+  set(LLKDU_LIBRARY llkdu)
+  set(LLKDU_STATIC_LIBRARY llkdu_static)
+  set(LLKDU_LIBRARIES ${LLKDU_LIBRARY})
+  set(LLKDU_STATIC_LIBRARIES ${LLKDU_STATIC_LIBRARY})
+endif (USE_KDU)
