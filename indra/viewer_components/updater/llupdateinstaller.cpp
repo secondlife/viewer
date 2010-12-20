2c68cb2c69348522ebb648aa4abd2ca7e4038b80
Andrew A. de Laix <alain@lindenlab.com>
2010-12-20 10:38:36 -0800

    fix windows build?

--- indra/viewer_components/updater/llupdateinstaller.cpp
+++ indra/viewer_components/updater/llupdateinstaller.cpp
@@ -25,7 +25,6 @@
 
 #include "linden_common.h"
 #include <apr_file_io.h>
-#include <boost/lexical_cast.hpp>
 #include "llapr.h"
 #include "llprocesslauncher.h"
 #include "llupdateinstaller.h"
@@ -35,6 +34,7 @@
 #if defined(LL_WINDOWS)
 #pragma warning(disable: 4702)      // disable 'unreachable code' so we can use lexical_cast (really!).
 #endif
+#include <boost/lexical_cast.hpp>
 
 
 namespace {
