2230b52145ba7b22bea4b365b010016e62c25b58
Andrew A. de Laix <alain@lindenlab.com>
2011-01-11 16:07:14 -0800

    remove develop.py and references to it (use autobuild instead)

--- indra/cmake/LLKDU.cmake
+++ indra/cmake/LLKDU.cmake
@@ -1,7 +1,7 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-# USE_KDU can be set when launching cmake or develop.py as an option using the argument -DUSE_KDU:BOOL=ON
+# USE_KDU can be set when launching cmake as an option using the argument -DUSE_KDU:BOOL=ON
 # When building using proprietary binaries though (i.e. having access to LL private servers), we always build with KDU
 if (INSTALL_PROPRIETARY AND NOT STANDALONE)
   set(USE_KDU ON)
