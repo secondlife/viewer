eb3de8543e5580cb353230577f9f6606e71002f2
Merov Linden <merov@lindenlab.com>
2011-02-24 20:57:34 -0800

    STORM-1023 (was OPEN-4) and STORM-1022 (was OPEN-24) : fmod and kdu changes : cmake changes making possible to use Findxxx when INSTALL_PROPRIETARY is OFF and STANDALONE is OFF too, simplify cmake scripts around INSTALL_PROPRIETARY, add fmodex to FindFMOD

--- indra/cmake/FMOD.cmake
+++ indra/cmake/FMOD.cmake
@@ -1,17 +1,23 @@
 # -*- cmake -*-
 
-set(FMOD ON CACHE BOOL "Use FMOD sound library.")
+# FMOD can be set when launching the make using the argument -DFMOD:BOOL=ON
+# When building using proprietary binaries though (i.e. having access to LL private servers),
+# we always build with FMOD.
+# Open source devs should use the -DFMOD:BOOL=ON then if they want to build with FMOD, whether
+# they are using STANDALONE or not.
+if (INSTALL_PROPRIETARY)
+  set(FMOD ON CACHE BOOL "Use FMOD sound library.")
+endif (INSTALL_PROPRIETARY)
 
 if (FMOD)
-  if (STANDALONE)
+  if (NOT INSTALL_PROPRIETARY)
+    # This cover the STANDALONE case and the NOT STANDALONE but not using proprietary libraries
+	# This should then be invoke by all open source devs outside LL
     set(FMOD_FIND_REQUIRED ON)
     include(FindFMOD)
-  else (STANDALONE)
-    if (INSTALL_PROPRIETARY)
-      include(Prebuilt)
-      use_prebuilt_binary(fmod)
-    endif (INSTALL_PROPRIETARY)
-    
+  else (NOT INSTALL_PROPRIETARY)
+	include(Prebuilt)
+	use_prebuilt_binary(fmod)    
     if (WINDOWS)
       set(FMOD_LIBRARY fmod)
     elseif (DARWIN)
@@ -19,8 +25,7 @@ if (FMOD)
     elseif (LINUX)
       set(FMOD_LIBRARY fmod-3.75)
     endif (WINDOWS)
-
     SET(FMOD_LIBRARIES ${FMOD_LIBRARY})
     set(FMOD_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
-  endif (STANDALONE)
+  endif (NOT INSTALL_PROPRIETARY)
 endif (FMOD)
