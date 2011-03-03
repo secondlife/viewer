6e97ead181f04c5cb4953e5e3316d7d8caf0a1ec
Kelly Washington <kelly@lindenlab.com>
2011-03-03 12:05:40 -0800

    Add early exit to message template check if it hasn't changed.

--- indra/cmake/TemplateCheck.cmake
+++ indra/cmake/TemplateCheck.cmake
@@ -5,10 +5,10 @@ include(Python)
 macro (check_message_template _target)
   add_custom_command(
       TARGET ${_target}
-      POST_BUILD
+      PRE_LINK
       COMMAND ${PYTHON_EXECUTABLE}
       ARGS ${SCRIPTS_DIR}/template_verifier.py
-           --mode=development --cache_master
-      COMMENT "Verifying message template"
+           --mode=development --cache_master ${TEMPLATE_VERIFIER_OPTIONS}
+      COMMENT "Verifying message template - See http://wiki.secondlife.com/wiki/Template_verifier.py"
       )
 endmacro (check_message_template)
