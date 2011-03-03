0e87f37161a7b8c5df4f27b0f3a491c3b6546f66
Richard Linden <none@none>
2011-03-02 21:29:36 -0800

    SOCIAL-623 FIX World map accessible from classified and picks in web profile in minimal skin

--- indra/newview/llfloaterworldmap.cpp
+++ indra/newview/llfloaterworldmap.cpp
@@ -110,6 +110,12 @@ public:
 	bool handle(const LLSD& params, const LLSD& query_map,
 				LLMediaCtrl* web)
 	{
+		if (!LLUI::sSettingGroups["config"]->getBOOL("EnableWorldMap"))
+		{
+			LLNotificationsUtil::add("NoWorldMap", LLSD(), LLSD(), std::string("SwitchToStandardSkinAndQuit"));
+			return true;
+		}
+
 		if (params.size() == 0)
 		{
 			// support the secondlife:///app/worldmap SLapp
