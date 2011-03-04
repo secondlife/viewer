c077250ff16aeaf0ec4c036154a427c677e85eb3
Vadim ProductEngine <vsavchuk@productengine.com>
2011-03-04 19:26:07 +0200

    STORM-1016 FIXED Crash after pressing ctrl-shift-w while there is an undocked sidepanel.
    Reason: The shortcut closes all floaters, including those wrapping undocked sidepanels.
    The sidepanels get destroyed as well, while they are still referenced by the side tray.
    
    Fix: Dock (i.e. move to side tray) the sidepanel before its floater gets destroyed.

--- indra/llui/llfloater.cpp
+++ indra/llui/llfloater.cpp
@@ -2865,7 +2865,7 @@ void LLFloater::initFromParams(const LLFloater::Params& p)
 	// close callback 
 	if (p.close_callback.isProvided())
 	{
-		mCloseSignal.connect(initCommitCallback(p.close_callback));
+		setCloseCallback(initCommitCallback(p.close_callback));
 	}
 }
 
@@ -2873,6 +2873,11 @@ boost::signals2::connection LLFloater::setMinimizeCallback( const commit_signal_
 { 
 	if (!mMinimizeSignal) mMinimizeSignal = new commit_signal_t();
 	return mMinimizeSignal->connect(cb); 
+}
+
+boost::signals2::connection LLFloater::setCloseCallback( const commit_signal_t::slot_type& cb )
+{
+	return mCloseSignal.connect(cb);
 }
 
 LLFastTimer::DeclareTimer POST_BUILD("Floater Post Build");
