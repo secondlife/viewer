ec0c28ad8383dd6b342a317d15d44015fde15416
Andrew de Laix <alain@lindenlab.com>
2011-03-14 20:24:55 +0000

    update ogg-vorbis archive for linux.

--- indra/llaudio/llaudiodecodemgr.cpp
+++ indra/llaudio/llaudiodecodemgr.cpp
@@ -680,7 +680,7 @@ BOOL LLAudioDecodeMgr::addDecodeRequest(const LLUUID &uuid)
 	return FALSE;
 }
 
-#ifdef LL_DARWIN
+#if LL_DARWIN || LL_LINUX
 // HACK: to fool the compiler into not emitting unused warnings.
 namespace {
 	const ov_callbacks callback_array[4] = {OV_CALLBACKS_DEFAULT, OV_CALLBACKS_NOCLOSE, OV_CALLBACKS_STREAMONLY, 
