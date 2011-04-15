04f79f63589ab35b01ec9e3d8128e68a201ff332
Jonathan Yap <none@none>
2011-04-15 13:13:15 -0400

    STORM-956 Ability to mute dialogs by muting object (or object owner)

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -6462,9 +6462,12 @@ void process_script_dialog(LLMessageSystem* msg, void**)
 	LLSD payload;
 
 	LLUUID object_id;
-	msg->getUUID("Data", "ObjectID", object_id);
+    LLUUID owner_id;
 
-	if (LLMuteList::getInstance()->isMuted(object_id))
+	msg->getUUID("Data", "ObjectID", object_id);
+    msg->getUUID("OwnerData", "OwnerID", owner_id);
+
+	if (LLMuteList::getInstance()->isMuted(object_id) || LLMuteList::getInstance()->isMuted(owner_id))
 	{
 		return;
 	}
