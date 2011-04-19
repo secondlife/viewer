27301aa5461ccc8e1e16edd6438875ecd701065d
Vadim ProductEngine <vsavchuk@productengine.com>
2011-04-19 23:46:18 +0300

    STORM-1182 FIX Fixed XUI Preview tool not loading XML files from a dev checkout on Linux.
    The bug seems to be caused by recent switch to Autobuild, which affected the build directory path.

--- indra/llvfs/lldir_linux.cpp
+++ indra/llvfs/lldir_linux.cpp
@@ -93,11 +93,11 @@ LLDir_Linux::LLDir_Linux()
 #else
 	mAppRODataDir = tmp_str;
 #endif
-    std::string::size_type indra_pos = mExecutableDir.find("/indra");
-    if (indra_pos != std::string::npos)
+    std::string::size_type build_dir_pos = mExecutableDir.find("/build-linux-");
+    if (build_dir_pos != std::string::npos)
     {
 		// ...we're in a dev checkout
-		mSkinBaseDir = mExecutableDir.substr(0, indra_pos) + "/indra/newview/skins";
+		mSkinBaseDir = mExecutableDir.substr(0, build_dir_pos) + "/indra/newview/skins";
 		llinfos << "Running in dev checkout with mSkinBaseDir "
 		 << mSkinBaseDir << llendl;
     }
