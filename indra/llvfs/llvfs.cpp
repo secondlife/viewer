4bc90f24b3ced350ed44ca782f876af19af83c41
brad kittenbrink <brad@lindenlab.com>
2011-04-26 14:58:58 -0700

    FIX VWR-25608 error on shutdow due to buffer overflow in LLVFS::audit

--- indra/llvfs/llvfs.cpp
+++ indra/llvfs/llvfs.cpp
@@ -26,6 +26,8 @@
 
 #include "linden_common.h"
 
+#include "llvfs.h"
+
 #include <sys/stat.h>
 #include <set>
 #include <map>
@@ -39,8 +41,6 @@
 #include <sys/file.h>
 #endif
     
-#include "llvfs.h"
-
 #include "llstl.h"
 #include "lltimer.h"
     
@@ -1711,7 +1711,8 @@ void LLVFS::audit()
     
 	BOOL vfs_corrupt = FALSE;
 	
-	std::vector<U8> buffer(index_size);
+	// since we take the address of element 0, we need to have at least one element.
+	std::vector<U8> buffer(llmax(index_size,1U));
 
 	if (fread(&buffer[0], 1, index_size, mIndexFP) != index_size)
 	{
