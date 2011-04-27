36986de1fc1437f509d120b79430c87575e755b1
brad kittenbrink <brad@lindenlab.com>
2011-04-27 16:02:16 -0700

    Fix for mac compile error in VWR-25608 fix.

--- indra/llvfs/llvfs.cpp
+++ indra/llvfs/llvfs.cpp
@@ -1712,7 +1712,7 @@ void LLVFS::audit()
 	BOOL vfs_corrupt = FALSE;
 	
 	// since we take the address of element 0, we need to have at least one element.
-	std::vector<U8> buffer(llmax(index_size,1U));
+	std::vector<U8> buffer(llmax<size_t>(index_size,1U));
 
 	if (fread(&buffer[0], 1, index_size, mIndexFP) != index_size)
 	{
