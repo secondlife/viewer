42fe138cbb1af4acbb6851711f8c7f86c65795c0
brad kittenbrink <brad@lindenlab.com>
2011-04-27 19:13:17 -0700

    Incorporated some suggestions from code review for VWR-25610

--- indra/llxml/llcontrol.cpp
+++ indra/llxml/llcontrol.cpp
@@ -837,7 +837,6 @@ U32 LLControlGroup::saveToFile(const std::string& filename, BOOL nondefault_only
 
 U32 LLControlGroup::loadFromFile(const std::string& filename, bool set_default_values, bool save_values)
 {
-	std::string name;
 	LLSD settings;
 	llifstream infile;
 	infile.open(filename);
@@ -862,8 +861,8 @@ U32 LLControlGroup::loadFromFile(const std::string& filename, bool set_default_v
 	for(LLSD::map_const_iterator itr = settings.beginMap(); itr != settings.endMap(); ++itr)
 	{
 		bool persist = true;
-		name = (*itr).first;
-		LLSD const & control_map = (*itr).second;
+		std::string const & name = itr->first;
+		LLSD const & control_map = itr->second;
 		
 		if(control_map.has("Persist")) 
 		{
