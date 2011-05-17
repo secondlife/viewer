086988e6ed0accd0f2cb2e87317dbcfd93a11e02
Leyla Farazha <leyla@lindenlab.com>
2011-05-16 18:07:26 -0700

    initial commit of llpanelmarketplace files

---
+++ indra/newview/llpanelmarketplace.h
@@ -0,0 +1,46 @@
+/** 
+ * @file llpanelmarketplaces.h
+ * @brief Panel for marketplace inbox and outbox
+ *
+* $LicenseInfo:firstyear=2011&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2010, Linden Research, Inc.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+#ifndef LL_LLPANELMARKETPLACE_H
+#define LL_LLPANELMARKETPLACE_H
+
+#include "llpanel.h"
+
+class LLPanelMarketplace : public LLPanel
+{
+public:
+
+	LOG_CLASS(LLPanelMarketplace);
+
+	LLPanelMarketplace();
+	~LLPanelMarketplace();
+
+	/*virtual*/ BOOL postBuild();
+};
+
+
+#endif //LL_LLPANELMARKETPLACE_H
+
