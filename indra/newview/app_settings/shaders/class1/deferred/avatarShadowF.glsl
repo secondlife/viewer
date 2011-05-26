a6fa155725205d1e6207abfe1112f92bbe38203d
Dave Parks <davep@lindenlab.com>
2011-05-25 19:23:57 -0500

    Make avatars use same shadow offset magic as prims.

--- indra/newview/app_settings/shaders/class1/deferred/avatarShadowF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/avatarShadowF.glsl
@@ -9,10 +9,13 @@
 
 uniform sampler2D diffuseMap;
 
+varying vec4 post_pos;
 
 void main() 
 {
 	//gl_FragColor = vec4(1,1,1,gl_Color.a * texture2D(diffuseMap, gl_TexCoord[0].xy).a);
 	gl_FragColor = vec4(1,1,1,1);
+
+	gl_FragDepth = max(post_pos.z/post_pos.w*0.5+0.5, 0.0);
 }
 
