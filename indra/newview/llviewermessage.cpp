cc7a041bfe908faa1a256d13d35cb1ad17358f60
Jonathan Yap <none@none>
2011-06-03 14:41:08 -0400

    Fix for Storm-956 crashes viewer when connected to non-LL grids

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -6483,10 +6483,14 @@ void process_script_dialog(LLMessageSystem* msg, void**)
 	LLSD payload;
 
 	LLUUID object_id;
-    LLUUID owner_id;
-
 	msg->getUUID("Data", "ObjectID", object_id);
-    msg->getUUID("OwnerData", "OwnerID", owner_id);
+
+//	For compability with OS grids first check for presence of extended packet before fetching data.
+    LLUUID owner_id;
+	if (gMessageSystem->getNumberOfBlocks("OwnerData") > 0)
+	{
+		msg->getUUID("OwnerData", "OwnerID", owner_id);
+	}
 
 	if (LLMuteList::getInstance()->isMuted(object_id) || LLMuteList::getInstance()->isMuted(owner_id))
 	{
