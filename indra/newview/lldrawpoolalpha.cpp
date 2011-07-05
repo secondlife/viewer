de374744294d5d23e1b84a8feb8fa8b5e4e7dc49
Dave Parks <davep@lindenlab.com>
2011-07-05 13:59:32 -0500

    SH-1918 Fix for highlight transparent not working when basic shaders enabled.

--- indra/newview/lldrawpoolalpha.cpp
+++ indra/newview/lldrawpoolalpha.cpp
@@ -295,16 +295,24 @@ void LLDrawPoolAlpha::render(S32 pass)
 
 	if (sShowDebugAlpha)
 	{
-		if(gPipeline.canUseWindLightShaders()) 
+		BOOL shaders = gPipeline.canUseVertexShaders();
+		if(shaders) 
 		{
-			LLGLSLShader::bindNoShader();
+			gObjectFullbrightNonIndexedProgram.bind();
 		}
-		gPipeline.enableLightsFullbright(LLColor4(1,1,1,1));
+		else
+		{
+			gPipeline.enableLightsFullbright(LLColor4(1,1,1,1));
+		}
 		glColor4f(1,0,0,1);
 		LLViewerFetchedTexture::sSmokeImagep->addTextureStats(1024.f*1024.f);
 		gGL.getTexUnit(0)->bind(LLViewerFetchedTexture::sSmokeImagep, TRUE) ;
 		renderAlphaHighlight(LLVertexBuffer::MAP_VERTEX |
 							LLVertexBuffer::MAP_TEXCOORD0);
+		if(shaders) 
+		{
+			gObjectFullbrightNonIndexedProgram.unbind();
+		}
 	}
 }
 
