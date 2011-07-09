72a52cb5de5be82b536d2c721bf69b3ac4dfc046
Xiaohong Bao <bao@lindenlab.com>
2011-07-08 21:01:41 -0600

    fix for STORM-1468: [crashhunters] pre-login crash at LLViewerWindow::LLViewerWindow(std::basic_string,std::allocator > const &,std::basic_string,std::allocator > const &,int,int,int,int,int,int) [secondlife-bin llviewerwindow.cpp]

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4519,7 +4519,7 @@ void LLAppViewer::idleShutdown()
 
 void LLAppViewer::sendLogoutRequest()
 {
-	if(!mLogoutRequestSent)
+	if(!mLogoutRequestSent && gMessageSystem)
 	{
 		LLMessageSystem* msg = gMessageSystem;
 		msg->newMessageFast(_PREHASH_LogoutRequest);
