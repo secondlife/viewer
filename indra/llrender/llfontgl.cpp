912e99906b30c4558b755eb2c9e1c5efbde5e88c
Boroondas Gupte <hg@boroon.dasgupta.ch>
2011-07-21 10:49:19 +0200

    Credit Zi: FIRE-543 change fixes SH-489 and VWR-24017 and therefore also VWR-25588
    It looks like the parent changeset (86eec7b46566; transplanted from
    http://hg.phoenixviewer.com/phoenix-firestorm-lgpl/rev/7af0278beaef)
    fixes the following issues:
    * SH-489 (object hover text not occluded)
    * VWR-24017 (nametag / bubblechat not occluded)
    * VWR-25588 (union of the above two symptopms)
    
    Thus list them all under Zi's entry in doc/contributions.txt.
    
    Also removed her name from the in-code comment, to adhere to our CS.

--- indra/llrender/llfontgl.cpp
+++ indra/llrender/llfontgl.cpp
@@ -195,7 +195,7 @@ S32 LLFontGL::render(const LLWString &wstr, S32 begin_offset, F32 x, F32 y, cons
 	origin.mV[VX] -= llround((F32)sCurOrigin.mX) - (sCurOrigin.mX);
 	origin.mV[VY] -= llround((F32)sCurOrigin.mY) - (sCurOrigin.mY);
 
-	// don't forget to do the depth translation, too. -Zi
+	// don't forget to do the depth translation, too.
 	gGL.translatef(0.f,0.f,sCurOrigin.mZ);
 
 	S32 chars_drawn = 0;
