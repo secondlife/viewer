1cff73e807018b8c258285f5b8dcc90e66e17955
Richard Nelson <richard@lindenlab.com>
2011-10-03 18:57:23 -0700

    fixed build

--- indra/llui/lltoolbar.cpp
+++ indra/llui/lltoolbar.cpp
@@ -107,7 +107,7 @@ LLToolBar::LLToolBar(const LLToolBar::Params& p)
 {
 	mButtonParams[LLToolBarEnums::BTNTYPE_ICONS_WITH_TEXT] = p.button_icon_and_text;
 	mButtonParams[LLToolBarEnums::BTNTYPE_ICONS_ONLY] = p.button_icon;
-	mUUID = LLUUID::LLUUID::generateNewID(p.name);
+	mUUID = LLUUID::generateNewID(p.name);
 }
 
 LLToolBar::~LLToolBar()
