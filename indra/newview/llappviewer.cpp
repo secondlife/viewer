c9c03ffa0ca21eeb2786920cf9c0cb547be8a468
Xiaohong Bao <bao@lindenlab.com>
2011-12-01 14:52:37 -0700

    fix for sh-2735: LLCurl causes SL to freeze when logout

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1781,6 +1781,7 @@ bool LLAppViewer::cleanup()
 		pending += LLAppViewer::getTextureFetch()->update(1); // unpauses the texture fetch thread
 		pending += LLVFSThread::updateClass(0);
 		pending += LLLFSThread::updateClass(0);
+		pending += LLCurl::getCurlThread()->update(1) ;
 		F64 idle_time = idleTimer.getElapsedTimeF64();
 		if(!pending)
 		{
@@ -1792,6 +1793,7 @@ bool LLAppViewer::cleanup()
 			break;
 		}
 	}
+	LLCurl::getCurlThread()->pause() ;
 
 	// Delete workers first
 	// shotdown all worker threads before deleting them in case of co-dependencies
