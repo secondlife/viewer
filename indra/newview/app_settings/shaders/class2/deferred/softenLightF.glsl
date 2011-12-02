a8cb28cb2c589a658900243f98c5c017b9a638c8
Dave Parks <davep@lindenlab.com>
2011-12-02 09:40:20 -0600

    SH-1912 Tweak based on QA/resident feedback

--- indra/newview/app_settings/shaders/class2/deferred/softenLightF.glsl
+++ indra/newview/app_settings/shaders/class2/deferred/softenLightF.glsl
@@ -316,7 +316,8 @@ void main()
 
 			//add environmentmap
 			vec3 env_vec = env_mat * refnormpersp;
-			col += textureCube(environmentMap, env_vec).rgb * max(spec.a-diffuse.a-0.2, 0.0); 
+			col = mix(col.rgb, textureCube(environmentMap, env_vec).rgb, 
+				max(spec.a-diffuse.a*2.0, 0.0)); 
 		}
 			
 		col = atmosLighting(col);
