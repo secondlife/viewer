739358779267413887a57d2a6d6b651312245495
Nyx (Neal Orman) <nyx@lindenlab.com>
2012-01-25 18:46:19 -0500

    PATH-192 WIP adding convex decomp to pathing
    First pass at using new llPhysicsExtensions library which
    combines pathing and convex decomp.

---
+++ indra/cmake/LLPhysicsExtensions.cmake
@@ -0,0 +1,31 @@
+# -*- cmake -*-
+include(Prebuilt)
+
+use_prebuilt_binary(llpysicsextensions)
+set(LLPHYSICS_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/lib/include)
+
+set(LLPHYSICS_DEBUG_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/debug)
+set(LLPHYSICS_RELEASE_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/release)
+
+find_library(LL_PHYSICS_DEBUG_LIB llpathinglib PATHS ${LLPHYSICS_DEBUG_LIBRARY_PATH})
+find_library(LL_PHYSICS_RELEASE_LIB llpathinglib PATHS ${LLPHYSICS_RELEASE_LIBRARY_PATH})
+
+set(LLPHYSICS_LIBRARIES
+
+    debug     ${LL_PHYSICS_DEBUG_LIB}
+    optimized ${LL_PHYSICS_RELEASE_LIB}
+)
+
+if (LINUX)
+    list(INSERT LLPHYSICS_LIBRARIES 0 -Wl,--start-group)
+    list(APPEND LLPHYSICS_LIBRARIES -Wl,--end-group)
+endif (LINUX)
+
+# if (INSTALL_PROPRIETARY AND NOT STANDALONE)
+#   use_prebuilt_binary(llconvexdecomposition)
+#   set(LLCONVEXDECOMP_LIBRARY llconvexdecomposition)
+# else (INSTALL_PROPRIETARY AND NOT STANDALONE)
+#  use_prebuilt_binary(llconvexdecompositionstub)
+#  set(LLCONVEXDECOMP_LIBRARY llconvexdecompositionstub)
+# endif (INSTALL_PROPRIETARY AND NOT STANDALONE)
+
