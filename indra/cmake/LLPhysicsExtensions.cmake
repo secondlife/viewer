e67cc17e437141301cedca5ae194a3845d8f265b
Nyx (Neal Orman) <nyx@lindenlab.com>
2012-01-25 19:05:37 -0500

    PATH-192 WIP adjusting cmake build for physics extensions

--- indra/cmake/LLPhysicsExtensions.cmake
+++ indra/cmake/LLPhysicsExtensions.cmake
@@ -1,14 +1,14 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-use_prebuilt_binary(llpysicsextensions)
+use_prebuilt_binary(llphysicsextensions)
 set(LLPHYSICS_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/lib/include)
 
 set(LLPHYSICS_DEBUG_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/debug)
 set(LLPHYSICS_RELEASE_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/release)
 
-find_library(LL_PHYSICS_DEBUG_LIB llpathinglib PATHS ${LLPHYSICS_DEBUG_LIBRARY_PATH})
-find_library(LL_PHYSICS_RELEASE_LIB llpathinglib PATHS ${LLPHYSICS_RELEASE_LIBRARY_PATH})
+find_library(LL_PHYSICS_DEBUG_LIB llphysicsextensions PATHS ${LLPHYSICS_DEBUG_LIBRARY_PATH})
+find_library(LL_PHYSICS_RELEASE_LIB llphysicsextensions PATHS ${LLPHYSICS_RELEASE_LIBRARY_PATH})
 
 set(LLPHYSICS_LIBRARIES
 
