00c85944548f7236a0951d4711b33b660b17588b
Todd Stinson <stinson@lindenlab.com>
2012-01-26 15:24:25 -0800

    PATH-181 and PATH-194: Implementing the building of the viewer with the stub library and validating that it does load/run as expected with the physics extensions disabled.

--- indra/cmake/LLPhysicsExtensions.cmake
+++ indra/cmake/LLPhysicsExtensions.cmake
@@ -1,25 +1,27 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+use_prebuilt_binary(llphysicsextensions)
 set(LLPHYSICS_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/lib/include)
 
+set(LLPHYSICS_DEBUG_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/debug)
+set(LLPHYSICS_RELEASE_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/release)
+
 if (INSTALL_PROPRIETARY AND NOT STANDALONE)
-   use_prebuilt_binary(llphysicsextensions)
-
-   set(LLPHYSICS_DEBUG_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/debug)
-   set(LLPHYSICS_RELEASE_LIBRARY_PATH ${LIBS_PREBUILT_DIR}/lib/release)
-
-   find_library(LL_PHYSICS_DEBUG_LIB llphysicsextensions PATHS ${LLPHYSICS_DEBUG_LIBRARY_PATH})
-   find_library(LL_PHYSICS_RELEASE_LIB llphysicsextensions PATHS ${LLPHYSICS_RELEASE_LIBRARY_PATH})
-
-   set(LLPHYSICS_LIBRARIES
-       debug     ${LL_PHYSICS_DEBUG_LIB}
-       optimized ${LL_PHYSICS_RELEASE_LIB}
-   )
-
-   if (LINUX)
-       list(INSERT LLPHYSICS_LIBRARIES 0 -Wl,--start-group)
-       list(APPEND LLPHYSICS_LIBRARIES -Wl,--end-group)
-   endif (LINUX)
+    set(LL_PHYSICS_LIB_NAME "llphysicsextensions")
 else (INSTALL_PROPRIETARY AND NOT STANDALONE)
+    set(LL_PHYSICS_LIB_NAME "llphysicsextensionsstub")
 endif (INSTALL_PROPRIETARY AND NOT STANDALONE)
+
+find_library(LL_PHYSICS_DEBUG_LIB ${LL_PHYSICS_LIB_NAME} PATHS ${LLPHYSICS_DEBUG_LIBRARY_PATH})
+find_library(LL_PHYSICS_RELEASE_LIB ${LL_PHYSICS_LIB_NAME} PATHS ${LLPHYSICS_RELEASE_LIBRARY_PATH})
+
+set(LLPHYSICS_LIBRARIES
+    debug     ${LL_PHYSICS_DEBUG_LIB}
+    optimized ${LL_PHYSICS_RELEASE_LIB}
+)
+
+if (LINUX)
+    list(INSERT LLPHYSICS_LIBRARIES 0 -Wl,--start-group)
+    list(APPEND LLPHYSICS_LIBRARIES -Wl,--end-group)
+endif (LINUX)
