fe2b04119546a4f2c6ea9faccc6208aba0488bcc
prep <none@none>
2012-02-03 14:41:17 -0500

    Path-118: Enable viewer to handle submitting multiple navmesh regions to the LLPathingLibrary.

--- indra/newview/llfloaterpathfindingsetup.cpp
+++ indra/newview/llfloaterpathfindingsetup.cpp
@@ -317,7 +317,7 @@ void LLFloaterPathfindingSetup::setHasNavMeshReceived()
 	--mNavMeshCnt;
 	if ( mNavMeshCnt == 0 )
 	{
-		//LLPathingLib::getInstance()->stitchNavMeshes();
+		LLPathingLib::getInstance()->stitchNavMeshes();
 	}
 }
 
@@ -383,11 +383,11 @@ void LLFloaterPathfindingSetup::onOpen(const LLSD& pKey)
 		LLViewerRegion* pCurrentRegion = gAgent.getRegion();
 		std::vector<LLViewerRegion*> regions;
 		regions.push_back( pCurrentRegion );
-		//pCurrentRegion->getNeighboringRegions( regions );
+		pCurrentRegion->getNeighboringRegions( regions );
 
 		std::vector<int> shift;
 		shift.push_back( CURRENT_REGION );
-		//pCurrentRegion->getNeighboringRegionsStatus( shift );
+		pCurrentRegion->getNeighboringRegionsStatus( shift );
 
 		//If the navmesh shift ops and the total region counts do not match - use the current region, only.
 		if ( shift.size() != regions.size() )
