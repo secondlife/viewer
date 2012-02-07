33a42b32ca72031a79edca821966f6ebbdcddc93
Nat Goodspeed <nat@lindenlab.com>
2012-02-07 13:06:38 -0500

    Disable MSVC warning C4702 (unreachable code) in Boost headers.

--- indra/test/test.cpp
+++ indra/test/test.cpp
@@ -54,8 +54,16 @@
 #include <gtest/gtest.h>
 #endif
 
+#if LL_MSVC
+#pragma warning (push)
+#pragma warning (disable : 4702) // warning C4702: unreachable code
+#endif
 #include <boost/iostreams/tee.hpp>
 #include <boost/iostreams/stream.hpp>
+#if LL_MSVC
+#pragma warning (pop)
+#endif
+
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/foreach.hpp>
