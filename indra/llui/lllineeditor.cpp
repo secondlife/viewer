d7303541ccd4ea1420de4cd11ff824aeb171fae7
Kitty Barnett <develop@catznip.com>
2012-05-31 17:37:19 +0200

    STORM-276 FIXED Line editor misspells are checked every frame

--- indra/llui/lllineeditor.cpp
+++ indra/llui/lllineeditor.cpp
@@ -1883,7 +1883,7 @@ void LLLineEditor::draw()
 		// Calculate start and end indices for the first and last visible word
 		U32 start = prevWordPos(mScrollHPos), end = nextWordPos(mScrollHPos + rendered_text);
 
-		if ( (mSpellCheckStart != start) || (mSpellCheckEnd = end) )
+		if ( (mSpellCheckStart != start) || (mSpellCheckEnd != end) )
 		{
 			const LLWString& text = mText.getWString().substr(start, end);
 
