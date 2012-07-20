12b1ec62645e772174cd710a208f59ab823eafe0
Oz Linden <oz@lindenlab.com>
2012-07-20 07:38:12 -0700

    refactor to build llphysicsextensions_tpv package for sublicensees

--- indra/cmake/LLPhysicsExtensions.cmake
+++ indra/cmake/LLPhysicsExtensions.cmake
@@ -1,21 +1,29 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-# Note that the use_prebuilt_binary macros below do not in fact include binaries;
-# the llphysicsextensions_* packages are source only and are built here.
-# The source package and the stub package both build libraries of the same name.
+# There are three possible solutions to provide the llphysicsextensions:
+# - The full source package, selected by -DHAVOK:BOOL=ON
+# - The stub source package, selected by -DHAVOK:BOOL=OFF 
+# - The prebuilt package available to those with sublicenses, selected by -DHAVOK_TPV:BOOL=ON
 
-# Using the real wrapper or the stub can be controlled with -DHAVOK:BOOL={ON,OFF}
-# the default
 if (INSTALL_PROPRIETARY)
    set(HAVOK ON CACHE BOOL "Use Havok physics library")
 endif (INSTALL_PROPRIETARY)
+
+
+# Note that the use_prebuilt_binary macros below do not in fact include binaries;
+# the llphysicsextensions_* packages are source only and are built here.
+# The source package and the stub package both build libraries of the same name.
 
 if (HAVOK)
    include(Havok)
    use_prebuilt_binary(llphysicsextensions_source)
    set(LLPHYSICSEXTENSIONS_SRC_DIR ${LIBS_PREBUILT_DIR}/llphysicsextensions/src)
    set(LLPHYSICSEXTENSIONS_LIBRARIES    llphysicsextensions)
+
+elseif (HAVOK_TPV)
+   use_prebuilt_binary(llphysicsextensions_tpv)
+   set(LLPHYSICSEXTENSIONS_LIBRARIES    llphysicsextensions_tpv)
 
 else (HAVOK)
    use_prebuilt_binary(llphysicsextensions_stub)
