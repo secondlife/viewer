86a6f5478c91d292f2becfb93e958dabc1d58dc3
maxim_productengine <mnikolenko@productengine.com>
2012-08-08 13:31:23 +0300

    MAINT-1121 FIXED Disable "Open" if several calling cards are selected.

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -4624,6 +4624,10 @@ void LLCallingCardBridge::buildContextMenu(LLMenuGL& menu, U32 flags)
 		{
 			disabled_items.push_back(std::string("Share"));
 		}
+		if ((flags & FIRST_SELECTED_ITEM) == 0)
+		{
+		disabled_items.push_back(std::string("Open"));
+		}
 		addOpenRightClickMenuOption(items);
 		items.push_back(std::string("Properties"));
 
