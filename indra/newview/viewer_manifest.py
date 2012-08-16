d400bc2a3fd30a446d00243e219e183697a610a5
callumprentice <none@none>
2012-08-15 20:04:10 -0700

    Add explicit reference to keychain to use

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -834,7 +834,8 @@ class DarwinManifest(ViewerManifest):
                 keychain_pwd = open(keychain_pwd_path).read().rstrip()
 
                 self.run_command('security unlock-keychain -p "%s" "%s/Library/Keychains/viewer.keychain"' % ( keychain_pwd, home_path ) )
-                self.run_command('codesign --force --sign --verbose %(identity)r %(bundle)r' % {
+                self.run_command('codesign --force --keychain '"%(home_path)/Library/Keychains/viewer.keychain"' --sign --verbose %(identity)r %(bundle)r' % {i
+                                 'home_path' : home_path,
                                  'identity': identity,
                                   'bundle': self.get_dst_prefix()
                 })
