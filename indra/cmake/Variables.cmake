3e05f169d68639d304c75cb464e05f27206b4739
Aura Linden <aura@lindenlab.com>
2012-08-17 09:04:16 -0700

    Minor change required for merge from viewer-development

--- indra/cmake/Variables.cmake
+++ indra/cmake/Variables.cmake
@@ -106,13 +106,12 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   # To support a different SDK update these Xcode settings:
   if (XCODE_VERSION GREATER 4.2)
   	set(CMAKE_OSX_DEPLOYMENT_TARGET 10.6)
-	set(CMAKE_OSX_SYSROOT macosx10.6)
-	set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvmgcc42")
   else (XCODE_VERSION GREATER 4.2)
   	set(CMAKE_OSX_DEPLOYMENT_TARGET 10.5)
-    set(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.5.sdk)
-  	set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "4.0")
   endif (XCODE_VERSION GREATER 4.2)
+
+  set(CMAKE_OSX_SYSROOT macosx10.6)
+  set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvmgcc42")
       
   set(CMAKE_XCODE_ATTRIBUTE_DEBUG_INFORMATION_FORMAT dwarf-with-dsym)
 
