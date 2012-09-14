72f6bebe59f5f900af02a4d234a4091a3e034529
AlexanderP ProductEngine <apaschenko@productengine.com>
2012-09-14 16:11:42 +0300

    CHUI-261 Additional fix (Nearby chat controls and log are not visible on next login after tearing off and docking nearby chat in previous session): prevent start LLNearbyChat before start its container

--- indra/newview/llimconversation.cpp
+++ indra/newview/llimconversation.cpp
@@ -381,6 +381,9 @@ void LLIMConversation::hideAllStandardButtons()
 
 void LLIMConversation::updateHeaderAndToolbar()
 {
+	// prevent start conversation before its container
+    LLIMFloaterContainer::getInstance();
+
 	bool is_torn_off = !getHost();
 	if (!is_torn_off)
 	{
