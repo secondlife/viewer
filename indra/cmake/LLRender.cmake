ed08113ce0e21471498c410a5e66c9e4c2b7a394
Don Kjer <don@lindenlab.com>
2012-09-20 05:07:09 +0000

    Temporarily disabling llrenderheadless due to build issues

--- indra/cmake/LLRender.cmake
+++ indra/cmake/LLRender.cmake
@@ -8,15 +8,18 @@ set(LLRENDER_INCLUDE_DIRS
     ${GLH_INCLUDE_DIR}
     )
 
-if ((SERVER OR BAKING) AND LINUX)
-  set(LLRENDER_LIBRARIES
-      llrenderheadless
-      )
-else ((SERVER OR BAKING) AND LINUX)
+#if ((SERVER OR BAKING) AND LINUX)
+#  set(LLRENDER_LIBRARIES
+#      llrenderheadless
+#      )
+#else ((SERVER OR BAKING) AND LINUX)
+#set(LLRENDER_LIBRARIES
+#    llrender
+#    )
+#endif ((SERVER OR BAKING) AND LINUX)
 set(LLRENDER_LIBRARIES
     llrender
     )
-endif ((SERVER OR BAKING) AND LINUX)
 
 # mapserver requires certain files to be copied so LL_MESA_HEADLESS can be set
 # differently for different object files.
