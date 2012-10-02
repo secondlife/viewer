983f62cc9e8077f9ce51672334bd359fb8bc4129
William Todd Stinson <stinson@lindenlab.com>
2012-10-02 11:13:27 -0700

    Adding libtcmalloc to the linux installer package.

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -1081,9 +1081,7 @@ class Linux_i686Manifest(LinuxManifest):
             # version number.
             self.path("libfontconfig.so.*.*")
             try:
-                self.path("libtcmalloc.so", "libtcmalloc.so") #formerly called google perf tools
-                self.path("libtcmalloc.so.0", "libtcmalloc.so.0") #formerly called google perf tools
-                self.path("libtcmalloc.so.0.1.0", "libtcmalloc.so.0.1.0") #formerly called google perf tools
+                self.path("libtcmalloc.so*") #formerly called google perf tools
                 pass
             except:
                 print "tcmalloc files not found, skipping"
