08f58ac3fdc89209b7bfbdf009803e75815505f4
Don Kjer <don@lindenlab.com>
2012-10-13 02:56:36 +0000

    Adding libdl to OSMesa16 link line

--- indra/cmake/OpenGL.cmake
+++ indra/cmake/OpenGL.cmake
@@ -6,7 +6,7 @@ include(Prebuilt)
 if (BUILD_HEADLESS)
   use_prebuilt_binary(mesa)
   SET(OPENGL_glu_LIBRARY GLU)
-  SET(OPENGL_HEADLESS_LIBRARIES OSMesa16 GLU)
+  SET(OPENGL_HEADLESS_LIBRARIES OSMesa16 dl GLU)
 endif (BUILD_HEADLESS)
 
 include(FindOpenGL)
