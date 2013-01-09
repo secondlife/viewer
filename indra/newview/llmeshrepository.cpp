4a84c7c8e5212d71cbd254bced17f3474253b672
Dave Parks <davep@lindenlab.com>
2013-01-09 13:20:41 -0600

    More merge cleanup.

--- indra/newview/llmeshrepository.cpp
+++ indra/newview/llmeshrepository.cpp
@@ -416,14 +416,6 @@ public:
 		}
 	}
 
-	~LLWholeModelFeeResponder()
-	{
-		if (mThread)
-		{
-			mThread->stopRequest();
-		}
-	}
-
 	virtual void completed(U32 status,
 						   const std::string& reason,
 						   const LLSD& content)
