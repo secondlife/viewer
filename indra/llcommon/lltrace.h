8c73ff245cee8da2ca75c5651be88a36358a5dae
Richard Linden <none@none>
2013-01-10 00:12:25 -0800

    SH-3468 WIP add memory tracking base class actually use return value of posix_memalign!

--- indra/llcommon/lltrace.h
+++ indra/llcommon/lltrace.h
@@ -680,7 +680,7 @@ void* allocAligned(size_t size)
 #elif LL_DARWIN
 		padded_allocation = ll_aligned_malloc(size_with_reserve, ALIGNMENT);
 #else
-		posix_memalign(&padded_allocation, ALIGNMENT, size_with_reserve);
+		padded_allocation = posix_memalign(&padded_allocation, ALIGNMENT, size_with_reserve);
 #endif
 	}
 	return (char*)padded_allocation + aligned_reserve;
