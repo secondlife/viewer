9de2371f157fa7f62b1c91949fb05b45f18c9812
Oz Linden <oz@lindenlab.com>
2013-02-01 17:04:53 -0500

    make teleport requests appear correctly with respect to open im sessions

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -3017,7 +3017,6 @@ void process_improved_im(LLMessageSystem *msg, void **user_data)
 					}
 					else if (IM_TELEPORT_REQUEST == dialog)
 					{
-						payload["SUPPRESS_TOAST"] = true;
 						params.name = "TeleportRequest";
 						params.functor.name = "TeleportRequest";
 					}
