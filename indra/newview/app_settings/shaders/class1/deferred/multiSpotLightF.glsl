f6ad4359756268067c74f3d17049f646a2139aef
Geenz <geenz@geenzo.com>
2013-02-22 17:52:22 -0500

    Fix for deferred rendering no longer working when the viewer is compiled against the OS X 10.7 SDK and up.

--- indra/newview/app_settings/shaders/class1/deferred/multiSpotLightF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/multiSpotLightF.glsl
@@ -32,6 +32,7 @@ out vec4 frag_color;
 //class 1 -- no shadows
 
 #extension GL_ARB_texture_rectangle : enable
+#extension GL_ARB_shader_texture_lod : enable
 
 uniform sampler2DRect diffuseRect;
 uniform sampler2DRect specularRect;
