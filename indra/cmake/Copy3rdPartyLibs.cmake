903996e8d4ebc30c42d3c2d041fb7a1c8e530ab8
Graham Madarasz (Graham Linden) <graham@lindenlab.com>
2013-03-28 19:25:51 -0700

    Google Breakpad Fix

--- indra/cmake/Copy3rdPartyLibs.cmake
+++ indra/cmake/Copy3rdPartyLibs.cmake
@@ -260,7 +260,6 @@ elseif(LINUX)
         libboost_filesystem-mt.so.${BOOST_VERSION}.0
         libboost_signals-mt.so.${BOOST_VERSION}.0
         libboost_system-mt.so.${BOOST_VERSION}.0
-        libbreakpad_client.so.0
         libcollada14dom.so
         libcrypto.so.1.0.0
         libdb-5.1.so
