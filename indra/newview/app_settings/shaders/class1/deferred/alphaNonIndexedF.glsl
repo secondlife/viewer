7d623abfc3c7f6ef99ae3189a592211f4bf696db
Graham Madarasz (Graham Linden) <graham@lindenlab.com>
2013-04-11 20:16:11 -0700

    NORSPEC-98 redux...hopefully fix mac AND linux at the same time

--- indra/newview/app_settings/shaders/class1/deferred/alphaNonIndexedF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/alphaNonIndexedF.glsl
@@ -126,8 +126,8 @@ void main()
 
 	color.rgb = scaleSoftClip(color.rgb);
 	vec3 light_col = vec3(0,0,0);
-
-#define LIGHT_LOOP(i) \
+#if MAC_GEFORCE_HACK
+  #define LIGHT_LOOP(i) \
 	light_col += light_diffuse[i].rgb * calcPointLightOrSpotLight(pos.xyz, vary_norm, light_position[i], light_direction[i], light_attenuation[i].x, light_attenuation[i].y, light_attenuation[i].z);
 
 	LIGHT_LOOP(1)
@@ -137,7 +137,12 @@ void main()
 	LIGHT_LOOP(5)
 	LIGHT_LOOP(6)
 	LIGHT_LOOP(7)
-
+#else
+	for (int i = 2; i < 8; i++)
+	{
+		light_col += light_diffuse[i].rgb * calcPointLightOrSpotLight(pos.xyz, vary_norm, light_position[i], light_direction[i], light_attenuation[i].x, light_attenuation[i].y, light_attenuation[i].z);
+	}
+#endif
 	color.rgb += diff.rgb * vary_pointlight_col * light_col;
 
 	frag_color = color;
