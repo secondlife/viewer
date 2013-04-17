83f3fd0dd68092b2f8917342ac45f31d322674c5
Graham Madarasz <graham@lindenlab.com>
2013-04-17 14:27:50 -0700

    NORSPEC-59 new shader changes -> new decrepit shader compiler workarounds

--- indra/newview/app_settings/shaders/class1/deferred/alphaV.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/alphaV.glsl
@@ -53,7 +53,7 @@ mat4 getSkinnedTransform();
 vec4 calcLighting(vec3 pos, vec3 norm, vec4 color, vec4 baseCol);
 void calcAtmospherics(vec3 inPositionEye);
 
-float calcDirectionalLight(vec3 n, vec3 l);
+vec3 calcDirectionalLight(vec3 n, vec3 l);
 
 vec3 atmosAmbient(vec3 light);
 vec3 atmosAffectDirectionalLight(float lightIntensity);
@@ -86,13 +86,13 @@ uniform vec3 light_direction[8];
 uniform vec3 light_attenuation[8]; 
 uniform vec3 light_diffuse[8];
 
-float calcDirectionalLight(vec3 n, vec3 l)
+vec3 calcDirectionalLight(vec3 n, vec3 l)
 {
         float a = max(dot(n,l),0.0);
-        return a;
+        return vec3(a,a,a);
 }
 
-float calcPointLightOrSpotLight(vec3 v, vec3 n, vec4 lp, vec3 ln, float la, float fa, float is_pointlight)
+vec3 calcPointLightOrSpotLight(vec3 v, vec3 n, vec4 lp, vec3 ln, float la, float fa, float is_pointlight)
 {
 	//get light vector
 	vec3 lv = lp.xyz-v;
@@ -119,14 +119,13 @@ float calcPointLightOrSpotLight(vec3 v, vec3 n, vec4 lp, vec3 ln, float la, floa
 		da *= max(dot(n, lv), 0.0);		
 	}
 
-	return da;	
+	return vec3(da,da,da);	
 }
 
 void main()
 {
 	vec4 pos;
 	vec3 norm;
-	
 	//transform vertex
 #if HAS_SKIN
 	mat4 trans = getObjectSkinnedTransform();
@@ -159,6 +158,7 @@ void main()
 	pos = (modelview_matrix * vert);
 	gl_Position = modelview_projection_matrix*vec4(position.xyz, 1.0);
 #endif
+
 	vary_texcoord1 = (texture_matrix0 * vec4(texcoord1,0,1)).xy;
 	vary_texcoord2 = (texture_matrix0 * vec4(texcoord2,0,1)).xy;
 #if INDEX_MODE == INDEXED
@@ -190,10 +190,11 @@ void main()
 	col.rgb = vec3(0,0,0);
 
 	// Add windlight lights
-	col.rgb = atmosAmbient(vec3(0.));
+	col.rgb = atmosAmbient(col.rgb);
 	
 	vary_ambient = col.rgb*diffuse_color.rgb;
-	vary_directional.rgb = atmosAffectDirectionalLight(1);
+
+	vary_directional.rgb = atmosAffectDirectionalLight(1.0f);
 	
 	col.rgb = col.rgb*diffuse_color.rgb;
 #if INDEX_MODE != NON_INDEXED_NO_COLOR
@@ -208,5 +209,5 @@ void main()
 	pos = modelview_projection_matrix * vert;
 	vary_fragcoord.xyz = pos.xyz + vec3(0,0,near_clip);
 #endif
-	
+
 }
