f42f01d4c298c19e476a7f2ecad926aebedfe170
Graham Madarasz (Graham Linden) <graham@lindenlab.com>
2013-04-20 05:38:57 -0700

    Another mismerge

--- indra/newview/app_settings/shaders/class2/deferred/alphaV.glsl
+++ indra/newview/app_settings/shaders/class2/deferred/alphaV.glsl
@@ -54,7 +54,7 @@ mat4 getSkinnedTransform();
 vec4 calcLighting(vec3 pos, vec3 norm, vec4 color, vec4 baseCol);
 void calcAtmospherics(vec3 inPositionEye);
 
-float calcDirectionalLight(vec3 n, vec3 l);
+vec3 calcDirectionalLight(vec3 n, vec3 l);
 
 vec3 atmosAmbient(vec3 light);
 vec3 atmosAffectDirectionalLight(float lightIntensity);
