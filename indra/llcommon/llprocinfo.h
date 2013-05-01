7992564e06ed4977c0f4d13fee4f858f6275542a
Monty Brandenberg <monty@lindenlab.com>
2013-05-01 23:19:34 +0000

    SH-4153  Port user and system cpu accounting from example program. The http_texture_load example program has some cpu usage gathering tools that should be generally useful and specifically for the deadman switch.  Port these into llcommon into new all-static class LLProcInfo.  Add unit test, etc.

---
+++ indra/llcommon/llprocinfo.h
@@ -0,0 +1,68 @@
+/** 
+* @file   llprocinfo.h
+* @brief  Interface to process/cpu/resource information services.
+* @author monty@lindenlab.com
+*
+* $LicenseInfo:firstyear=2013&license=viewerlgpl$
+* Second Life Viewer Source Code
+* Copyright (C) 2013, Linden Research, Inc.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation;
+* version 2.1 of the License only.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
+* Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+* $/LicenseInfo$
+*/
+
+#ifndef	LL_PROCINFO_H
+#define	LL_PROCINFO_H
+
+
+#include "linden_common.h"
+
+/// @file llprocinfo.h
+///
+/// Right now, this is really a namespace disguised as a class.
+/// It wraps some types and functions to return information about
+/// process resource consumption in a non-OS-specific manner.
+///
+/// Threading:  No instances so that's thread-safe.  Implementations
+/// of static functions should be thread-safe, they mostly involve
+/// direct syscall invocations.
+///
+/// Allocation:  Not instantiatable.
+
+class LL_COMMON_API LLProcInfo
+{
+public:
+	/// Public types
+
+	typedef U64 time_type;								/// Relative microseconds
+	
+private:
+	LLProcInfo();										// Not defined
+	~LLProcInfo();										// Not defined
+	LLProcInfo(const LLProcInfo &);						// Not defined
+	void operator=(const LLProcInfo &);					// Not defined
+
+public:
+	/// Get accumulated system and user CPU time in
+	/// microseconds.  Syscalls involved in every invocation.
+	///
+	/// Threading:  expected to be safe.
+	static void getCPUUsage(time_type & user_time, time_type & system_time);
+};
+	
+
+#endif	// LL_PROCINFO_H
---
+++ indra/llcommon/tests/llprocinfo_test.cpp
@@ -0,0 +1,91 @@
+/** 
+ * @file llprocinfo_test.cpp
+ * @brief Tests for the LLProcInfo class.
+ *
+ * $LicenseInfo:firstyear=2013&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2013, Linden Research, Inc.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+#include "linden_common.h"
+
+#include "../llprocinfo.h"
+
+#include "../test/lltut.h"
+#include "../lltimer.h"
+
+
+static const LLProcInfo::time_type bad_user(289375U), bad_system(275U);
+
+
+namespace tut
+{
+
+struct procinfo_test
+{
+	procinfo_test()
+		{
+		}
+};
+
+typedef test_group<procinfo_test> procinfo_group_t;
+typedef procinfo_group_t::object procinfo_object_t;
+tut::procinfo_group_t procinfo_instance("LLProcInfo");
+
+
+// Basic invocation works
+template<> template<>
+void procinfo_object_t::test<1>()
+{
+	LLProcInfo::time_type user(bad_user), system(bad_system);
+
+	set_test_name("getCPUUsage() basic function");
+
+	LLProcInfo::getCPUUsage(user, system);
+	
+	ensure_not_equals("getCPUUsage() writes to its user argument", user, bad_user);
+	ensure_not_equals("getCPUUsage() writes to its system argument", system, bad_system);
+}
+
+
+// Time increases
+template<> template<>
+void procinfo_object_t::test<2>()
+{
+	LLProcInfo::time_type user(bad_user), system(bad_system);
+	LLProcInfo::time_type user2(bad_user), system2(bad_system);
+
+	set_test_name("getCPUUsage() increases over time");
+
+	LLProcInfo::getCPUUsage(user, system);
+	
+	for (int i(0); i < 100000; ++i)
+	{
+		ms_sleep(0);
+	}
+	
+	LLProcInfo::getCPUUsage(user2, system2);
+
+	ensure_equals("getCPUUsage() user value increases over time", user2 > user, true);
+	ensure_equals("getCPUUsage() system value increases over time", system2 > system, true);
+}
+
+
+} // end namespace tut
