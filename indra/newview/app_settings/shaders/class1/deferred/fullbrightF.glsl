ae0f362f220c54e72700b96545b9f4956ee8a113
Graham Madarasz <graham@lindenlab.com>
2013-05-08 20:00:38 -0700

    Rollback NORSPEC-156 change which fails to compile due to implicit cast

--- indra/newview/app_settings/shaders/class1/deferred/fullbrightF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/fullbrightF.glsl
@@ -42,7 +42,8 @@ void main()
 {
 	float shadow = 1.0;
 
-	vec4 color = diffuseLookup(vary_texcoord0.xy) * pow(vertex_color.rgb,vec3(2.2f));
+	vec4 color = diffuseLookup(vary_texcoord0.xy) * vertex_color;
+	color.rgb = pow(color.rgb,vec3(2.2f,2.2f,2.2f));
 	
 	color.rgb = fullbrightAtmosTransport(color.rgb);
 
