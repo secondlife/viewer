e12b6caaa0596eae2fad43636663dd4e14a1949c
Cinders <cinder.roxley@phoenixviewer.com>
2013-07-15 14:24:59 -0600

    Remove trivial duplicate ; at the end of a line

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -6895,7 +6895,7 @@ bool handle_lure_callback(const LLSD& notification, const LLSD& response)
 				std::string target_name;
 				gCacheName->getFullName(target_id, target_name);  // for im log filenames
 				LLSD args;
-				args["TO_NAME"] = LLSLURL("agent", target_id, "displayname").getSLURLString();;
+				args["TO_NAME"] = LLSLURL("agent", target_id, "displayname").getSLURLString();
 	
 				LLSD payload;
 				
