22a3b0bd7baf132a90ddf02d5eb965b71451b757
Merov Linden <merov@lindenlab.com>
2013-11-19 20:27:25 -0800

    MAINT-2287 : Fix merge errors when merging viewer-release

--- indra/newview/app_settings/shaders/class1/avatar/avatarSkinV.glsl
+++ indra/newview/app_settings/shaders/class1/avatar/avatarSkinV.glsl
@@ -31,12 +31,10 @@ uniform vec4 matrixPalette[45];
 mat4 getSkinnedTransform()
 {
 	mat4 ret;
-	
-	float x = fract(weight.x);
 	int i = int(floor(weight.x));
-    i = int(min(i, 15));
-    i = int(max(i, 0));
-	ret[0] = mix(matrixPalette[i+0], matrixPalette[i+1],  x);
+	float x = fract(weight.x);
+		
+	ret[0] = mix(matrixPalette[i+0], matrixPalette[i+1], x);
 	ret[1] = mix(matrixPalette[i+15],matrixPalette[i+16], x);
 	ret[2] = mix(matrixPalette[i+30],matrixPalette[i+31], x);
 	ret[3] = vec4(0,0,0,1);
