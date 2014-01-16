d9b09804910ae47ab4cecdd2cdd6fa2490fa748e
Aura Linden <aura@lindenlab.com>
2014-01-16 10:37:10 -0800

    Fixed syntax issue that was wrapped in #if that didn't get hit until we got to teamcity. oops.

--- indra/llcommon/llapp.cpp
+++ indra/llcommon/llapp.cpp
@@ -353,7 +353,7 @@ void LLApp::setupErrorHandling()
 														0,
 														google_breakpad::ExceptionHandler::HANDLER_ALL,
 														MiniDumpNormal, //Generate a 'normal' minidump.
-														strinize(wpipe_name).c_str(),
+														stringize(wpipe_name).c_str(),
 														NULL);  //No custom client info.
 			if (mExceptionHandler)
 			{
