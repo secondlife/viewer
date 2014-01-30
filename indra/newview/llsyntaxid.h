861d68e27d0c973179618b41903d164cfbc96f94
Ima Mechanique <ima.mechanique@secondlife.com>
2014-01-30 12:58:28 +0000

    Moved fetchKeywordsFileResponder class to end of file, so that LLSyntaxIdLSL is declared ahead of it for friend status.

--- indra/newview/llsyntaxid.h
+++ indra/newview/llsyntaxid.h
@@ -37,9 +37,71 @@
 #include "llhttpclient.h"
 #include "llviewerregion.h"
 
+
+/**
+ * @file llsyntaxid.h
+ * @brief Tracks the file needed to decorate the current sim's version of LSL.
+ */
+class LLSyntaxIdLSL
+{
+friend class fetchKeywordsFileResponder;
+public:
+
+
+protected:
+	LLViewerRegion*	region;
+
+
+private:
+	std::string		mCapabilityName;
+	std::string		mCapabilityURL;
+	std::string		mFileNameCurrent;
+	std::string		mFileNameDefault;
+	std::string		mFileNameNew;
+	ELLPath			mFilePath;
+	std::string		mFullFileSpec;
+	std::string		mSimulatorFeature;
+	LLUUID			mSyntaxIdCurrent;
+	LLUUID			mSyntaxIdNew;
+
+	static LLSD		sKeywordsXml;
+	static bool		sLoaded;
+
+
+public:
+	LLSyntaxIdLSL();
+
+	bool			checkSyntaxIdChanged();
+	std::string		getFileNameCurrent()	const { return mFileNameCurrent; }
+	ELLPath			getFilePath()			const { return mFilePath; }
+	std::string		getFileSpec()			const { return mFullFileSpec; }
+	LLSD			getKeywordsXML()		const { return sKeywordsXml; }
+	LLUUID			getSyntaxId()			const { return mSyntaxIdCurrent; }
+
+	void			initialise();
+	bool			isLoaded() { return sLoaded; }
+
+	static bool		isSupportedVersion(const LLSD& content);
+	static void		setKeywordsXml(const LLSD& content) { sKeywordsXml = content; }
+
+
+protected:
+	std::string		buildFileNameNew();
+	std::string		buildFullFileSpec();
+	void			fetchKeywordsFile();
+	void			loadDefaultKeywordsIntoLLSD(const std::string message);
+	bool			loadKeywordsIntoLLSD();
+	void			setSyntaxId(LLUUID SyntaxId) { mSyntaxIdCurrent = SyntaxId; }
+	void			setFileNameCurrent(std::string& name) { mFileNameCurrent = name; }
+	void			setFileNameDefault(std::string& name) { mFileNameDefault = name; }
+	void			setFileNameNew(std::string name) { mFileNameNew = name; }
+	void			setSimulatorFeatureName(const std::string& name) { mSimulatorFeature = name; }
+};
+
+
 /**
  * @file  llsyntaxid.h
- * @brief Handles responses for the LSLSyntax capability's get call.
+ * @brief Handles responses for the LSLSyntax capability's get call. Is a friend of LLSyntaxIdLSL
  */
 class fetchKeywordsFileResponder : public LLHTTPClient::Responder
 {
@@ -61,61 +123,4 @@ public:
 	 * @param content_ref	The LSL syntax file for the sim.
 	 */
 	void result(const LLSD& content_ref);
-};
-
-
-/**
- * @file llsyntaxid.h
- * @brief Tracks the file needed to decorate the current sim's version of LSL.
- */
-class LLSyntaxIdLSL
-{
-public:
-
-
-protected:
-	LLViewerRegion*	region;
-
-
-private:
-	std::string		mCapabilityName;
-	std::string		mCapabilityURL;
-	std::string		mFileNameCurrent;
-	std::string		mFileNameDefault;
-	std::string		mFileNameNew;
-	ELLPath			mFilePath;
-	std::string		mFullFileSpec;
-	std::string		mSimulatorFeature;
-	LLUUID			mSyntaxIdCurrent;
-	LLUUID			mSyntaxIdNew;
-
-	static LLSD		sKeywordsXml;
-
-
-public:
-	LLSyntaxIdLSL();
-
-	bool			checkSyntaxIdChanged();
-	std::string		getFileNameCurrent()	const { return mFileNameCurrent; }
-	ELLPath			getFilePath()			const { return mFilePath; }
-	std::string		getFileSpec()			const { return mFullFileSpec; }
-	LLSD			getKeywordsXML()		const { return sKeywordsXml; }
-	LLUUID			getSyntaxId()			const { return mSyntaxIdCurrent; }
-
-	void			initialise();
-
-	static void		setKeywordsXml(const LLSD& content) { sKeywordsXml = content; }
-
-
-protected:
-	std::string		buildFileNameNew();
-	std::string		buildFullFileSpec();
-	void			fetchKeywordsFile();
-	void			loadDefaultKeywordsIntoLLSD(const std::string message);
-	bool			loadKeywordsIntoLLSD();
-	void			setSyntaxId(LLUUID SyntaxId) { mSyntaxIdCurrent = SyntaxId; }
-	void			setFileNameCurrent(std::string& name) { mFileNameCurrent = name; }
-	void			setFileNameDefault(std::string& name) { mFileNameDefault = name; }
-	void			setFileNameNew(std::string name) { mFileNameNew = name; }
-	void			setSimulatorFeatureName(const std::string& name) { mSimulatorFeature = name; }
 };
