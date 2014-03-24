877f87a360d00b5d9cf6d9520102b8620727a7a9
Aura Linden <aura@lindenlab.com>
2014-03-24 15:19:03 -0700

    Old is the new new.

--- indra/llcrashlogger/llcrashlogger.cpp
+++ indra/llcrashlogger/llcrashlogger.cpp
@@ -218,7 +218,7 @@ void LLCrashLogger::gatherFiles()
 	{
 		// Figure out the filename of the second life log
 		LLCurl::setCAFile(gDirUtilp->getCAFile());
-		mFileMap["SecondLifeLog"] = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"SecondLife.log");
+		mFileMap["SecondLifeLog"] = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"SecondLife.old");
 		mFileMap["SettingsXml"] = gDirUtilp->getExpandedFilename(LL_PATH_USER_SETTINGS,"settings.xml");
 	}
 
