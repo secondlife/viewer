73ef75498692e7b7cecb4960f4e308061917d86e
Mnikolenko ProductEngine <mnikolenko@productengine.com>
2014-03-26 11:10:26 +0200

    MAINT-2376 FIXED Centre the map on friend's location

--- indra/newview/llavataractions.cpp
+++ indra/newview/llavataractions.cpp
@@ -373,7 +373,7 @@ void LLAvatarActions::showOnMap(const LLUUID& id)
 	}
 
 	gFloaterWorldMap->trackAvatar(id, av_name.getDisplayName());
-	LLFloaterReg::showInstance("world_map");
+	LLFloaterReg::showInstance("world_map", "center");
 }
 
 // static
