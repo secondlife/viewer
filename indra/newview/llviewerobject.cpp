75b93c15abc9ad9964fd5ef1e89335e50bdd6647
andreykproductengine <akleshchev@productengine.com>
2014-04-09 16:21:56 +0300

    MAINT-2245 FIXED Object's content doesn't display inside "Buy" dialog when prim is set for sale

--- indra/newview/llviewerobject.cpp
+++ indra/newview/llviewerobject.cpp
@@ -2563,8 +2563,8 @@ void LLViewerObject::dirtyInventory()
 		mInventory->clear(); // will deref and delete entries
 		delete mInventory;
 		mInventory = NULL;
-		mInventoryDirty = TRUE;
 	}
+	mInventoryDirty = TRUE;
 }
 
 void LLViewerObject::registerInventoryListener(LLVOInventoryListener* listener, void* user_data)
@@ -2601,12 +2601,15 @@ void LLViewerObject::clearInventoryListeners()
 
 void LLViewerObject::requestInventory()
 {
-	mInventoryDirty = FALSE;
+	if(mInventoryDirty && mInventory && !mInventoryCallbacks.empty())
+	{
+		mInventory->clear(); // will deref and delete entries
+		delete mInventory;
+		mInventory = NULL;
+		mInventoryDirty = FALSE; //since we are going to request it now
+	}
 	if(mInventory)
 	{
-		//mInventory->clear() // will deref and delete it
-		//delete mInventory;
-		//mInventory = NULL;
 		doInventoryCallback();
 	}
 	// throw away duplicate requests
