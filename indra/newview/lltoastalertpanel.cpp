83bd0625f058e0ca64deb3eaa4ccd87c09c4624f
Mnikolenko ProductEngine <mnikolenko@productengine.com>
2014-04-09 10:50:18 +0300

    MAINT-27 FIXED Prevent pasting non-ascii chars to avoid creating outfits with empty name.

--- indra/newview/lltoastalertpanel.cpp
+++ indra/newview/lltoastalertpanel.cpp
@@ -267,6 +267,11 @@ LLToastAlertPanel::LLToastAlertPanel( LLNotificationPtr notification, bool modal
 			mLineEditor->setMaxTextChars(edit_text_max_chars);
 			mLineEditor->setText(edit_text_contents);
 
+			if("SaveOutfitAs"  == mNotification->getName())
+			{
+				mLineEditor->setPrevalidate(&LLTextValidate::validateASCII);
+			}
+
 			// decrease limit of line editor of teleport offer dialog to avoid truncation of
 			// location URL in invitation message, see EXT-6891
 			if ("OfferTeleport" == mNotification->getName())
