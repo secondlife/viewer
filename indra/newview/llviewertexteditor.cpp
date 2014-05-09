06ac02fc2c548972e3024af24857c4daf8ffb3ff
simon <none@none>
2014-05-09 11:10:33 -0700

    MAINT-3981 : Notecard passed around crashes viewer when opened

--- indra/newview/llviewertexteditor.cpp
+++ indra/newview/llviewertexteditor.cpp
@@ -543,7 +543,7 @@ LLUIImagePtr LLEmbeddedItems::getItemImage(llwchar ext_char) const
 			case LLAssetType::AT_ANIMATION:		img_name = "Inv_Animation";	break;
 			case LLAssetType::AT_GESTURE:		img_name = "Inv_Gesture";	break;
 			case LLAssetType::AT_MESH:          img_name = "Inv_Mesh";	    break;
-			default: llassert(0);
+			default:                        	img_name = "Inv_Invalid";   break; // use the Inv_Invalid icon for undefined object types (see MAINT-3981)
 		}
 
 		return LLUI::getUIImage(img_name);
