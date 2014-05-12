b36458a98fb3aeb63d2aca5e3249c7aa508192c8
andreykproductengine <akleshchev@productengine.com>
2014-05-12 20:01:20 +0300

    MAINT-1734 FIXED Avatar chooser panel is empty the session after an avatar is chosen.

--- indra/newview/llmediactrl.cpp
+++ indra/newview/llmediactrl.cpp
@@ -504,6 +504,16 @@ void LLMediaCtrl::navigateForward()
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+void LLMediaCtrl::navigateStop()
+{
+	if (mMediaSource && mMediaSource->hasMedia())
+	{
+		mMediaSource->getMediaPlugin()->browse_stop();
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
 bool LLMediaCtrl::canNavigateBack()
 {
 	if (mMediaSource)
