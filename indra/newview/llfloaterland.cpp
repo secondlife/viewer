be64891922f5a5dadcebd156dab5fb1879cc9b1b
dolphin <dolphin@lindenlab.com>
2014-05-22 10:09:25 -0700

    Fixed merge

--- indra/newview/llfloaterland.cpp
+++ indra/newview/llfloaterland.cpp
@@ -2247,6 +2247,7 @@ public:
 
 	static void onCommitPublicAccess(LLUICtrl* ctrl, void *userdata);
 	static void onCommitAny(LLUICtrl* ctrl, void *userdata);
+	static void onCommitGroupCheck(LLUICtrl* ctrl, void *userdata);
 	static void onClickRemoveAccess(void*);
 	static void onClickRemoveBanned(void*);
 
