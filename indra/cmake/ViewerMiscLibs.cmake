37cd872f40ffeb90d03ea5b0f9025670e920da43
Oz Linden <oz@lindenlab.com>
2014-12-15 13:00:47 -0500

    include libuuid and fontconfig only on linux

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -3,8 +3,10 @@ include(Prebuilt)
 
 if (NOT USESYSTEMLIBS)
   use_prebuilt_binary(libhunspell)
-  use_prebuilt_binary(libuuid)
   use_prebuilt_binary(slvoice)
-  use_prebuilt_binary(fontconfig)
+  if (LINUX)
+    use_prebuilt_binary(libuuid)
+    use_prebuilt_binary(fontconfig)
+  endif (LINUX)
 endif(NOT USESYSTEMLIBS)
 
