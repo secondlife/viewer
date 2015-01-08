355d00685137aa0c42fdd93853096ac718a474b2
Nat Goodspeed <nat@lindenlab.com>
2015-01-08 16:48:29 -0500

    Rationalize use of AUTOBUILD environment variable across languages. Ensure that AUTOBUILD is always in native path syntax on every platform. Remove local build.sh logic to infer AUTOBUILD: either the generic build.sh has set it properly, or scream for help. Since cygwin bash scripts need a cygwin-syntax path to autobuild, convert to a local 'autobuild' variable using shell_path. Remove kludges in other languages involving cygdrive twiddling.

--- indra/cmake/FindAutobuild.cmake
+++ indra/cmake/FindAutobuild.cmake
@@ -4,7 +4,7 @@
 #
 # Output variables:
 #
-#   AUTOBUILD_EXECUTABLE - path to autobuild or pautobuild executable
+#   AUTOBUILD_EXECUTABLE - path to autobuild executable
 
 
 
@@ -13,12 +13,9 @@ IF (NOT AUTOBUILD_EXECUTABLE)
   # If cmake was executed by autobuild, autobuild will have set the AUTOBUILD env var
   IF (DEFINED ENV{AUTOBUILD})
     SET(AUTOBUILD_EXECUTABLE $ENV{AUTOBUILD})
-    # In case $AUTOBUILD is a cygwin path, fix it back to Windows style
-    STRING(REGEX REPLACE "^/cygdrive/(.)/" "\\1:/" AUTOBUILD_EXECUTABLE
-           "${AUTOBUILD_EXECUTABLE}")
   ELSE (DEFINED ENV{AUTOBUILD})
     IF(WIN32)
-      SET(AUTOBUILD_EXE_NAMES autobuild.cmd autobuild.exe)
+      SET(AUTOBUILD_EXE_NAMES autobuild.exe autobuild.cmd)
     ELSE(WIN32)
       SET(AUTOBUILD_EXE_NAMES autobuild)
     ENDIF(WIN32)
@@ -28,21 +25,19 @@ IF (NOT AUTOBUILD_EXECUTABLE)
       AUTOBUILD_EXECUTABLE 
       NAMES ${AUTOBUILD_EXE_NAMES}
       PATHS 
-    ENV PATH
-    ${CMAKE_SOURCE_DIR}/.. 
-    ${CMAKE_SOURCE_DIR}/../..
-    ${CMAKE_SOURCE_DIR}/../../..
+        ENV PATH
+        ${CMAKE_SOURCE_DIR}/.. 
+        ${CMAKE_SOURCE_DIR}/../..
+        ${CMAKE_SOURCE_DIR}/../../..
       PATH_SUFFIXES "/autobuild/bin/"
     )
   ENDIF (DEFINED ENV{AUTOBUILD})
 
-  IF (AUTOBUILD_EXECUTABLE)
-    GET_FILENAME_COMPONENT(_autobuild_name ${AUTOBUILD_EXECUTABLE} NAME_WE)
-  ELSE (AUTOBUILD_EXECUTABLE)
+  IF (NOT AUTOBUILD_EXECUTABLE)
     IF (AUTOBUILD_FIND_REQUIRED)
       MESSAGE(FATAL_ERROR "Could not find autobuild executable")
     ENDIF (AUTOBUILD_FIND_REQUIRED)
-  ENDIF (AUTOBUILD_EXECUTABLE)
+  ENDIF (NOT AUTOBUILD_EXECUTABLE)
 
   MARK_AS_ADVANCED(AUTOBUILD_EXECUTABLE)
 ENDIF (NOT AUTOBUILD_EXECUTABLE)
