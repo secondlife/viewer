0fe901d67ea7c1f34d3f415a1537bfa676a79fef
callum_linden <none@none>
2015-03-25 15:42:35 -0700

    Point to initial pass at a CEF package in autobuild and make sure autobuild configure picks it up

--- indra/cmake/CEFPlugin.cmake
+++ indra/cmake/CEFPlugin.cmake
@@ -6,6 +6,7 @@ if (USESYSTEMLIBS)
     set(CEFPLUGIN OFF CACHE BOOL
         "CEFPLUGIN support for the llplugin/llmedia test apps.")
 else (USESYSTEMLIBS)
+    use_prebuilt_binary(cef)
     set(CEFPLUGIN ON CACHE BOOL
         "CEFPLUGIN support for the llplugin/llmedia test apps.")
 endif (USESYSTEMLIBS)
