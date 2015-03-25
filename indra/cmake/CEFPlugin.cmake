d368babe7cc84ac6d8532e9d2438620cfa6d172d
callum_linden <none@none>
2015-03-25 16:16:51 -0700

    Make VS2013 look inside include/cef folder for headers

--- indra/cmake/CEFPlugin.cmake
+++ indra/cmake/CEFPlugin.cmake
@@ -9,6 +9,7 @@ else (USESYSTEMLIBS)
     use_prebuilt_binary(cef)
     set(CEFPLUGIN ON CACHE BOOL
         "CEFPLUGIN support for the llplugin/llmedia test apps.")
+        set(CEF_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/cef)
 endif (USESYSTEMLIBS)
 
 if (WINDOWS)
