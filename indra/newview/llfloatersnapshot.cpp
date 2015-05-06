2bedf20aa9e03bc4a18c3a1460040b1dd7638ed9
pavelkproductengine <none@none>
2015-05-06 16:55:32 +0300

    Fixed Mac build after tools update - stripped unsused symbols

--- indra/newview/llfloatersnapshot.cpp
+++ indra/newview/llfloatersnapshot.cpp
@@ -56,7 +56,6 @@ LLSnapshotFloaterView* gSnapshotFloaterView = NULL;
 
 const F32 AUTO_SNAPSHOT_TIME_DELAY = 1.f;
 
-const S32 MAX_POSTCARD_DATASIZE = 1024 * 1024; // one megabyte
 const S32 MAX_TEXTURE_SIZE = 512 ; //max upload texture size 512 * 512
 
 static LLDefaultChildRegistry::Register<LLSnapshotFloaterView> r("snapshot_floater_view");
