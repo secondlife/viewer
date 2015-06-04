fda0c3eb590f39fe04347da0182ba38ab87e8bc2
Mnikolenko ProductEngine <mnikolenko@productengine.com>
2015-06-04 12:50:16 +0300

    MAINT-1693 FIXED Building Block Type is changed to Torus after cancel changing of texture for sculpted object

--- indra/newview/llpanelobject.cpp
+++ indra/newview/llpanelobject.cpp
@@ -2000,7 +2000,11 @@ void LLPanelObject::onCancelSculpt(const LLSD& data)
 	LLTextureCtrl* mTextureCtrl = getChild<LLTextureCtrl>("sculpt texture control");
 	if(!mTextureCtrl)
 		return;
-	
+
+	if(mSculptTextureRevert == LLUUID::null)
+	{
+		mSculptTextureRevert = LLUUID(SCULPT_DEFAULT_TEXTURE);
+	}
 	mTextureCtrl->setImageAssetID(mSculptTextureRevert);
 	
 	sendSculpt();
