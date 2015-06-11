ac88d58ff5fa273ae0dc71fdf24f6ad03d925d4f
callum_linden <none@none>
2015-06-10 18:32:41 -0700

    Changes to let CMake generate determine where the build files for OS X CEF media plugin are

--- indra/cmake/CEFPlugin.cmake
+++ indra/cmake/CEFPlugin.cmake
@@ -6,7 +6,7 @@ if (USESYSTEMLIBS)
     set(CEFPLUGIN OFF CACHE BOOL
         "CEFPLUGIN support for the llplugin/llmedia test apps.")
 else (USESYSTEMLIBS)
-    use_prebuilt_binary(cef)
+    use_prebuilt_binary(llceflib)
     set(CEFPLUGIN ON CACHE BOOL
         "CEFPLUGIN support for the llplugin/llmedia test apps.")
         set(CEF_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/cef)
@@ -19,11 +19,22 @@ if (WINDOWS)
         llceflib.lib
     )
 elseif (DARWIN)
+    FIND_LIBRARY(APPKIT_LIBRARY AppKit)
+    if (NOT APPKIT_LIBRARY)
+        message(FATAL_ERROR "AppKit not found")
+    endif()
+
+    FIND_LIBRARY(CEF_LIBRARY "Chromium Embedded Framework" ${ARCH_PREBUILT_DIRS_RELEASE})
+    if (NOT CEF_LIBRARY)
+        message(FATAL_ERROR "CEF not found")
+    endif()
+
     set(CEF_PLUGIN_LIBRARIES
         ${ARCH_PREBUILT_DIRS_RELEASE}/libcef_dll_wrapper.a
         ${ARCH_PREBUILT_DIRS_RELEASE}/libLLCefLib.a
-
-        #${ARCH_PREBUILT_DIRS_RELEASE}/libQtWebKit.4.dylib
+        ${APPKIT_LIBRARY}
+        ${CEF_LIBRARY}
        )
+
 elseif (LINUX)
 endif (WINDOWS)
