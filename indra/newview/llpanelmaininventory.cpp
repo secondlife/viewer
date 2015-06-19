e998f1427ff1cd0fbdfb7f69b39720e19ec0a7d0
andreykproductengine <akleshchev@productengine.com>
2015-06-19 16:21:50 +0300

    MAINT-5274 missing inventory count

--- indra/newview/llpanelmaininventory.cpp
+++ indra/newview/llpanelmaininventory.cpp
@@ -604,7 +604,7 @@ void LLPanelMainInventory::updateItemcountText()
 	}
 	else
 	{
-		text = getString("ItemcountUnknown");
+		text = getString("ItemcountUnknown", string_args);
 	}
 	
     mCounterCtrl->setValue(text);
