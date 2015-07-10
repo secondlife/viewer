f9929bb62eb79f80acd49c74f83edee0abc1ab66
andreykproductengine <akleshchev@productengine.com>
2015-07-10 15:55:25 +0300

    MAINT-5363 FIXED Selecting an inventory item plus an inventory link displays delete menu twice but both delete options are greyed out.

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -863,15 +863,7 @@ void LLInvFVBridge::addDeleteContextMenuOptions(menuentry_vec_t &items,
 		return;
 	}
 
-	// "Remove link" and "Delete" are the same operation.
-	if (obj && obj->getIsLinkType() && !get_is_item_worn(mUUID))
-	{
-		items.push_back(std::string("Remove Link"));
-	}
-	else
-	{
-		items.push_back(std::string("Delete"));
-	}
+	items.push_back(std::string("Delete"));
 
 	if (!isItemRemovable())
 	{
