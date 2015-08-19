be7ed4acd4a75915bfc04e65ed385a179d74b646
Rider Linden <rider@lindenlab.com>
2015-08-19 15:38:12 -0700

    Previous try didn't work.  What about this?

--- indra/cmake/LLAppearance.cmake
+++ indra/cmake/LLAppearance.cmake
@@ -1,14 +1,25 @@
 # -*- cmake -*-
 
 include(Variables)
+include(Boost)
+include(LLMessage)
+include(LLCoreHttp)
 
 set(LLAPPEARANCE_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llappearance
+    llcorehttp
+    llmessage
+    ${BOOST_COROUTINE_LIBRARY}
+    ${BOOST_CONTEXT_LIBRARY}
     )
 
 if (BUILD_HEADLESS)
   set(LLAPPEARANCE_HEADLESS_LIBRARIES
     llappearanceheadless
+    llcorehttp
+    llmessage
+    ${BOOST_COROUTINE_LIBRARY}
+    ${BOOST_CONTEXT_LIBRARY}
     )
 endif (BUILD_HEADLESS)
 
--- indra/cmake/LLAppearanceUtility.cmake
+++ indra/cmake/LLAppearanceUtility.cmake
@@ -8,14 +8,6 @@ if (INSTALL_PROPRIETARY)
         use_prebuilt_binary(llappearance_utility)
         set(LLAPPEARANCEUTILITY_SRC_DIR ${LIBS_PREBUILT_DIR}/llappearanceutility/src)
         set(LLAPPEARANCEUTILITY_BIN_DIR ${CMAKE_BINARY_DIR}/llappearanceutility)
-        target_link_libraries(appearance-utility-bin
-          ${BOOST_COROUTINE_LIBRARY}
-          ${BOOST_CONTEXT_LIBRARY}
-          ${BOOST_SYSTEM_LIBRARY})
-        target_link_libraries(appearance-utility-headless-bin
-          ${BOOST_COROUTINE_LIBRARY}
-          ${BOOST_CONTEXT_LIBRARY}
-          ${BOOST_SYSTEM_LIBRARY})
     endif (LINUX)
 endif (INSTALL_PROPRIETARY)
 
