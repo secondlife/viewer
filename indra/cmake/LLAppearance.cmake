19afd182346354474e2e06a6bc2119096cfe86c3
Rider Linden <rider@lindenlab.com>
2015-08-19 16:04:57 -0700

    Try disabling LLAppearanceUtility.cmake just for the moment.

--- indra/cmake/LLAppearance.cmake
+++ indra/cmake/LLAppearance.cmake
@@ -7,19 +7,11 @@ include(LLCoreHttp)
 
 set(LLAPPEARANCE_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llappearance
-    llcorehttp
-    llmessage
-    ${BOOST_COROUTINE_LIBRARY}
-    ${BOOST_CONTEXT_LIBRARY}
     )
 
 if (BUILD_HEADLESS)
   set(LLAPPEARANCE_HEADLESS_LIBRARIES
     llappearanceheadless
-    llcorehttp
-    llmessage
-    ${BOOST_COROUTINE_LIBRARY}
-    ${BOOST_CONTEXT_LIBRARY}
     )
 endif (BUILD_HEADLESS)
 
