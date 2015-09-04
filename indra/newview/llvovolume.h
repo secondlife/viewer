67757ddd5b1196ebab004095e202d9dc012f520f
AndreyL ProductEngine <andreylproductengine@lindenlab.com>
2015-09-04 21:06:22 +0300

    MAINT-5416 FIXED Cannot right-click a rigged mesh that's worn
    --HG--
    branch : maint-5416

--- indra/newview/llvovolume.h
+++ indra/newview/llvovolume.h
@@ -140,6 +140,7 @@ public:
 	/*virtual*/ BOOL lineSegmentIntersect(const LLVector4a& start, const LLVector4a& end, 
 										  S32 face = -1,                        // which face to check, -1 = ALL_SIDES
 										  BOOL pick_transparent = FALSE,
+										  BOOL pick_rigged = FALSE,
 										  S32* face_hit = NULL,                 // which face was hit
 										  LLVector4a* intersection = NULL,       // return the intersection point
 										  LLVector2* tex_coord = NULL,          // return the texture coordinates of the intersection point
@@ -312,7 +313,7 @@ public:
 	
 
 	//rigged volume update (for raycasting)
-	void updateRiggedVolume();
+	void updateRiggedVolume(bool force_update = false);
 	LLRiggedVolume* getRiggedVolume();
 
 	//returns true if volume should be treated as a rigged volume
