2d2c90157dbc75c4f1b4a8ac35ae67e005bc98b4
Rider Linden <rider@lindenlab.com>
2015-09-23 15:48:06 -0700

    Remove ares dependency from build.

--- indra/cmake/LLCoreHttp.cmake
+++ indra/cmake/LLCoreHttp.cmake
@@ -1,13 +1,11 @@
 # -*- cmake -*-
 
-include(CARes)
 include(CURL)
 include(OpenSSL)
 include(Boost)
 
 set(LLCOREHTTP_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llcorehttp
-    ${CARES_INCLUDE_DIRS}
     ${CURL_INCLUDE_DIRS}
     ${OPENSSL_INCLUDE_DIRS}
     ${BOOST_INCLUDE_DIRS}
