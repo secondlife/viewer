1a7b2d3d388d12a16fab5e585f06ab0574136f3d
Brad Payne (Vir Linden) <vir@lindenlab.com>
2015-11-04 09:33:43 -0500

    SL-114 WIP - show skin weights checkbox can get out of sync with corresponding view option.

--- indra/newview/llfloatermodelpreview.cpp
+++ indra/newview/llfloatermodelpreview.cpp
@@ -3578,6 +3578,7 @@ BOOL LLModelPreview::render()
 			fmp->enableViewOption("show_skin_weight");
 			fmp->setViewOptionEnabled("show_joint_positions", skin_weight);	
 			mFMP->childEnable("upload_skin");
+			mFMP->childSetValue("show_skin_weight", skin_weight);
 		}
 	}
 	else
