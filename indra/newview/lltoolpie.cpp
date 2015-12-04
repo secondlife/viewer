9ab1cb7ae003687ae90ec0a920a0f5932cc4acc7
AndreyL ProductEngine <alihatskiy@productengine.com>
2015-12-04 20:51:11 +0200

    MAINT-5892 Argument fix

--- indra/newview/lltoolpie.cpp
+++ indra/newview/lltoolpie.cpp
@@ -122,7 +122,7 @@ BOOL LLToolPie::handleMouseDown(S32 x, S32 y, MASK mask)
 BOOL LLToolPie::handleRightMouseDown(S32 x, S32 y, MASK mask)
 {
 	// don't pick transparent so users can't "pay" transparent objects
-	mPick = gViewerWindow->pickImmediate(x, y, FALSE, TRUE);
+	mPick = gViewerWindow->pickImmediate(x, y, /*BOOL pick_transparent*/ FALSE, /*BOOL pick_rigged*/ TRUE, /*BOOL pick_particle*/ TRUE);
 	mPick.mKeyMask = mask;
 
 	// claim not handled so UI focus stays same
