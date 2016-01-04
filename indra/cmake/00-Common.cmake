f26103e56fe0979bd38683c6c70ac6d32b6b08fe
Brad Payne (Vir Linden) <vir@lindenlab.com>
2016-01-04 11:30:07 -0500

    possible fix for linux build failure

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -137,11 +137,11 @@ if (LINUX)
   # This is pure rubbish; I wish there was another way.
   #
   if(${CXX_VERSION_NUMBER} LESS 420)
-    set(CMAKE_CXX_FLAGS "-Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-unused-function ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS "-Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-unused-function -Wno-unused-local-typedef ${CMAKE_CXX_FLAGS}")
   endif (${CXX_VERSION_NUMBER} LESS 420)
 
   if(${CXX_VERSION_NUMBER} GREATER 459)
-    set(CMAKE_CXX_FLAGS "-Wno-deprecated -Wno-unused-but-set-variable -Wno-unused-variable ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS "-Wno-deprecated -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unused-local-typedef ${CMAKE_CXX_FLAGS}")
   endif (${CXX_VERSION_NUMBER} GREATER 459)
 
   # gcc 4.3 and above don't like the LL boost and also
