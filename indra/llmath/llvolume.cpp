ba1e3e4f86f969932f9bdc077b716a7a647d6fb7
Brad Payne (Vir Linden) <vir@lindenlab.com>
2016-03-31 10:53:26 -0400

    SL-352 - another case where legacy content revealed a bug in weight remapping.

--- indra/llmath/llvolume.cpp
+++ indra/llmath/llvolume.cpp
@@ -4594,6 +4594,7 @@ LLVolumeFace::LLVolumeFace(const LLVolumeFace& src)
 	mTexCoords(NULL),
 	mIndices(NULL),
 	mWeights(NULL),
+    mWeightsRemapped(FALSE),
 	mOctree(NULL)
 { 
 	mExtents = (LLVector4a*) ll_aligned_malloc_16(sizeof(LLVector4a)*3);
@@ -4665,6 +4666,7 @@ LLVolumeFace& LLVolumeFace::operator=(const LLVolumeFace& src)
 			ll_aligned_free_16(mWeights);
 			mWeights = NULL;
 		}
+        mWeightsRemapped = src.mWeightsRemapped;
 	}
 
 	if (mNumIndices)
