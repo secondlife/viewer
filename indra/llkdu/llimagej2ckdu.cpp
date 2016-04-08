6f4b709ab646f3f6483f96e5ee9dce6302ae07d3
Glenn Glazer <coyot@lindenlab.com>
2016-04-08 09:57:18 -0700

    fix paren

--- indra/llkdu/llimagej2ckdu.cpp
+++ indra/llkdu/llimagej2ckdu.cpp
@@ -1034,7 +1034,7 @@ all necessary level shifting, type conversion, rounding and truncation. */
 				val = (kdu_int32)(sp->fval*scale16);
 				val = (val+128)>>8; // May be faster than true rounding
 				val += 128;
-				if (val & (((0xffffffffU)<<8))
+				if (val & ((0xffffffffU)<<8))
 				{
 					val = (val < 0 ? 0 : 255);
 				}
