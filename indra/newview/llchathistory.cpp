1241558dbf7e21a39487c16f940aed1db689ef84
andreykproductengine <akleshchev@productengine.com>
2016-04-20 19:40:19 +0300

    MAINT-2129 "Block" button doesn't become disabled in remote object inspector after object has been added to block list

--- indra/newview/llchathistory.cpp
+++ indra/newview/llchathistory.cpp
@@ -169,6 +169,16 @@ public:
 
 	}
 
+    bool onObjectIconContextMenuItemEnabled(const LLSD& userdata)
+    {
+        std::string level = userdata.asString();
+        if (level == "is_blocked")
+        {
+            return !LLMuteList::getInstance()->isMuted(getAvatarId(), mFrom, LLMute::flagTextChat);
+        }
+        return false;
+    }
+
 	void onAvatarIconContextMenuItemClicked(const LLSD& userdata)
 	{
 		std::string level = userdata.asString();
@@ -275,6 +285,7 @@ public:
 		registrar.add("AvatarIcon.Action", boost::bind(&LLChatHistoryHeader::onAvatarIconContextMenuItemClicked, this, _2));
 		registrar_enable.add("AvatarIcon.Check", boost::bind(&LLChatHistoryHeader::onAvatarIconContextMenuItemChecked, this, _2));
 		registrar.add("ObjectIcon.Action", boost::bind(&LLChatHistoryHeader::onObjectIconContextMenuItemClicked, this, _2));
+		registrar_enable.add("ObjectIcon.Enable", boost::bind(&LLChatHistoryHeader::onObjectIconContextMenuItemEnabled, this, _2));
 
 		LLMenuGL* menu = LLUICtrlFactory::getInstance()->createFromFile<LLMenuGL>("menu_avatar_icon.xml", gMenuHolder, LLViewerMenuHolderGL::child_registry_t::instance());
 		mPopupMenuHandleAvatar = menu->getHandle();
