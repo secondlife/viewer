8edfdc617df6e282570521a9076d077699e632e5
AndreyL ProductEngine <alihatskiy@productengine.com>
2016-05-23 02:23:03 +0300

    MAINT-6424 Disable the delete button for empty graphics presets list Fixed related notifications after improper merge as well

--- indra/newview/llfloaterdeleteprefpreset.cpp
+++ indra/newview/llfloaterdeleteprefpreset.cpp
@@ -53,6 +53,8 @@ BOOL LLFloaterDeletePrefPreset::postBuild()
 	getChild<LLButton>("cancel")->setCommitCallback(boost::bind(&LLFloaterDeletePrefPreset::onBtnCancel, this));
 	LLPresetsManager::instance().setPresetListChangeCallback(boost::bind(&LLFloaterDeletePrefPreset::onPresetsListChange, this));
 
+	onPresetsListChange(); // ensure that delete button is disabled when the list is empty
+
 	return TRUE;
 }
 
