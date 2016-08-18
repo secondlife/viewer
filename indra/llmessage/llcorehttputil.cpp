7df153e352ce50ded382df020cc3696b8c1b9325
Oz Linden <oz@lindenlab.com>
2016-08-18 18:13:41 -0400

    further tone down http error response logging; not all http error responses are unexpected or will cause problems

--- indra/llmessage/llcorehttputil.cpp
+++ indra/llmessage/llcorehttputil.cpp
@@ -84,7 +84,7 @@ void logMessageSuccess(std::string logAuth, std::string url, std::string message
 
 void logMessageFail(std::string logAuth, std::string url, std::string message)
 {
-    LL_WARNS() << logAuth << " Failure '" << message << "' for " << url << LL_ENDL;
+    LL_INFOS() << logAuth << " Possible failure '" << message << "' for " << url << LL_ENDL;
 }
 
 //=========================================================================
@@ -279,8 +279,8 @@ void HttpCoroHandler::onCompleted(LLCore::HttpHandle handle, LLCore::HttpRespons
         result = LLSD::emptyMap();
         LLCore::HttpStatus::type_enum_t errType = status.getType();
 
-        LL_WARNS()
-            << "Error[" << status.toTerseString() << "] cannot access url '" << response->getRequestURL()
+        LL_INFOS()
+            << "Possible failure [" << status.toTerseString() << "] cannot access url '" << response->getRequestURL()
             << "' because " << status.toString()
             << LL_ENDL;
         if ((errType >= 400) && (errType < 500))
