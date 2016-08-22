2699ef356aae4f31246ab6ccb6613676a2e2cc0f
Glenn Glazer <coyot@lindenlab.com>
2016-08-22 09:36:00 -0700

    SL-323: ripping out old code completed

--- indra/newview/tests/lllogininstance_test.cpp
+++ indra/newview/tests/lllogininstance_test.cpp
@@ -186,6 +186,13 @@ void LLUIColorTable::saveUserSettings(void)const {}
 #include "../llversioninfo.h"
 const std::string &LLVersionInfo::getVersion() { return VIEWERLOGIN_VERSION; }
 const std::string &LLVersionInfo::getChannel() { return VIEWERLOGIN_CHANNEL; }
+const int MD5HEX_STR_SIZE = 33;  // char hex[MD5HEX_STR_SIZE]; with null
+
+bool llHashedUniqueID(unsigned char* id) 
+{
+	memcpy( id, "66666666666666666666666666666666", MD5HEX_STR_SIZE );
+	return true;
+}
 
 //-----------------------------------------------------------------------------
 #include "../llappviewer.h"
