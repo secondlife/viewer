fef70f8f1c75c724871216b0af672c569b56334b
Mnikolenko Productengine <mnikolenko@productengine.com>
2016-11-16 15:54:00 +0200

    MAINT-6872 Account for CTRL+0 zoom when mesh LOD is calculated

--- indra/newview/llvovolume.h
+++ indra/newview/llvovolume.h
@@ -327,7 +327,7 @@ public:
 	void clearRiggedVolume();
 
 protected:
-	S32	computeLODDetail(F32	distance, F32 radius);
+	S32	computeLODDetail(F32	distance, F32 radius, F32 lod_factor);
 	BOOL calcLOD();
 	LLFace* addFace(S32 face_index);
 	void updateTEData();
