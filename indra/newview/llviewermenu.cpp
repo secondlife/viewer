d91584ed708e87be1906a23772c56b31a34164dc
Mnikolenko Productengine <mnikolenko@productengine.com>
2016-11-29 11:46:51 +0200

    MAINT-6936 Black screen following toggle to wireframe in photo dialogue.

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -1212,14 +1212,15 @@ class LLAdvancedToggleWireframe : public view_listener_t
 	bool handleEvent(const LLSD& userdata)
 	{
 		gUseWireframe = !(gUseWireframe);
+		gWindowResized = TRUE;
+
+		LLPipeline::updateRenderDeferred();
 
 		if (gUseWireframe)
 		{
 			gInitialDeferredModeForWireframe = LLPipeline::sRenderDeferred;
 		}
 
-		gWindowResized = TRUE;
-		LLPipeline::updateRenderDeferred();
 		gPipeline.resetVertexBuffers();
 
 		if (!gUseWireframe && !gInitialDeferredModeForWireframe && LLPipeline::sRenderDeferred != gInitialDeferredModeForWireframe && gPipeline.isInit())
