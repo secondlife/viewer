3a8aa2d13d4d362847ce2ec16376907e7f562500
Oz Linden <oz@lindenlab.com>
2016-12-02 10:59:06 -0500

    add viewer channel and version to the beginning of the About box Licenses information

--- indra/cmake/BuildPackagesInfo.cmake
+++ indra/cmake/BuildPackagesInfo.cmake
@@ -6,5 +6,5 @@ add_custom_command(OUTPUT packages-info.txt
   COMMENT Generating packages-info.txt for the about box
   MAIN_DEPENDENCY ${CMAKE_SOURCE_DIR}/../autobuild.xml
   DEPENDS ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py
-  COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py > packages-info.txt
+  COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py "${VIEWER_CHANNEL}" "${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}" > packages-info.txt
   )
