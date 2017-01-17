b94aa751c93b2c9bf9a9567946fe388a7c475d77
andreykproductengine <akleshchev@productengine.com>
2017-01-17 20:11:00 +0200

    MAINT-6959 Ability to set custom folders for uploading items

--- indra/newview/llinventorypanel.h
+++ indra/newview/llinventorypanel.h
@@ -201,6 +201,7 @@ public:
 	void doToSelected(const LLSD& userdata);
 	void doCreate(const LLSD& userdata);
 	bool beginIMSession();
+	void fileUploadLocation(const LLSD& userdata);
 	bool attachObject(const LLSD& userdata);
 	static void idle(void* user_data);
 
