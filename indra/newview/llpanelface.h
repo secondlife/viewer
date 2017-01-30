bd3815699cf107cd79a7d0db0d8298b356652add
andreykproductengine <akleshchev@productengine.com>
2017-01-30 20:02:59 +0200

    MAINT-3216 Fixed "Vertical scale" does not change "Repeats per meter" on "Tab"

--- indra/newview/llpanelface.h
+++ indra/newview/llpanelface.h
@@ -152,7 +152,8 @@ protected:
 	// Make UI reflect state of currently selected material (refresh)
 	// and UI mode (e.g. editing normal map v diffuse map)
 	//
-	void updateUI();
+	// @param force_set_values forces spinners to set value even if they are focused
+	void updateUI(bool force_set_values = false);
 
 	// Convenience func to determine if all faces in selection have
 	// identical planar texgen settings during edits
