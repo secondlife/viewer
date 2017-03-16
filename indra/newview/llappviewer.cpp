4992654682255a6cace1c5ee86b4b8bf28fde15d
Glenn Glazer <coyot@lindenlab.com>
2017-03-16 15:45:52 -0700

    merge cleanup

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -5500,20 +5500,6 @@ void LLAppViewer::handleLoginComplete()
 	mSavePerAccountSettings=true;
 }
 
-/**
-* Check if user is running a new version of the viewer.
-* Display the Release Notes if it's not overriden by the "UpdaterShowReleaseNotes" setting.
-*/
-void LLAppViewer::showReleaseNotesIfRequired()
-{
-	if (LLVersionInfo::getChannelAndVersion() != gLastRunVersion
-		&& gSavedSettings.getBOOL("UpdaterShowReleaseNotes")
-		&& !gSavedSettings.getBOOL("FirstLoginThisInstall"))
-	{
-		LLSD info(getViewerInfo());
-		LLWeb::loadURLInternal(info["VIEWER_RELEASE_NOTES_URL"]);
-	}
-}
 //virtual
 void LLAppViewer::setMasterSystemAudioMute(bool mute)
 {
