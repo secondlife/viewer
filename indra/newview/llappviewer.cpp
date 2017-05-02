e4996c5d8182a0ef24f0eb445db82121fe5733d7
Nat Goodspeed <nat@lindenlab.com>
2017-05-02 14:39:59 -0400

    DRTVWR-418, MAINT-6996: clarify divide-by-1024 (not shift-right 10)

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3745,7 +3745,7 @@ void LLAppViewer::handleViewerCrash()
 	}
 
 	gDebugInfo["Dynamic"]["SessionLength"] = F32(LLFrameTimer::getElapsedSeconds());
-	gDebugInfo["Dynamic"]["RAMInfo"]["Allocated"] = LLSD::Integer(LLMemory::getCurrentRSS() >> 10);
+	gDebugInfo["Dynamic"]["RAMInfo"]["Allocated"] = LLSD::Integer(LLMemory::getCurrentRSS() / 1024);
 
 	if(gLogoutInProgress)
 	{
