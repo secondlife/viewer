999b0b47b59cb6be6f229066825bdec88f00304e
Brad Payne (Vir Linden) <vir@lindenlab.com>
2017-06-29 14:15:58 +0100

    SL-722 - crash fix

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -3320,6 +3320,10 @@ bool LLVOVolume::canBeAnimatedObject() const
     {
         return false;
     }
+	if (!getVolume())
+	{
+		return false;
+	}
 	const LLMeshSkinInfo* skin = gMeshRepo.getSkinInfo(getVolume()->getParams().getSculptID(), this);
     if (!skin)
     {
