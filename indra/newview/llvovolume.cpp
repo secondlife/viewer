59976ece36a304421135650e6bc3b4f70db6f64c
Brad Payne (Vir Linden) <vir@lindenlab.com>
2017-07-06 18:50:54 +0100

    SL-722 - crash fix when leaving region

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -3414,8 +3414,11 @@ void LLVOVolume::updateAnimatedObjectState(LLViewerObject *old_parent, LLViewerO
     }
     if (old_volp && old_volp->isAnimatedObject())
     {
-        // W have been removed from an animated object, need to do cleanup.
-        old_volp->getControlAvatar()->removeAttachmentOverridesForObject(this);
+        if (old_volp->getControlAvatar())
+        {
+            // We have been removed from an animated object, need to do cleanup.
+            old_volp->getControlAvatar()->removeAttachmentOverridesForObject(this);
+        }
     }
     
     if (old_volp)
