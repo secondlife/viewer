a4d1600a79ea9118bcbe854a55f7491d738b844c
Brad Payne (Vir Linden) <vir@lindenlab.com>
2017-07-10 22:17:51 +0100

    SL-748 - viewer must request the ObjectAnimation cap in order to get the new messages

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -2857,6 +2857,7 @@ void LLViewerRegionImpl::buildCapabilityNames(LLSD& capabilityNames)
 	capabilityNames.append("MeshUploadFlag");	
 	capabilityNames.append("NavMeshGenerationStatus");
 	capabilityNames.append("NewFileAgentInventory");
+	capabilityNames.append("ObjectAnimation");
 	capabilityNames.append("ObjectMedia");
 	capabilityNames.append("ObjectMediaNavigate");
 	capabilityNames.append("ObjectNavMeshProperties");
