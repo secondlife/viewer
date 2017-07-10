4bcaac489ee909f6cec30b6e1783c225869747b5
Mnikolenko Productengine <mnikolenko@productengine.com>
2017-07-10 15:47:26 +0300

    MAINT-7565 FIXED Inconsistent name format in nearby chat toast and chat history

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -3569,7 +3569,7 @@ void process_chat_from_simulator(LLMessageSystem *msg, void **user_data)
 		LLAvatarName av_name;
 		if (LLAvatarNameCache::get(from_id, &av_name))
 		{
-			chat.mFromName = av_name.getDisplayName();
+			chat.mFromName = av_name.getCompleteName();
 		}
 		else
 		{
