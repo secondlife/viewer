628e28639d4c92cd9e70d4441ec737d9538eaeec
Mnikolenko Productengine <mnikolenko@productengine.com>
2017-10-20 15:41:38 +0300

    MAINT-7906 Inventory filter fix

--- indra/newview/llpanelmaininventory.cpp
+++ indra/newview/llpanelmaininventory.cpp
@@ -29,7 +29,6 @@
 
 #include "llagent.h"
 #include "llagentcamera.h"
-#include "llagentwearables.h"
 #include "llavataractions.h"
 #include "llcheckboxctrl.h"
 #include "llcombobox.h"
@@ -180,8 +179,6 @@ BOOL LLPanelMainInventory::postBuild()
 		mWornItemsPanel->setFilterLinks(LLInventoryFilter::FILTERLINK_EXCLUDE_LINKS);
 		mWornItemsPanel->getFilter().markDefault();
 		mWornItemsPanel->setSelectCallback(boost::bind(&LLPanelMainInventory::onSelectionChange, this, mWornItemsPanel, _1, _2));
-
-		gAgentWearables.addLoadedCallback(boost::bind(&LLPanelMainInventory::updateWornItemsPanel, this));
 	}
 	mSearchTypeCombo  = getChild<LLComboBox>("search_type");
 	if(mSearchTypeCombo)
@@ -337,18 +334,6 @@ BOOL LLPanelMainInventory::handleKeyHere(KEY key, MASK mask)
 
 	return LLPanel::handleKeyHere(key, mask);
 
-}
-
-void LLPanelMainInventory::updateWornItemsPanel()
-{
-	if (!mUpdateWornTimer.getStarted())
-	{
-		mUpdateWornTimer.start();
-	}
-	else
-	{
-		mUpdateWornTimer.reset();
-	}
 }
 
 //----------------------------------------------------------------------------
@@ -684,11 +669,6 @@ void LLPanelMainInventory::draw()
 		mActivePanel->setSortOrder(LLInventoryFilter::SO_NAME);
 		mActivePanel->setSortOrder(order);
 		mResortActivePanel = false;
-	}
-	if (mWornItemsPanel && mUpdateWornTimer.getStarted() && (mUpdateWornTimer.getElapsedTimeF32() > 1))
-	{
-		mUpdateWornTimer.stop();
-		mWornItemsPanel->getFilter().setModified(LLFolderViewFilter::FILTER_MORE_RESTRICTIVE);
 	}
 	LLPanel::draw();
 	updateItemcountText();
--- indra/newview/llpanelmaininventory.h
+++ indra/newview/llpanelmaininventory.h
@@ -112,8 +112,6 @@ protected:
 	const std::string getFilterSubString();
 	void setFilterSubString(const std::string& string);
 
-	void updateWornItemsPanel();
-
 	// menu callbacks
 	void doToSelected(const LLSD& userdata);
 	void closeAllFolders();
@@ -144,7 +142,6 @@ private:
 	S32							mItemCount;
 	std::string 				mItemCountString;
 	LLComboBox*					mSearchTypeCombo;
-	LLFrameTimer				mUpdateWornTimer;
 
 
 
