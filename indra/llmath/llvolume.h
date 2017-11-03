fec6bbddc371b2d1e0aa41d39a1b3dfaa8fb2016
andreykproductengine <andreykproductengine@lindenlab.com>
2017-11-03 20:30:47 +0200

    MAINT-7974 Fixed LLProfile crash(error) caused by thread unsafe variable

--- indra/llmath/llvolume.h
+++ indra/llmath/llvolume.h
@@ -679,6 +679,8 @@ protected:
 
 class LLProfile
 {
+	friend class LLVolume;
+
 public:
 	LLProfile()
 		: mOpen(FALSE),
@@ -688,8 +690,6 @@ public:
 		  mTotal(2)
 	{
 	}
-
-	~LLProfile();
 
 	S32	 getTotal() const								{ return mTotal; }
 	S32	 getTotalOut() const							{ return mTotalOut; }	// Total number of outside points
@@ -723,6 +723,8 @@ public:
 	friend std::ostream& operator<<(std::ostream &s, const LLProfile &profile);
 
 protected:
+	~LLProfile();
+
 	static S32 getNumNGonPoints(const LLProfileParams& params, S32 sides, F32 offset=0.0f, F32 bevel = 0.0f, F32 ang_scale = 1.f, S32 split = 0);
 	void genNGon(const LLProfileParams& params, S32 sides, F32 offset=0.0f, F32 bevel = 0.0f, F32 ang_scale = 1.f, S32 split = 0);
 
