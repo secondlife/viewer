e5ef898728b5881eb1f233aa4e0dc9dbbd0991a5
andreykproductengine <andreykproductengine@lindenlab.com>
2017-11-27 15:02:33 +0200

    MAINT-8028 Fixed memory leak in allocateDataSize()

--- indra/llimage/llimage.cpp
+++ indra/llimage/llimage.cpp
@@ -748,7 +748,11 @@ U8* LLImageBase::allocateData(S32 size)
 	{
 		size = 0;
 		mWidth = mHeight = 0;
-		mData = NULL;
+		if (mData)
+		{
+			deleteData(); // virtual
+			mData = NULL;
+		}
 	}
 	mDataSize = size;
 	claimMem(mDataSize);
@@ -775,6 +779,7 @@ U8* LLImageBase::reallocateData(S32 size)
 	disclaimMem(mDataSize);
 	mDataSize = size;
 	claimMem(mDataSize);
+	mBadBufferAllocation = false;
 	return mData;
 }
 
