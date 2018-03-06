8617e974272c71c768942e8e25e7e1761c003fb9
maxim_productengine <mnikolenko@productengine.com>
2018-03-06 11:32:28 +0200

    MAINT-8342 FIXED [maint-viewer] Viewer crash when click Take off in Temporary attachments

--- indra/newview/llpanelwearing.cpp
+++ indra/newview/llpanelwearing.cpp
@@ -342,7 +342,11 @@ bool LLPanelWearing::isActionEnabled(const LLSD& userdata)
 		}
 		else
 		{
-			return mTempItemsList->hasSelectedItem();
+			LLScrollListItem* item = mTempItemsList->getFirstSelected();
+			if (item && item->getUUID().notNull())
+			{
+				return true;
+			}
 		}
 	}
 
@@ -524,7 +528,7 @@ void LLPanelWearing::onEditAttachment()
 void LLPanelWearing::onRemoveAttachment()
 {
 	LLScrollListItem* item = mTempItemsList->getFirstSelected();
-	if (item)
+	if (item && item->getUUID().notNull())
 	{
 		LLSelectMgr::getInstance()->deselectAll();
 		LLSelectMgr::getInstance()->selectObjectAndFamily(mAttachmentsMap[item->getUUID()]);
