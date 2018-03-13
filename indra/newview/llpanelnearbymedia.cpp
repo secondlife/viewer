b1d9487556ab904da12757454dd7c5bf56a8a822
maxim_productengine <mnikolenko@productengine.com>
2018-03-13 18:31:27 +0200

    MAINT-8382 FIXED “Zoom back” button is displayed after selecting other nearby media

--- indra/newview/llpanelnearbymedia.cpp
+++ indra/newview/llpanelnearbymedia.cpp
@@ -1047,7 +1047,7 @@ void LLPanelNearByMedia::updateControls()
 			else {
 				showBasicControls(!impl->isMediaDisabled(), 
 								  ! impl->isParcelMedia(),  // include_zoom
-								  LLViewerMediaFocus::getInstance()->isZoomed(),
+								  LLViewerMediaFocus::getInstance()->isZoomedOnMedia(impl->getMediaTextureID()),
 								  impl->getVolume() == 0.0,
 								  impl->getVolume());
 			}
