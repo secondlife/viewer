43ae8aee6a8719e3bdd87ff2711343dd77edaefc
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2018-03-30 13:11:30 +0000

    SL-440 Obtain Abuse Report categories from capability

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -2816,6 +2816,7 @@ void LLViewerRegion::unpackRegionHandshake()
 
 void LLViewerRegionImpl::buildCapabilityNames(LLSD& capabilityNames)
 {
+	capabilityNames.append("AbuseCategories");
 	capabilityNames.append("AgentPreferences");
 	capabilityNames.append("AgentState");
 	capabilityNames.append("AttachmentResources");
