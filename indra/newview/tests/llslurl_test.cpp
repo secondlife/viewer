b587e02a6e1c0219ddd31411783f7a5e86945d0e
andreykproductengine <andreykproductengine@lindenlab.com>
2018-04-10 18:01:02 +0300

    Fix for some tests not building

--- indra/newview/tests/llslurl_test.cpp
+++ indra/newview/tests/llslurl_test.cpp
@@ -46,10 +46,10 @@ static const char * const TEST_FILENAME("llslurl_test.xml");
 class LLTrans
 {
 public:
-	static std::string getString(const std::string &xml_desc, const LLStringUtil::format_map_t& args);
+	static std::string getString(const std::string &xml_desc, const LLStringUtil::format_map_t& args, bool def_string = false);
 };
 
-std::string LLTrans::getString(const std::string &xml_desc, const LLStringUtil::format_map_t& args)
+std::string LLTrans::getString(const std::string &xml_desc, const LLStringUtil::format_map_t& args, bool def_string)
 {
 	return std::string();
 }
