0abe3a4919bef4f80ccca23eef85fcf195165b37
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2018-05-04 15:46:05 +0000

    MAINT-8593 Viewer should not repeat mesh dowloads indefinetely

--- indra/newview/llmeshrepository.cpp
+++ indra/newview/llmeshrepository.cpp
@@ -982,7 +982,7 @@ void LLMeshRepoThread::run()
                     // failed to load before, wait a bit
                     incomplete.push_front(req);
                 }
-                else if (!fetchMeshHeader(req.mMeshParams, req.getRetries()))
+                else if (!fetchMeshHeader(req.mMeshParams, req.canRetry()))
                 {
                     if (req.canRetry())
                     {
@@ -1071,14 +1071,14 @@ void LLMeshRepoThread::run()
                     mMutex->unlock();
                     if (req.isDelayed())
                     {
-                        incomplete.insert(req.mId);
+                        incomplete.insert(req);
                     }
                     else if (!fetchMeshDecomposition(req.mId))
                     {
                         if (req.canRetry())
                         {
                             req.updateTime();
-                            incomplete.insert(req.mId);
+                            incomplete.insert(req);
                         }
                         else
                         {
@@ -1097,7 +1097,7 @@ void LLMeshRepoThread::run()
             // holding lock, final list
             if (!mPhysicsShapeRequests.empty())
             {
-                std::set<LLUUID> incomplete;
+                std::set<UUIDBasedRequest> incomplete;
                 while (!mPhysicsShapeRequests.empty() && mHttpRequestSet.size() < sRequestHighWater)
                 {
                     mMutex->lock();
@@ -1107,14 +1107,14 @@ void LLMeshRepoThread::run()
                     mMutex->unlock();
                     if (req.isDelayed())
                     {
-                        incomplete.insert(req.mId);
+                        incomplete.insert(req);
                     }
                     else if (!fetchMeshPhysicsShape(req.mId))
                     {
                         if (req.canRetry())
                         {
                             req.updateTime();
-                            incomplete.insert(req.mId);
+                            incomplete.insert(req);
                         }
                         else
                         {
