bce40a19348a35f5e223cf740e4ba7e96314286b
AndreyL ProductEngine <alihatskiy@productengine.com>
2018-07-18 19:59:00 +0300

    Backed out changeset: 1d9ad7f485e6

--- indra/newview/llinventorymodel.cpp
+++ indra/newview/llinventorymodel.cpp
@@ -344,23 +344,13 @@ LLViewerInventoryCategory* LLInventoryModel::getCategory(const LLUUID& id) const
 
 bool LLInventoryModel::isCategoryHidden(const LLUUID& id) const
 {
-	bool res = false;
 	const LLViewerInventoryCategory* category = getCategory(id);
 	if (category)
 	{
 		LLFolderType::EType cat_type = category->getPreferredType();
-		switch (cat_type)
-		{
-			case LLFolderType::FT_INBOX:
-			case LLFolderType::FT_OUTBOX:
-			case LLFolderType::FT_MARKETPLACE_LISTINGS:
-				res = true;
-				break;
-			default:
-				break;
-		}
+		return (cat_type == LLFolderType::FT_INBOX || cat_type == LLFolderType::FT_OUTBOX);
 	}
-	return res;
+	return false;
 }
 
 S32 LLInventoryModel::getItemCount() const
