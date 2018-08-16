7bb2faac0fb790c76e54493c641e5a77561fde26
AndreyL ProductEngine <alihatskiy@productengine.com>
2018-08-16 21:09:45 +0300

    mac build fix

--- indra/newview/llselectmgr.cpp
+++ indra/newview/llselectmgr.cpp
@@ -6317,7 +6317,7 @@ void LLSelectNode::renderOneWireframe(const LLColor4& color)
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    bool wireframe_selection = gFloaterTools && gFloaterTools->getVisible() || LLSelectMgr::sRenderHiddenSelections;
+    bool wireframe_selection = (gFloaterTools && gFloaterTools->getVisible()) || LLSelectMgr::sRenderHiddenSelections;
 
     if (LLSelectMgr::sRenderHiddenSelections)
     {
