8a162bc59cefc246da80d66f223a5cd545ff1dd3
Graham Linden <graham@lindenlab.com>
2018-09-10 20:41:10 +0100

    Pull fname out of class loop so we can see which actual shader file we were trying to load while debugging.

--- indra/llrender/llshadermgr.cpp
+++ indra/llrender/llshadermgr.cpp
@@ -588,10 +588,11 @@ GLhandleARB LLShaderMgr::loadShaderFile(const std::string& filename, S32 & shade
 	S32 try_gpu_class = shader_level;
 	S32 gpu_class;
 
+    std::stringstream fname;
+
 	//find the most relevant file
 	for (gpu_class = try_gpu_class; gpu_class > 0; gpu_class--)
-	{	//search from the current gpu class down to class 1 to find the most relevant shader
-		std::stringstream fname;
+	{	//search from the current gpu class down to class 1 to find the most relevant shader		
 		fname << getShaderDirPrefix();
 		fname << gpu_class << "/" << filename;
 		
