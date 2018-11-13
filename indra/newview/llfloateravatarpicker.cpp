31be136d4056e02cbf5c377f3544235debb7376b
andreykproductengine <andreykproductengine@lindenlab.com>
2018-11-13 20:00:47 +0200

    SL-10042 Fixed hang in inventory floater

--- indra/newview/llfloateravatarpicker.cpp
+++ indra/newview/llfloateravatarpicker.cpp
@@ -812,7 +812,7 @@ bool LLFloaterAvatarPicker::isSelectBtnEnabled()
 {
 	bool ret_val = visibleItemsSelected();
 
-	if ( ret_val )
+	if ( ret_val && !isMinimized())
 	{
 		std::string acvtive_panel_name;
 		LLScrollListCtrl* list =  NULL;
