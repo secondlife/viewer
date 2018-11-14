dd6167ae8d652e32d1c3849631dd8c0022032f79
maxim_productengine <mnikolenko@productengine.com>
2018-11-14 14:01:50 +0200

    SL-10061 FIXED [EEP] Parcel Environment settings can't be changed when Region settings are used

--- indra/llinventory/llparcel.h
+++ indra/llinventory/llparcel.h
@@ -102,7 +102,9 @@ const U32 RT_LIST	= 0x1 << 4;
 const U32 RT_SELL	= 0x1 << 5;
 
 const S32 INVALID_PARCEL_ID = -1;
-const S32 INVALID_PARCEL_ENVIRONMENT_VERSION = -1;
+
+// if Region settings are used, parcel env. version is -1
+const S32 INVALID_PARCEL_ENVIRONMENT_VERSION = -2;
 
 // Timeouts for parcels
 // default is 21 days * 24h/d * 60m/h * 60s/m *1000000 usec/s = 1814400000000
