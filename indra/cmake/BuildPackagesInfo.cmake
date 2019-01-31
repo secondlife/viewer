96639a8d09862eca95c483336d603aeb85694370
Brad Kittenbrink <brad@lindenlab.com>
2019-01-30 17:16:06 -0800

    Fix failure when building packages-info.txt when autobuild can't be found in xcode's PATH

--- indra/cmake/BuildPackagesInfo.cmake
+++ indra/cmake/BuildPackagesInfo.cmake
@@ -1,6 +1,7 @@
 # -*- cmake -*-
 # Construct the version and copyright information based on package data.
 include(Python)
+include(FindAutobuild)
 
 # packages-formatter.py runs autobuild install --versions, which needs to know
 # the build_directory, which (on Windows) depends on AUTOBUILD_ADDRSIZE.
@@ -13,7 +14,7 @@ add_custom_command(OUTPUT packages-info.txt
   DEPENDS ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py
           ${CMAKE_SOURCE_DIR}/../autobuild.xml
   COMMAND ${PYTHON_EXECUTABLE}
-          ${CMAKE_SOURCE_DIR}/cmake/run_build_test.py -DAUTOBUILD_ADDRSIZE=${ADDRESS_SIZE}
+          ${CMAKE_SOURCE_DIR}/cmake/run_build_test.py -DAUTOBUILD_ADDRSIZE=${ADDRESS_SIZE} -DAUTOBUILD=${AUTOBUILD_EXECUTABLE}
           ${PYTHON_EXECUTABLE}
           ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py "${VIEWER_CHANNEL}" "${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}" > packages-info.txt
   )
