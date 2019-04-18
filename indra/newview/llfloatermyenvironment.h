32598518816be6c312acc9b027d3a28ed807429a
maxim_productengine <mnikolenko@productengine.com>
2019-04-18 17:35:28 +0300

    SL-10983 [EEP] Add a search box to the "My Environments" floater

--- indra/newview/llfloatermyenvironment.h
+++ indra/newview/llfloatermyenvironment.h
@@ -1,10 +1,10 @@
 /** 
  * @file llfloatermyenvironment.h
- * @brief Read-only list of gestures from your inventory.
+ * @brief LLFloaterMyEnvironment class header file
  *
  * $LicenseInfo:firstyear=2002&license=viewerlgpl$
  * Second Life Viewer Source Code
- * Copyright (C) 2010, Linden Research, Inc.
+ * Copyright (C) 2019, Linden Research, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,10 +24,6 @@
  * $/LicenseInfo$
  */
 
-/**
- * (Also has legacy gesture editor for testing.)
- */
-
 #ifndef LL_LLFLOATERMYENVIRONMENT_H
 #define LL_LLFLOATERMYENVIRONMENT_H
 #include <vector> 
@@ -35,14 +31,8 @@
 #include "llfloater.h"
 #include "llinventoryobserver.h"
 #include "llinventoryfilter.h"
+#include "llfiltereditor.h"
 
-class LLView;
-class LLButton;
-class LLLineEditor;
-class LLComboBox;
-class LLViewerGesture;
-class LLGestureOptions;
-class LLMenuGL;
 class LLInventoryPanel;
 
 class LLFloaterMyEnvironment
@@ -60,10 +50,13 @@ public:
 
 private:
     LLInventoryPanel *              mInventoryList;
+    LLFilterEditor *                mFilterEdit;
     U64                             mTypeFilter;
     LLUUID                          mSelectedAsset;
+    LLSaveFolderState               mSavedFolderState;
 
     void                            onFilterCheckChange();
+    void                            onFilterEdit(const std::string& search_string);
     void                            onSelectionChange();
     void                            onDeleteSelected();
     void                            onDoCreate(const LLSD &data);
@@ -77,53 +70,6 @@ private:
     bool                            isSettingSelected(LLUUID item_id);
 
     static LLUUID                   findItemByAssetId(LLUUID asset_id, bool copyable_only, bool ignore_library);
-
-#if 0
-	virtual void done ();
-	void        refreshAll();
-	/**
-	 * @brief Add new scrolllistitem into gesture_list.
-	 * @param  item_id inventory id of gesture
-	 * @param  gesture can be NULL , if item was not loaded yet
-	 */
-	void addGesture(const LLUUID& item_id, LLMultiGesture* gesture, LLCtrlListInterface * list);
-
-protected:
-	// Reads from the gesture manager's list of active gestures
-	// and puts them in this list.
-	void buildGestureList();
-	void playGesture(LLUUID item_id);
-private:
-	void addToCurrentOutFit();
-	/**
-	 * @brief  This method is using to collect selected items. 
-	 * In some places gesture_list can be rebuilt by gestureObservers during  iterating data from LLScrollListCtrl::getAllSelected().
-	 * Therefore we have to copy these items to avoid viewer crash.
-	 * @see LLFloaterGesture::onActivateBtnClick
-	 */
-	void getSelectedIds(uuid_vec_t& ids);
-	bool isActionEnabled(const LLSD& command);
-	/**
-	 * @brief Activation rules:
-	 *  According to Gesture Spec:
-	 *  1. If all selected gestures are active: set to inactive
-	 *  2. If all selected gestures are inactive: set to active
-	 *  3. If selected gestures are in a mixed state: set all to active
-	 */
-	void onActivateBtnClick();
-	void onClickEdit();
-	void onClickPlay();
-	void onClickNew();
-	void onCommitList();
-	void onCopyPasteAction(const LLSD& command);
-	void onDeleteSelected();
-
-	LLUUID mSelectedID;
-	LLUUID mGestureFolderID;
-	LLScrollListCtrl* mGestureList;
-
-	LLFloaterGestureObserver* mObserver;
-#endif
 };
 
 
