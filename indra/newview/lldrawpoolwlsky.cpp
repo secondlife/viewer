1ff876a961dc00b7f162054ea1a9fed79240bffc
Graham Linden <graham@lindenlab.com>
2019-04-30 12:19:35 -0700

    SL-11071
    Make sun disc and moon shaders use sRGB hardware tex sampling.
    
    Remove sun disc shader use of fullbright transport (fogging).

--- indra/newview/lldrawpoolwlsky.cpp
+++ indra/newview/lldrawpoolwlsky.cpp
@@ -484,6 +484,9 @@ void LLDrawPoolWLSky::renderHeavenlyBodies()
             {
                 sun_shader->bind();
 
+                gGL.getTexUnit(0)->setTextureColorSpace(LLTexUnit::TCS_SRGB);
+                gGL.getTexUnit(1)->setTextureColorSpace(LLTexUnit::TCS_SRGB);
+
                 if (tex_a && (!tex_b || (tex_a == tex_b)))
                 {
                     // Bind current and next sun textures
@@ -531,6 +534,9 @@ void LLDrawPoolWLSky::renderHeavenlyBodies()
         if (can_use_vertex_shaders && can_use_windlight_shaders && (tex_a || tex_b))
         {
             moon_shader->bind();
+
+            gGL.getTexUnit(0)->setTextureColorSpace(LLTexUnit::TCS_SRGB);
+            gGL.getTexUnit(1)->setTextureColorSpace(LLTexUnit::TCS_SRGB);
 
             if (tex_a && (!tex_b || (tex_a == tex_b)))
             {
