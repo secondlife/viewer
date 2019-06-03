930bf680c2d184b8678a0c42a8d3a0d30e104643
andreykproductengine <andreykproductengine@lindenlab.com>
2019-06-03 21:37:43 +0300

    SL-10423 Dump path data when missing critical file

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -2882,6 +2882,7 @@ void LLAppViewer::initStrings()
 	if (strings_path_full.empty() || !LLFile::isfile(strings_path_full))
 	{
 		// initial check to make sure files are there failed
+		gDirUtilp->dumpCurrentDirectories(LLError::LEVEL_WARN);
 		LL_ERRS() << "Viewer failed to find localization and UI files. Please reinstall viewer from  https://secondlife.com/support/downloads/ and contact https://support.secondlife.com if issue persists after reinstall." << LL_ENDL;
 	}
 	LLTransUtil::parseStrings(strings_file, default_trans_args);
