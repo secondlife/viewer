5e9b7b82a8e303c2480e07c993698020b770defe
andreykproductengine <andreykproductengine@lindenlab.com>
2019-06-24 21:24:01 +0300

    SL-11473 Block calculate button until model fully loads

--- indra/newview/llfloatermodelpreview.cpp
+++ indra/newview/llfloatermodelpreview.cpp
@@ -3014,6 +3014,15 @@ void LLModelPreview::updateStatusMessages()
 	{
 		mFMP->childDisable("ok_btn");
 	}
+
+    if (mModelNoErrors && mLodsWithParsingError.empty())
+    {
+        mFMP->childEnable("calculate_btn");
+    }
+    else
+    {
+        mFMP->childDisable("calculate_btn");
+    }
 	
 	//add up physics triangles etc
 	S32 phys_tris = 0;
