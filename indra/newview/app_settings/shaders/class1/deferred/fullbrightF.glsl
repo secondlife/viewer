118151750d908a8990bd611afc1dc9be7563ce6e
Graham Linden <graham@lindenlab.com>
2019-06-28 15:33:39 -0700

    SL-11381
    Make fullbright alpha test use correct handling when mask cutoff == 0.

--- indra/newview/app_settings/shaders/class1/deferred/fullbrightF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/fullbrightF.glsl
@@ -65,12 +65,7 @@ void main()
 	float final_alpha = color.a * vertex_color.a;
 
 #ifdef HAS_ALPHA_MASK
-    if (color.a < 0.05)
-    {
-        discard;
-    }
-
-	if (minimum_alpha > 0 && color.a < minimum_alpha)
+	if (color.a < minimum_alpha)
 	{
 		discard;
 	}
