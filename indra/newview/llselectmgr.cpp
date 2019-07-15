68b69714b739d7b33d91fdfde2d4e4434ff11339
maxim_productengine <mnikolenko@productengine.com>
2019-07-15 18:01:26 +0300

    SL-11528 FIXED Object Profile > Details does not show magenta highlight for mesh objects

--- indra/newview/llselectmgr.cpp
+++ indra/newview/llselectmgr.cpp
@@ -5950,6 +5950,10 @@ void LLSelectMgr::renderSilhouettes(BOOL for_hud)
                     && objectp->mDrawable->getVOVolume()->isMesh())
                 {
                     LLColor4 hlColor = objectp->isRootEdit() ? sParentColor : sChildColor;
+                    if (objectp->getID() == inspect_item_id)
+                    {
+                        hlColor = sHighlightInspectColor;
+                    }
                     renderMeshSelection_f(node, objectp, hlColor);
                 }
                 else
