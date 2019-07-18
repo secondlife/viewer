ba2429163af92b510d26444bb33f9f9d941c6dda
andreykproductengine <andreykproductengine@lindenlab.com>
2019-07-18 18:37:45 +0300

    SL-11592 [Win] Ability to bind extra mouse buttons for push to talk

--- indra/newview/llvoiceclient.cpp
+++ indra/newview/llvoiceclient.cpp
@@ -125,7 +125,7 @@ LLVoiceClient::LLVoiceClient()
 	mPTTDirty(true),
 	mPTT(true),
 	mUsePTT(true),
-	mPTTIsMiddleMouse(false),
+	mPTTMouseButton(0),
 	mPTTKey(0),
 	mPTTIsToggle(false),
 	mUserPTTState(false),
@@ -638,13 +638,22 @@ bool LLVoiceClient::getPTTIsToggle()
 
 void LLVoiceClient::setPTTKey(std::string &key)
 {
+	// Value is stored as text for readability
 	if(key == "MiddleMouse")
 	{
-		mPTTIsMiddleMouse = true;
+		mPTTMouseButton = LLMouseHandler::CLICK_MIDDLE;
+	}
+	else if(key == "MouseButton4")
+	{
+		mPTTMouseButton = LLMouseHandler::CLICK_BUTTON4;
+	}
+	else if (key == "MouseButton5")
+	{
+		mPTTMouseButton = LLMouseHandler::CLICK_BUTTON5;
 	}
 	else
 	{
-		mPTTIsMiddleMouse = false;
+		mPTTMouseButton = 0;
 		if(!LLKeyboard::keyFromString(key, &mPTTKey))
 		{
 			// If the call failed, don't match any key.
@@ -681,7 +690,7 @@ void LLVoiceClient::keyDown(KEY key, MASK mask)
 		return;
 	}
 	
-	if (!mPTTIsMiddleMouse && LLAgent::isActionAllowed("speak") && (key == mPTTKey))
+	if (mPTTMouseButton == 0 && LLAgent::isActionAllowed("speak") && (key == mPTTKey))
 	{
 		bool down = gKeyboard->getKeyDown(mPTTKey);
 		if (down)
@@ -693,7 +702,7 @@ void LLVoiceClient::keyDown(KEY key, MASK mask)
 }
 void LLVoiceClient::keyUp(KEY key, MASK mask)
 {
-	if (!mPTTIsMiddleMouse && (key == mPTTKey))
+	if (mPTTMouseButton == 0 && (key == mPTTKey))
 	{
 		bool down = gKeyboard->getKeyDown(mPTTKey);
 		if (!down)
@@ -702,9 +711,9 @@ void LLVoiceClient::keyUp(KEY key, MASK mask)
 		}
 	}
 }
-void LLVoiceClient::middleMouseState(bool down)
+void LLVoiceClient::updateMouseState(S32 click, bool down)
 {
-	if(mPTTIsMiddleMouse && LLAgent::isActionAllowed("speak"))
+	if(mPTTMouseButton == click && LLAgent::isActionAllowed("speak"))
 	{
 		inputUserControlState(down);
 	}
