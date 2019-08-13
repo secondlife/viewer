b1cab5d24687b07c124e0dcd61485fa9e368c882
Graham Linden <graham@lindenlab.com>
2019-08-13 09:20:37 -0700

    SL-11212
    Remove color clamping in determining sun diffuse and moonlight diffuse color from atmo settings.

--- indra/llinventory/llsettingssky.cpp
+++ indra/llinventory/llsettingssky.cpp
@@ -1313,22 +1313,9 @@ void LLSettingsSky::calculateLightSettings() const
     componentMultBy(sunlight, componentExp((light_atten * -1.f) * lighty));
     componentMultBy(sunlight, light_transmittance);
 
-    F32 max_color = llmax(sunlight.mV[0], sunlight.mV[1], sunlight.mV[2]);
-    if (max_color > 1.0f)
-    {
-        sunlight *= 1.0f/max_color;
-    }
-
     //increase ambient when there are more clouds
     LLColor3 tmpAmbient = ambient + (smear(1.f) - ambient) * cloud_shadow * 0.5;
     componentMultBy(tmpAmbient, light_transmittance);
-
-    //tmpAmbient = LLColor3::clamp(tmpAmbient, getGamma(), 1.0f);
-    max_color = llmax(tmpAmbient.mV[0], tmpAmbient.mV[1], tmpAmbient.mV[2]);
-    if (max_color > 1.0f)
-    {
-        tmpAmbient *= 1.0f/max_color;
-    }
 
     //brightness of surface both sunlight and ambient
     mSunDiffuse = sunlight;
@@ -1347,7 +1334,6 @@ void LLSettingsSky::calculateLightSettings() const
     LLColor3 moonlight_b(0.66, 0.66, 1.2); // scotopic ambient value
 
     componentMultBy(moonlight, componentExp((light_atten * -1.f) * lighty));
-    clampColor(moonlight, getGamma(), 1.0f);
 
     mMoonDiffuse  = componentMult(moonlight, light_transmittance) * moon_brightness;
     mMoonAmbient  = componentMult(moonlight_b, light_transmittance) * 0.0125f;
