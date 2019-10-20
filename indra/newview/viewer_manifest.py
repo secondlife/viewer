4e2b2f662ad3f7b2e09f9451227bbf7018d91708
Kitty Barnett <develop@catznip.com>
2019-10-20 23:45:52 +0200

    Update freetype to 2.10.1 and move viewer fonts to their own package

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -137,7 +137,7 @@ class ViewerManifest(LLManifest):
                 self.path("*.tga")
 
             # Include our fonts
-            with self.prefix(src_dst="fonts"):
+            with self.prefix(src="../packages/fonts"):
                 self.path("*.ttf")
                 self.path("*.txt")
 
