c7e08750639759e7c98f2b9da5075e25d1445d0d
Rider Linden <rider@lindenlab.com>
2019-12-11 15:34:37 -0800

    See if this might allow my unit test without pulling in LLAppViewer

--- indra/newview/llviewerassetstats.cpp
+++ indra/newview/llviewerassetstats.cpp
@@ -196,7 +196,11 @@ void LLViewerAssetStats::initSingleton()
     mHttpHeaders = std::make_shared<LLCore::HttpHeaders>();
     mHttpHeaders->append(HTTP_OUT_HEADER_CONTENT_TYPE, HTTP_CONTENT_LLSD_XML);
     
+#ifdef LL_TEST_llviewerassetstats
+    mHttpPolicy = 0;    // Do not pull in the app viewer for the tests.
+#else
     mHttpPolicy = LLAppViewer::instance()->getAppCoreHttp().getPolicy(LLAppCoreHttp::AP_REPORTING);
+#endif
 
     mHttpPriority = 1;
 }
