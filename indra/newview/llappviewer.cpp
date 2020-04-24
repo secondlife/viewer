fd654315be42df0ad14cbd959f6c21b3dba7b0b2
Callum Prentice <callum@gmail.com>
2020-04-24 08:41:11 -0700

    We hadn't been including the Dullahan point release version so I added it. Also added some newlines to the extremely long Dullahan version number to make it easier to read

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3211,12 +3211,16 @@ LLSD LLAppViewer::getViewerInfo() const
 	cef_ver_codec << ".";
 	cef_ver_codec << DULLAHAN_VERSION_MINOR;
 	cef_ver_codec << ".";
+	cef_ver_codec << DULLAHAN_VERSION_POINT;
+	cef_ver_codec << ".";
 	cef_ver_codec << DULLAHAN_VERSION_BUILD;
 
-	cef_ver_codec << " / CEF: ";
+	cef_ver_codec << std::endl;
+	cef_ver_codec << "CEF: ";
 	cef_ver_codec << CEF_VERSION;
 
-	cef_ver_codec << " / Chromium: ";
+	cef_ver_codec << std::endl;
+	cef_ver_codec << "Chromium: ";
 	cef_ver_codec << CHROME_VERSION_MAJOR;
 	cef_ver_codec << ".";
 	cef_ver_codec << CHROME_VERSION_MINOR;
