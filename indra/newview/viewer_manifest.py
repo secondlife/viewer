1305c03a13ada7617531c0c3daf3092b7c7b3b05
Callum Prentice <callum@gmail.com>
2020-04-28 15:38:45 -0700

    Remove dullahan_host.exe from signing list - doesn't change anything security wise and leads to an extra copy of dullahan_host.exe because of a code signing bug

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -796,7 +796,6 @@ class WindowsManifest(ViewerManifest):
         for exe in (
             self.final_exe(),
             "SLVersionChecker.exe",
-            "llplugin/dullahan_host.exe",
             ):
             self.sign(exe)
             
