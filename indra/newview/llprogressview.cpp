1fe92a9df951d37ec1373beef39dbb1bb8ae28c4
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-05-28 18:22:17 +0300

    SL-13340 Restored status display for login

--- indra/newview/llprogressview.cpp
+++ indra/newview/llprogressview.cpp
@@ -517,8 +517,7 @@ void LLProgressView::initTextures(S32 location_id, bool is_in_production)
     initLogos();
 
     childSetVisible("panel_icons", mLogosList.empty() ? FALSE : TRUE);
-    childSetVisible("panel_login", TRUE);
-    childSetVisible("panel_teleport", FALSE);
+    childSetVisible("panel_top_spacer", mLogosList.empty() ? TRUE : FALSE);
 }
 
 void LLProgressView::releaseTextures()
@@ -526,8 +525,7 @@ void LLProgressView::releaseTextures()
     gStartTexture = NULL;
     mLogosList.clear();
 
-    childSetVisible("panel_login", FALSE);
-    childSetVisible("panel_teleport", TRUE);
+    childSetVisible("panel_top_spacer", TRUE);
     childSetVisible("panel_icons", FALSE);
 }
 
