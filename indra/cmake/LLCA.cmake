2d3cd9127e2d8d3f7e3e4b6aeb9f34d41b2f8f97
Oz Linden <oz@lindenlab.com>
2020-06-04 09:12:01 -0400

    SL-13365: add check for the age of the CA certificate bundle

--- indra/cmake/LLCA.cmake
+++ indra/cmake/LLCA.cmake
@@ -2,3 +2,19 @@
 include(Prebuilt)
 
 use_prebuilt_binary(llca)
+
+set(CA_BUNDLE_MAX_AGE_DAYS 90)
+
+execute_process(
+    COMMAND bash bin/check-ca-bundle-age.sh --packages . --days ${CA_BUNDLE_MAX_AGE_DAYS}
+    WORKING_DIRECTORY ${AUTOBUILD_INSTALL_DIR}
+    RESULT_VARIABLE ca_bundle_check_result
+    )
+
+if (NOT ${ca_bundle_check_result} EQUAL 0)
+  message(SEND_ERROR "Certificate Authority Bundle is out of date (rebuild and update the llca package)")
+else (NOT ${ca_bundle_check_result} EQUAL 0)
+  message(STATUS "Certificate Authority Bundle is recent enough.")
+endif (NOT ${ca_bundle_check_result} EQUAL 0)
+
+
