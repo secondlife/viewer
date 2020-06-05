132be6fe3e2827fc4f1773b66dc995e36d396226
Ptolemy <ptolemy@lindenlab.com>
2020-06-05 09:27:13 -0700

    SL-13084: Add references to other common files

--- indra/newview/app_settings/shaders/class1/deferred/cloudsV.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/cloudsV.glsl
@@ -70,20 +70,17 @@ uniform float cloud_scale;
 // NOTE: Keep these in sync!
 //       indra\newview\app_settings\shaders\class1\deferred\skyV.glsl
 //       indra\newview\app_settings\shaders\class1\deferred\cloudsV.glsl
+//       indra\newview\app-settings\shaders\class2\windlight\cloudsV.glsl
 //       indra\newview\lllegacyatmospherics.cpp
+//       indra\newview\llsettingsvo.cpp
 void main()
 {
-
 	// World / view / projection
 	gl_Position = modelview_projection_matrix * vec4(position.xyz, 1.0);
 
 	// Texture coords
     // SL-13084 EEP added support for custom cloud textures -- flip them horizontally to match the preview of Clouds > Cloud Scroll
-    // Keep in Sync!
-    // * indra\newview\llsettingsvo.cpp
-    // * indra\newview\app_settings\shaders\class2\windlight\cloudsV.glsl
-    // * indra\newview\app_settings\shaders\class1\deferred\cloudsV.glsl
-	vary_texcoord0 = vec2( -texcoord0.x, texcoord0.y );
+	vary_texcoord0 = vec2( -texcoord0.x, texcoord0.y ); // See: LLSettingsVOSky::applySpecial
 
 	vary_texcoord0.xy -= 0.5;
 	vary_texcoord0.xy /= cloud_scale;
