b00b733e3cbdd07e39f42765db220a5ec74d0705
Mnikolenko Productengine <mnikolenko@productengine.com>
2020-06-26 15:10:46 +0300

    SL-13397 Reduce Viewer freeze when opening chat with a large group.

--- indra/newview/llfloaterimcontainer.h
+++ indra/newview/llfloaterimcontainer.h
@@ -181,6 +181,7 @@ private:
 	bool isParticipantListExpanded();
 
 	void idleUpdate(); // for convenience (self) from static idle
+	void idleProcessEvents();
 
 	LLButton* mExpandCollapseBtn;
 	LLButton* mStubCollapseBtn;
@@ -220,6 +221,7 @@ private:
 	LLConversationViewSession* createConversationItemWidget(LLConversationItem* item);
 	LLConversationViewParticipant* createConversationViewParticipant(LLConversationItem* item);
 	bool onConversationModelEvent(const LLSD& event);
+	void handleConversationModelEvent(const LLSD& event);
 
 	// Conversation list data
 	LLPanel* mConversationsListPanel;	// This is the main widget we add conversation widget to
@@ -228,6 +230,8 @@ private:
 	LLConversationViewModel mConversationViewModel;
 	LLFolderView* mConversationsRoot;
 	LLEventStream mConversationsEventStream; 
+
+	std::deque<LLSD> mConversationEventQueue;
 
 	LLTimer mParticipantRefreshTimer;
 };
