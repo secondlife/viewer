6e20fc99e76e5632e6e609d959156b444290286e
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-10-23 18:42:57 +0300

    SL-14181 Fix missing message when deleting last classified

--- indra/newview/llpanelprofileclassifieds.cpp
+++ indra/newview/llpanelprofileclassifieds.cpp
@@ -248,6 +248,9 @@ void LLPanelProfileClassifieds::callbackDeleteClassified(const LLSD& notificatio
         }
 
         updateButtons();
+
+        BOOL no_data = !mTabContainer->getTabCount();
+        mNoItemsLabel->setVisible(no_data);
     }
 }
 
