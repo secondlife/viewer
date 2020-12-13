dec231827b896d64e068b074d371774293cb8b37
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-12-13 13:32:20 +0200

    SL-14543 Fix reason in environments 'settings couldn't be applied' error

--- indra/newview/llenvironment.cpp
+++ indra/newview/llenvironment.cpp
@@ -2007,7 +2007,15 @@ void LLEnvironment::coroUpdateEnvironment(S32 parcel_id, S32 track_no, UpdateInf
         LL_WARNS("ENVIRONMENT") << "Couldn't update Windlight settings for " << ((parcel_id == INVALID_PARCEL_ID) ? ("region!") : ("parcel!")) << LL_ENDL;
 
         notify = LLSD::emptyMap();
-        notify["FAIL_REASON"] = result["message"].asString();
+        std::string reason = result["message"].asString();
+        if (reason.empty())
+        {
+            notify["FAIL_REASON"] = status.toString();
+        }
+        else
+        {
+            notify["FAIL_REASON"] = reason;
+        }
     }
     else
     {
@@ -2069,7 +2077,15 @@ void LLEnvironment::coroResetEnvironment(S32 parcel_id, S32 track_no, environmen
         LL_WARNS("ENVIRONMENT") << "Couldn't reset Windlight settings in " << ((parcel_id == INVALID_PARCEL_ID) ? ("region!") : ("parcel!")) << LL_ENDL;
 
         notify = LLSD::emptyMap();
-        notify["FAIL_REASON"] = result["message"].asString();
+        std::string reason = result["message"].asString();
+        if (reason.empty())
+        {
+            notify["FAIL_REASON"] = status.toString();
+        }
+        else
+        {
+            notify["FAIL_REASON"] = reason;
+        }
     }
     else
     {
