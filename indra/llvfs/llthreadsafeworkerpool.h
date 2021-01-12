4900d5cb9fc75666ed7c601682c3f34409d90f12
Callum Prentice <callum@gmail.com>
2021-01-11 17:45:48 -0800

    First commit: Add first pass at LLThreadSafeWorkerPool impl and hook up some tests in login menu to experiment with it

---
+++ indra/llvfs/llthreadsafeworkerpool.h
@@ -0,0 +1,129 @@
+/**
+ * @file llthreadsafeworkerpool.h
+ * @brief Use a worker thread read/write from/to disk
+ *        via a thread safe request queue and avoid the
+ *        need for complex code or file locking.
+ *        See the implementation file for a description of
+ *        how it works and how to use it.
+ *
+ * $LicenseInfo:firstyear=2009&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2020, Linden Research, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+#ifndef _LLTHREADSAFEWORKERPOOL
+#define _LLTHREADSAFEWORKERPOOL
+
+#include "llthreadsafequeue.h"
+#include "llassettype.h"
+#include "lleventtimer.h"
+#include "llsingleton.h"
+
+class llThreadSafeWorkerPool :
+    public LLSingleton<llThreadSafeWorkerPool>,
+    public LLEventTimer
+{
+        LLSINGLETON(llThreadSafeWorkerPool);
+
+    public:
+        virtual ~llThreadSafeWorkerPool() = default;
+
+        void cleanupSingleton() override;
+
+        typedef std::shared_ptr<std::vector<U8>> request_payload_t;
+        typedef std::function<void(request_payload_t, std::string, bool)> request_callback_t;
+
+        void addReadRequest(std::string id,
+                            LLAssetType::EType at,
+                            unsigned long pos,
+                            unsigned long bytes,
+                            request_callback_t cb);
+
+        request_payload_t waitForReadComplete(std::string id,
+                                              LLAssetType::EType at,
+                                              unsigned long pos,
+                                              unsigned long bytes);
+
+        void addWriteRequest(std::string id,
+                             LLAssetType::EType at,
+                             request_payload_t buffer,
+                             request_callback_t cb,
+                             bool append);
+
+        void addRenameRequest(std::string old_id,
+                              LLAssetType::EType old_at,
+                              std::string new_id,
+                              LLAssetType::EType new_at,
+                              request_callback_t cb);
+
+        void addRemoveRequest(std::string id,
+                              LLAssetType::EType at,
+                              request_callback_t cb);
+
+        struct ReadError : public LLContinueError
+        {
+            ReadError(const std::string& what) : LLContinueError(what) {}
+        };
+
+    private:
+        std::thread mWorkerThread;
+
+        struct mResult
+        {
+            U32 id;
+            request_payload_t payload;
+            std::string filename;
+            bool ok;
+        };
+
+        struct mRequest
+        {
+            request_callback_t cb;
+        };
+
+        BOOL tick() override;
+
+        typedef std::function<mResult()> callable_t;
+        LLThreadSafeQueue<callable_t> mITaskQueue;
+        LLThreadSafeQueue<mResult> mResultQueue;
+
+        typedef std::map<U32, mRequest> request_map_t;
+        request_map_t mRequestMap;
+
+        /**
+         * This is the time period in seconds to run for.
+         * It can set this to 0 which means run every time
+         * through the main event loop.
+         * All this extra decoration is required because of
+         * the interesting C++ class initialization rules
+         * in action when we initialize the LLEventTimer base
+         * within the constructor for this class.
+         */
+        static constexpr F32 mTimePeriod = 0.05;
+
+        U32 mRequestId{ 0 };
+
+    private:
+        void requestThread();
+        const std::string assetTypeToString(LLAssetType::EType at);
+        const std::string idToFilepath(const std::string id, LLAssetType::EType at);
+};
+
+#endif // _LLTHREADSAFEWORKERPOOL
--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -133,6 +133,7 @@
 #include <boost/algorithm/string.hpp>
 #include "llcleanup.h"
 #include "llviewershadermgr.h"
+#include "llthreadsafeworkerpool.h"
 
 using namespace LLAvatarAppearanceDefines;
 
@@ -321,6 +322,10 @@ void handle_debug_avatar_textures(void*);
 void handle_grab_baked_texture(void*);
 BOOL enable_grab_baked_texture(void*);
 void handle_dump_region_object_cache(void*);
+
+// testing items for thread safe worker queue
+void simulate_cache_asynchronous_read_access(void*);
+void simulate_cache_asynchronous_write_access(void*);
 
 BOOL enable_save_into_task_inventory(void*);
 
@@ -1364,7 +1369,26 @@ class LLAdvancedCheckDebugClicks : public view_listener_t
 	}
 };
 
+///////////////////////////////////////
+// Tests for Thread safe worker pool //
+///////////////////////////////////////
+class LLSimulateCacheAsynchronousReadAccess : public view_listener_t
+{
+    bool handleEvent(const LLSD& userdata)
+    {
+        simulate_cache_asynchronous_read_access(NULL);
+        return true;
+    }
+};
 
+class LLSimulateCacheAsynchronousWriteAccess : public view_listener_t
+{
+    bool handleEvent(const LLSD& userdata)
+    {
+        simulate_cache_asynchronous_write_access(NULL);
+        return true;
+    }
+};
 
 /////////////////
 // DEBUG VIEWS //
@@ -3715,6 +3739,60 @@ class LLSelfStandUp : public view_listener_t
 		return true;
 	}
 };
+
+void simulate_cache_asynchronous_read_access(void*)
+{
+    LL_INFOS() << "Simulating cache asynchronous READ..." << LL_ENDL;
+
+    llThreadSafeWorkerPool::request_callback_t cb([](llThreadSafeWorkerPool::request_payload_t payload, std::string filename, bool)
+    {
+        LL_INFOS() << "Reading from " << filename << LL_ENDL;
+
+        if (!payload)
+        {
+            LL_INFOS() << "Payload is empty" << LL_ENDL;
+        }
+        else
+        {
+            const std::string payload_str((char*)payload->data(), payload->size());
+            LL_INFOS() << "Payload size is " << payload->size() << " and contains " << payload_str << LL_ENDL;
+        }
+    });
+
+    const std::string filename = "read_async.payload";
+
+    const unsigned long pos = 0;
+    const unsigned long bytes = 0; // read whole file
+
+    llThreadSafeWorkerPool::instance().addReadRequest(filename, LLAssetType::AT_UNKNOWN, pos, bytes, cb);
+}
+
+void simulate_cache_asynchronous_write_access(void*)
+{
+    LL_INFOS() << "Simulating cache asynchronous WRITE..." << LL_ENDL;
+
+    llThreadSafeWorkerPool::request_callback_t cb([](llThreadSafeWorkerPool::request_payload_t, std::string filename, bool result)
+    {
+        LL_INFOS() << "Reading from " << filename << LL_ENDL;
+
+        if (result)
+        {
+            LL_INFOS() << "File written successfully" << LL_ENDL;
+        }
+        else
+        {
+            LL_INFOS() << "Unable to write file" << LL_ENDL;
+        }
+    });
+
+    const std::string filename = "write_async.payload";
+    const U32 filesize = 24;
+    llThreadSafeWorkerPool::request_payload_t file_contents = std::make_shared<std::vector<U8>>(filesize);
+    memset(file_contents->data(), 'Z', file_contents->size());
+
+    const bool append = false;
+    llThreadSafeWorkerPool::instance().addWriteRequest(filename, LLAssetType::AT_UNKNOWN, file_contents, cb, append);
+}
 
 bool enable_standup_self()
 {
@@ -9108,6 +9186,8 @@ void initialize_menus()
 	// Advanced > World
 	view_listener_t::addMenu(new LLAdvancedDumpScriptedCamera(), "Advanced.DumpScriptedCamera");
 	view_listener_t::addMenu(new LLAdvancedDumpRegionObjectCache(), "Advanced.DumpRegionObjectCache");
+    view_listener_t::addMenu(new LLSimulateCacheAsynchronousReadAccess(), "Advanced.SimulateCacheAsynchronousReadAccess");
+    view_listener_t::addMenu(new LLSimulateCacheAsynchronousWriteAccess(), "Advanced.SimulateCacheAsynchronousWriteAccess");
 
 	// Advanced > UI
 	commit.add("Advanced.WebBrowserTest", boost::bind(&handle_web_browser_test,	_2));	// sigh! this one opens the MEDIA browser
