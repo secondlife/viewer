c83e740ef94e16ba85574454f3138905edecb029
Brad Payne (Vir Linden) <vir@lindenlab.com>
2021-03-08 13:56:16 +0000

    Revert "Merge branch 'master' of https://bitbucket.org/lindenlab/viewer into DRTVWR-519"
    This reverts commit e61f485a04dc8c8ac6bcf6a24848359092884d14, reversing
    changes made to 00c47d079f7e958e473ed4083a7f7691fa02dcd5.

--- indra/llmessage/lltransfertargetvfile.h
+++ indra/llmessage/lltransfertargetvfile.h
@@ -29,9 +29,9 @@
 
 #include "lltransfermanager.h"
 #include "llassetstorage.h"
-#include "llfilesystem.h"
+#include "llvfile.h"
 
-class LLFileSystem;
+class LLVFile;
 
 // Lame, an S32 for now until I figure out the deal with how we want to do
 // error codes.
