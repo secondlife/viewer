b1fad32e8b50fc7bb09a45f3454c24d6efeb4b15
Mnikolenko Productengine <mnikolenko@productengine.com>
2021-04-21 12:08:24 +0300

    Revert "SL-14633 FIXED The item from the favorite tab in the inventory floater can be purged"

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -987,7 +987,7 @@ void LLInvFVBridge::addTrashContextMenuOptions(menuentry_vec_t &items,
 		}
 	}
 	items.push_back(std::string("Purge Item"));
-	if (!isItemRemovable() || isPanelActive("Favorite Items"))
+	if (!isItemRemovable())
 	{
 		disabled_items.push_back(std::string("Purge Item"));
 	}
