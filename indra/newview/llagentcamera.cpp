176e56d3e8ddc7f937b2b67c005d7a210e2526a1
Ptolemy <ptolemy@lindenlab.com>
2021-04-30 10:56:08 -0700

    SL-14885: Use LLCachedControl

--- indra/newview/llagentcamera.cpp
+++ indra/newview/llagentcamera.cpp
@@ -2039,7 +2039,8 @@ LLVector3d LLAgentCamera::getFocusOffsetInitial()
 F32 LLAgentCamera::getCameraMaxZoomDistance()
 {
     // SL-14706 / SL-14885 TPV have relaxed camera constraints allowing you to mousewheeel zoom WAY out.
-    if (gSavedSettings.getBOOL("DisableCameraConstraints"))
+    static LLCachedControl<bool> s_disable_camera_constraints(gSavedSettings, "DisableCameraConstraints", false);
+    if (s_disable_camera_constraints)
     {
         return (F32)INT_MAX;
     }
