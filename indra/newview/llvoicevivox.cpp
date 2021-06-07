261ba33537235fc0ed88e2fea169dbae793ba7ff
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2021-06-07 21:02:44 +0300

    SL-15351 'Speak' button remains active on the parcel with disabled voice

--- indra/newview/llvoicevivox.cpp
+++ indra/newview/llvoicevivox.cpp
@@ -4609,6 +4609,12 @@ bool LLVivoxVoiceClient::switchChannel(
 			// The old session may now need to be deleted.
 			reapSession(oldSession);
 
+            // If voice was on, turn it off
+            if (LLVoiceClient::getInstance()->getUserPTTState())
+            {
+                LLVoiceClient::getInstance()->setUserPTTState(false);
+            }
+
 			notifyStatusObservers(LLVoiceClientStatusObserver::STATUS_VOICE_DISABLED);
 		}
 		else
