8ca811f84989538d8834eb098103637d3d0d6673
Mnikolenko Productengine <mnikolenko@productengine.com>
2021-07-09 16:43:10 +0300

    SL-14799 add "Hide beacon" button

--- indra/newview/llfloaterworldmap.h
+++ indra/newview/llfloaterworldmap.h
@@ -107,7 +107,8 @@ public:
 	// teleport to the tracked item, if there is one
 	void			teleport();
 	void			onChangeMaturity();
-	
+
+	void			onClearBtn();
 	
 	//Slapp instigated avatar tracking
 	void			avatarTrackFromSlapp( const LLUUID& id ); 
@@ -124,7 +125,6 @@ protected:
 	void			onComboTextEntry( );
 	void			onSearchTextEntry( );
 
-	void			onClearBtn();
 	void			onClickTeleportBtn();
 	void			onShowTargetBtn();
 	void			onShowAgentBtn();
@@ -198,6 +198,26 @@ private:
 };
 
 extern LLFloaterWorldMap* gFloaterWorldMap;
+
+
+class LLPanelHideBeacon : public LLPanel
+{
+public:
+	static LLPanelHideBeacon* getInstance();
+
+	LLPanelHideBeacon();
+	/*virtual*/ BOOL postBuild();
+	/*virtual*/ void setVisible(BOOL visible);
+	/*virtual*/ void draw();
+
+private:
+	static LLPanelHideBeacon* getPanelHideBeacon();
+	void onHideButtonClick();
+	void updatePosition();
+
+	LLButton* mHideButton;
+
+};
 
 #endif
 
