54171dbc9fe3fa42ebbf59a243b7381fdb3aa662
Andrey Lihatskiy <alihatskiy@productengine.com>
2021-07-19 18:58:04 +0300

    SL-15585 more logging for altool

--- indra/newview/installers/darwin/apple-notarize.sh
+++ indra/newview/installers/darwin/apple-notarize.sh
@@ -6,12 +6,12 @@ if [ -f "$CONFIG_FILE" ]; then
     zip_file=${app_file/app/zip}
     ditto -c -k --keepParent "$app_file" "$zip_file"
     if [ -f "$zip_file" ]; then
-        requestUUID=$(xcrun altool --notarize-app --primary-bundle-id "com.secondlife.viewer" \
-                                --username $USERNAME \
-                                --password $PASSWORD \
-                                --asc-provider $ASC_PROVIDER \
-                                --file "$zip_file" 2>&1 \
-                    | awk '/RequestUUID/ { print $NF; }')
+        res=$(xcrun altool --notarize-app --primary-bundle-id "com.secondlife.viewer" \
+                                   --username $USERNAME \
+                                   --password $PASSWORD \
+                                   --asc-provider $ASC_PROVIDER \
+                                   --file "$zip_file" 2>&1)
+        requestUUID=$(echo $res | awk '/RequestUUID/ { print $NF; }')
 
         echo "Apple Notarization RequestUUID: $requestUUID"
 
@@ -41,6 +41,7 @@ if [ -f "$CONFIG_FILE" ]; then
             fi
         else
             echo "Notarization error: couldn't get request UUID"
+            echo $res
             exit 1
         fi
     fi
