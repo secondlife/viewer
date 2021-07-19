1a782ed690e56bf81ec9073041c7559e1762855d
Dave Houlton <euclid@lindenlab.com>
2021-07-19 16:26:33 -0600

    SL-15595 update viewer autobuild to import tracy lib

---
+++ indra/cmake/Tracy.cmake
@@ -0,0 +1,17 @@
+# -*- cmake -*-
+include(Prebuilt)
+
+set(TRACY ON CACHE BOOL "Use Tracy profiler.")
+
+if (TRACY)
+  set(TRACY_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/tracy) 
+  if (WINDOWS)
+    use_prebuilt_binary(tracy)
+    set(TRACY_LIBRARY tracy)
+  else (WINDOWS)
+    set(TRACY_LIBRARY "")
+  endif (WINDOWS)
+else (TRACY)
+  set(TRACY_LIBRARY "")
+endif (TRACY)
+
