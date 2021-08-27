bb601196ddbe7d37584468f611ada67647a050d3
Ptolemy <ptolemy@lindenlab.com>
2021-08-26 22:52:07 -0700

    SL-15709: IF Tracy is enabled default to using only Tracy instead of Fast Timers; also print Tracy include directory

--- indra/cmake/Tracy.cmake
+++ indra/cmake/Tracy.cmake
@@ -5,12 +5,25 @@ set(TRACY OFF CACHE BOOL "Use Tracy profiler.")
 
 if (TRACY)
   set(TRACY_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/tracy) 
+
+# See: indra/llcommon/llprofiler.h
+  add_definitions(-DLL_PROFILER_CONFIGURATION=2)
+  use_prebuilt_binary(tracy)
+
   if (WINDOWS)
-    use_prebuilt_binary(tracy)
+    MESSAGE(STATUS "Including Tracy for Windows: '${TRACY_INCLUDE_DIR}'")
     set(TRACY_LIBRARY tracy)
-  else (WINDOWS)
-    set(TRACY_LIBRARY "")
   endif (WINDOWS)
+
+  if (DARWIN)
+    MESSAGE(STATUS "Including Tracy for Darwin: '${TRACY_INCLUDE_DIR}'")
+    set(TRACY_LIBRARY "")
+  endif (DARWIN)
+
+  if (LINUX)
+    MESSAGE(STATUS "Including Tracy for Linux: '${TRACY_INCLUDE_DIR}'")
+    set(TRACY_LIBRARY "")
+  endif (LINUX)
 else (TRACY)
   set(TRACY_LIBRARY "")
 endif (TRACY)
