aa2169aa372492b3a5367997536fb6dfa89b74cf
Runitai Linden <davep@lindenlab.com>
2021-10-13 13:57:56 -0500

    SL-16166 Remove LLDrawPoolGlow::pushBatch non-specialization

--- indra/newview/lldrawpoolsimple.cpp
+++ indra/newview/lldrawpoolsimple.cpp
@@ -150,13 +150,6 @@ void LLDrawPoolGlow::render(S32 pass)
 	}
 }
 
-void LLDrawPoolGlow::pushBatch(LLDrawInfo& params, U32 mask, BOOL texture, BOOL batch_textures)
-{
-	//gGL.diffuseColor4ubv(params.mGlowColor.mV);
-	LLRenderPass::pushBatch(params, mask, texture, batch_textures);
-}
-
-
 LLDrawPoolSimple::LLDrawPoolSimple() :
 	LLRenderPass(POOL_SIMPLE)
 {
