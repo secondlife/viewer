7200f9fb2dfb106e0afe36b54cbc9fb826de7344
Ptolemy <ptolemy@lindenlab.com>
2022-01-13 12:25:21 -0800

    SL-16606: Add profiler category MEMORY

--- indra/llcommon/llcommon.cpp
+++ indra/llcommon/llcommon.cpp
@@ -42,7 +42,7 @@ void *operator new(size_t size)
     void* ptr;
     if (gProfilerEnabled)
     {
-        LL_PROFILE_ZONE_SCOPED;
+        LL_PROFILE_ZONE_SCOPED_CATEGORY_MEMORY;
         ptr = (malloc)(size);
     }
     else
@@ -62,7 +62,7 @@ void operator delete(void *ptr) noexcept
     TracyFree(ptr);
     if (gProfilerEnabled)
     {
-        LL_PROFILE_ZONE_SCOPED;
+        LL_PROFILE_ZONE_SCOPED_CATEGORY_MEMORY;
         (free)(ptr);
     }
     else
