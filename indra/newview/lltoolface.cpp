0bef5e553e5234b9373507341e3b001f4f4e54e7
Andrey Lihatskiy <alihatskiy@productengine.com>
2022-01-21 19:13:28 +0200

    Revert "SL-14717 Ability to trigger left clicks (and more) on animesh objects"
    This reverts commit 8215fff0c2a1c895ef916837847cf7526ae61ef9.
    
    # Conflicts:
    #	indra/newview/lltoolpie.cpp

--- indra/newview/lltoolface.cpp
+++ indra/newview/lltoolface.cpp
@@ -73,7 +73,7 @@ BOOL LLToolFace::handleDoubleClick(S32 x, S32 y, MASK mask)
 
 BOOL LLToolFace::handleMouseDown(S32 x, S32 y, MASK mask)
 {
-	gViewerWindow->pickAsync(x, y, mask, pickCallback, false, gSavedSettings.getBOOL("AnimatedObjectsAllowLeftClick"));
+	gViewerWindow->pickAsync(x, y, mask, pickCallback);
 	return TRUE;
 }
 
--- indra/newview/pipeline.cpp
+++ indra/newview/pipeline.cpp
@@ -7176,7 +7176,6 @@ LLViewerObject* LLPipeline::lineSegmentIntersectInWorld(const LLVector4a& start,
 	
 	if (!sPickAvatar)
 	{
-		pick_rigged = false;
 		//save hit info in case we need to restore
 		//due to attachment override
 		LLVector4a local_normal;
