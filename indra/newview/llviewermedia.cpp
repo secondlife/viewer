26e03f0c65c0b36705e468687553bbf54cca77be
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2022-02-03 02:57:29 +0200

    SL-16755 Show alert that download is not supported instead of failing silently

--- indra/newview/llviewermedia.cpp
+++ indra/newview/llviewermedia.cpp
@@ -3212,7 +3212,6 @@ void LLViewerMediaImpl::handleMediaEvent(LLPluginClassMedia* plugin, LLPluginCla
             //unblock media plugin
             const std::vector<std::string> empty_response;
             plugin->sendPickFileResponse(empty_response);
-            LLNotificationsUtil::add("MediaFileDownloadUnsupported");
 		}
 		break;
 
