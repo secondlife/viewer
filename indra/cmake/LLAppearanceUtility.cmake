07d7dcc0be9b793b1bcef63f526b441a58a9b764
Nicky <nicky.dasmijn@gmail.com>
2022-02-18 21:24:40 +0100

    Brinng back indra/cmake/LLAppearanceUtility.cmake as well.

---
+++ indra/cmake/LLAppearanceUtility.cmake
@@ -0,0 +1,12 @@
+# -*- cmake -*-
+include(Prebuilt)
+include(Boost)
+
+# Linux proprietary build only
+if (INSTALL_PROPRIETARY)
+    if(LINUX)
+        use_prebuilt_binary(llappearance_utility)
+        set(LLAPPEARANCEUTILITY_SRC_DIR ${LIBS_PREBUILT_DIR}/llappearanceutility/src)
+        set(LLAPPEARANCEUTILITY_BIN_DIR ${CMAKE_BINARY_DIR}/llappearanceutility)
+    endif (LINUX)
+endif (INSTALL_PROPRIETARY)
