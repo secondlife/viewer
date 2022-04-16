c8eb53859fb9b9f59d5ce8ef67f4011e95e34a9a
Nicky <nicky.dasmijn@gmail.com>
2022-04-16 14:21:26 +0200

    Remove unused code

--- indra/cmake/ExamplePlugin.cmake
+++ indra/cmake/ExamplePlugin.cmake
@@ -4,8 +4,3 @@ include(Prebuilt)
 
 set(EXAMPLEPLUGIN ON CACHE BOOL
         "EXAMPLEPLUGIN support for the llplugin/llmedia test apps.")
-
-if (WINDOWS)
-elseif (DARWIN)
-elseif (LINUX)
-endif (WINDOWS)
