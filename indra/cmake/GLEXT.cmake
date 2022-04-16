4934b65298b0f2bcf8898c87ec6f46f8d1412277
Nicky <nicky.dasmijn@gmail.com>
2022-04-16 19:51:02 +0200

    Cleanup

--- indra/cmake/GLEXT.cmake
+++ indra/cmake/GLEXT.cmake
@@ -4,5 +4,5 @@ include(Prebuilt)
 if (WINDOWS OR LINUX)
   use_prebuilt_binary(glext)
 endif (WINDOWS OR LINUX)
+
 use_prebuilt_binary(glh_linear)
-set(GLEXT_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
