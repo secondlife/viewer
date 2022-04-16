0eb2d8056a9d7c858545987cec74a4d604dcd5b5
Nicky <nicky.dasmijn@gmail.com>
2022-04-16 19:55:58 +0200

    Pull in LL_GLOD=1 via target, not by hand crafted magic.

--- indra/cmake/GLOD.cmake
+++ indra/cmake/GLOD.cmake
@@ -6,7 +6,6 @@ create_target( ll::glod )
 
 use_prebuilt_binary(glod)
 
-set(GLODLIB ON CACHE BOOL "Using GLOD library")
-
 set_target_include_dirs( ll::glod ${LIBS_PREBUILT_DIR}/include)
 set_target_libraries( ll::glod GLOD )
+target_compile_definitions( ll::glod INTERFACE LL_GLOD=1)
