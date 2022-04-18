d65a5832bfb1b35e22ee9ae14a4f00034c7cf2dd
Nicky <nicky.dasmijn@gmail.com>
2022-04-18 03:10:02 +0200

    ll::sdl had a wrong include directory.

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -9,7 +9,7 @@ add_library( ll::sdl INTERFACE IMPORTED )
 
 if (LINUX)
   use_prebuilt_binary(SDL)
-  target_include_directories( ll::sdl SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/i686-linux)
+  target_include_directories( ll::sdl SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include)
   target_link_libraries( ll::sdl INTERFACE SDL directfb fusion direct X11)
   target_compile_definitions( ll::sdl INTERFACE LL_SDL=1)
 endif (LINUX)
