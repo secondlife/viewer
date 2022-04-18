0319ed661159d32e92bf2071aa379b052798448e
Nicky <nicky.dasmijn@gmail.com>
2022-04-18 17:05:31 +0200

    Another instance where LLFilePicker::getSaveFile had the wrong number of arguments.

--- indra/newview/llfilepicker.cpp
+++ indra/newview/llfilepicker.cpp
@@ -1206,7 +1206,7 @@ static std::string add_save_texture_filter_to_gtkchooser(GtkWindow *picker)
 	return caption;
 }
 
-BOOL LLFilePicker::getSaveFile( ESaveFilter filter, const std::string& filename )
+BOOL LLFilePicker::getSaveFile( ESaveFilter filter, const std::string& filename, bool blocking )
 {
 	BOOL rtn = FALSE;
 
