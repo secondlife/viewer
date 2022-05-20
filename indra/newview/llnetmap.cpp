6fb787cd10ea840a9d099715e7462bb5f29c5e6b
Cosmic Linden <cosmic@lindenlab.com>
2022-05-20 11:56:31 -0700

    SL-17463: Set the default for MiniMapRotate back to true (enable minimap rotation by default), but set MiniMapRotate to false for new installs

--- indra/newview/llnetmap.cpp
+++ indra/newview/llnetmap.cpp
@@ -106,6 +106,13 @@ LLNetMap::LLNetMap (const Params & p)
 	mPopupMenu(NULL)
 {
 	mScale = gSavedSettings.getF32("MiniMapScale");
+    if (gAgent.isFirstLogin())
+    {
+        // *HACK: On first run, set this to false for new users, otherwise the
+        // default is true to maintain consistent experience for existing
+        // users.
+        gSavedSettings.setBOOL("MiniMapRotate", false);
+    }
 	mPixelsPerMeter = mScale / REGION_WIDTH_METERS;
 	mDotRadius = llmax(DOT_SCALE * mPixelsPerMeter, MIN_DOT_RADIUS);
 }
