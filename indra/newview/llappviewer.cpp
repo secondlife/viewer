7dab504b0aec1840196068645b5f60f6efccc14e
Nat Goodspeed <nat@lindenlab.com>
2022-06-07 16:59:50 -0400

    DRTVWR-564: Honor --leap cmd line switch even for developer builds.
    The logic that runs the viewer updater has been #if'ed out except in
    LL_RELEASE_FOR_DOWNLOAD builds. But for some reason the --leap switch
    processing was enclosed in the same compile-time conditional.
    
    We want to be able to run scripted tests even of developer builds, and the
    --leap switch is the way we do that.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1184,12 +1184,8 @@ bool LLAppViewer::init()
     {
         LL_WARNS("InitInfo") << "Skipping updater check." << LL_ENDL;
     }
+#endif //LL_RELEASE_FOR_DOWNLOAD
 
-    if (mUpdaterNotFound)
-    {
-        LL_WARNS("InitInfo") << "Failed to launch updater. Skipping Leap commands." << LL_ENDL;
-    }
-    else
     {
         // Iterate over --leap command-line options. But this is a bit tricky: if
         // there's only one, it won't be an array at all.
@@ -1222,7 +1218,6 @@ bool LLAppViewer::init()
                              << "lleventhost no longer supported as a dynamic library"
                              << LL_ENDL;
     }
-#endif //LL_RELEASE_FOR_DOWNLOAD
 
 	LLTextUtil::TextHelpers::iconCallbackCreationFunction = create_text_segment_icon_from_url_match;
 
