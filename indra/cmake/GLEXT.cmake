6b857059516be849e60570445a03f58647656c53
Brad Kittenbrink <brad@lindenlab.com>
2022-06-23 17:33:03 -0700

    fixing DRTVWR-559 glext usage on mac by upgrading to use khronos distributed glext.h

--- indra/cmake/GLEXT.cmake
+++ indra/cmake/GLEXT.cmake
@@ -2,9 +2,7 @@
 include(Prebuilt)
 
 if (NOT USESYSTEMLIBS)
-  if (WINDOWS OR LINUX)
-    use_prebuilt_binary(glext)
-  endif (WINDOWS OR LINUX)
+  use_prebuilt_binary(glext)
   use_prebuilt_binary(glh_linear)
   set(GLEXT_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
 endif (NOT USESYSTEMLIBS)
