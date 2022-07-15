2785f1f33d95348853d6c3e08a14f7627407b50c
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2022-07-15 20:23:12 +0300

    SL-17741 Fixed wrong condition

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -2096,7 +2096,7 @@ BOOL LLItemBridge::confirmRemoveItem(const LLSD& notification, const LLSD& respo
 bool LLItemBridge::isItemCopyable(bool can_copy_as_link) const
 {
     LLViewerInventoryItem* item = getItem();
-    if (item)
+    if (!item)
     {
         return false;
     }
