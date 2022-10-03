f7cc8e4c8c70bd6a48b9c3a8e99bef13ebca7d89
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2022-10-03 23:47:19 +0300

    SL-18047 Birthdate one day early

--- indra/newview/llpanelprofile.cpp
+++ indra/newview/llpanelprofile.cpp
@@ -1280,6 +1280,8 @@ void LLPanelProfileSecondLife::fillRightsData()
 
 void LLPanelProfileSecondLife::fillAgeData(const LLDate &born_on)
 {
+    // Date from server comes already converted to stl timezone,
+    // so display it as an UTC + 0
     std::string name_and_date = getString("date_format");
     LLSD args_name;
     args_name["datetime"] = (S32)born_on.secondsSinceEpoch();
