37766d9297fd692cc70490c898d647a11e04098b
Rye Mutt <rye@alchemyviewer.org>
2022-10-19 19:42:49 -0400

    Fix use after free in material manager

--- indra/newview/llmaterialmgr.cpp
+++ indra/newview/llmaterialmgr.cpp
@@ -660,8 +660,8 @@ void LLMaterialMgr::processGetQueue()
 		{
 			material_queue_t::iterator itMaterial = loopMaterial++;
 			materialsData.append((*itMaterial).asLLSD());
-			materials.erase(itMaterial);
 			markGetPending(region_id, *itMaterial);
+			materials.erase(itMaterial);
 		}
 		if (materials.empty())
 		{
