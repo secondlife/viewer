22dc6de993cfc3de497edbce998a2bad719809d0
Rye Mutt <rye@alchemyviewer.org>
2022-10-19 19:41:46 -0400

    Fix trivial memory leaks in note card processing, chat history, and task inventory

--- indra/newview/llchathistory.cpp
+++ indra/newview/llchathistory.cpp
@@ -1103,10 +1103,7 @@ LLChatHistory::LLChatHistory(const LLChatHistory::Params& p)
 
 LLSD LLChatHistory::getValue() const
 {
-  LLSD* text=new LLSD(); 
-  text->assign(mEditor->getText());
-  return *text;
-    
+	return LLSD(mEditor->getText());
 }
 
 LLChatHistory::~LLChatHistory()
