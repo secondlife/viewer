7ceb266604de07ca6743dcd1077b675dea39f1d9
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2022-11-03 23:31:47 +0200

    SL-18565 Built-in bump maps and some environment textures fail to load
    Due to udp texture fetching no longer being present

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1276,7 +1276,6 @@ bool LLAppViewer::init()
 
     //LLSimpleton creations
     LLEnvironment::createInstance();
-    LLEnvironment::getInstance()->initSingleton();
     LLWorld::createInstance();
     LLSelectMgr::createInstance();
     LLViewerCamera::createInstance();
