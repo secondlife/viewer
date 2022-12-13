43c0dc7f15a013405ce8172e1fbd390c771a1967
Brad Linden <brad@lindenlab.com>
2022-12-12 18:03:09 -0800

    Attempt to restore SL-17823 fix after merge with DRTVWR-559 texture pipeline work removed the getDecodePriority() method.

--- indra/newview/llvlcomposition.cpp
+++ indra/newview/llvlcomposition.cpp
@@ -287,7 +287,7 @@ BOOL LLVLComposition::generateTexture(const F32 x, const F32 y,
 			BOOL delete_raw = (mDetailTextures[i]->reloadRawImage(ddiscard) != NULL) ;
 			if(mDetailTextures[i]->getRawImageLevel() != ddiscard)//raw iamge is not ready, will enter here again later.
 			{
-                if (mDetailTextures[i]->getDecodePriority() <= 0.0f && !mDetailTextures[i]->hasSavedRawImage())
+                if (mDetailTextures[i]->getFetchPriority() <= 0.0f && !mDetailTextures[i]->hasSavedRawImage())
                 {
                     mDetailTextures[i]->setBoostLevel(LLGLTexture::BOOST_MAP);
                     mDetailTextures[i]->forceToRefetchTexture(ddiscard);
