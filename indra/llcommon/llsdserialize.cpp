4df2c0b8d67af267d3c9c6d58d63df3ed063a89e
Kitty Barnett <develop@catznip.com>
2023-02-08 17:17:47 +0100

    Post-merge fix for xcode-14.1 branch

--- indra/llcommon/llsdserialize.cpp
+++ indra/llcommon/llsdserialize.cpp
@@ -2388,7 +2388,7 @@ U8* unzip_llsdNavMesh( bool& valid, size_t& outsize, std::istream& is, S32 size 
 	return result;
 }
 
-char* strip_deprecated_header(char* in, U32& cur_size, U32* header_size)
+char* strip_deprecated_header(char* in, llssize& cur_size, U32* header_size)
 {
 	const char* deprecated_header = "<? LLSD/Binary ?>";
 	constexpr size_t deprecated_header_size = 17;
