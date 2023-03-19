f5adfae1ade864d80f630855bc65bf65f5ae7ece
Alexander Gavriliuk <alexandrgproductengine@lindenlab.com>
2023-03-19 05:07:44 +0100

    SL-19099 The long name of the item is out of bounds on the Editing Tools/Content tab

--- indra/llui/llpanel.h
+++ indra/llui/llpanel.h
@@ -127,6 +127,7 @@ public:
 	virtual 	void	clearCtrls(); // overridden in LLPanelObject and LLPanelVolume
 
 	// Border controls
+	const LLViewBorder* getBorder() const { return mBorder; }
 	void addBorder( LLViewBorder::Params p);
 	void addBorder();
 	void			removeBorder();
