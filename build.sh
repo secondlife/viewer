d65a7c648a516c0e100795cb461eb71ecbe3931b
Nat Goodspeed <nat@lindenlab.com>
2023-04-03 09:29:17 -0400

    DRTVWR-558: Drop extra llbase install; try coding checks on Windows.
    Log output from previous runs confirms that when we 'pip install llbase', it
    shows as already installed in the virtualenv. Now that the fixed llbase has
    been released officially, we no longer need to explicitly specify a beta
    version.
    
    For a long time the viewer's TeamCity build.sh script has only run Linden's
    git coding policy checks on Mac, because it failed on Windows for reasons we
    didn't understand.
    
    Now that we've tweaked the script to install explicitly in our virtualenv,
    it's worth trying on Windows again.

--- build.sh
+++ build.sh
@@ -310,24 +310,13 @@ python_cmd "$helpers/codeticket.py" addinput "Viewer Channel" "${viewer_channel}
 initialize_version # provided by buildscripts build.sh; sets version id
 
 begin_section "coding policy check"
-# On our TC Windows build hosts, the GitPython library underlying our
-# coding_policy_git.py script fails to run git for reasons we have not tried
-# to diagnose. Clearly git works fine on those hosts, or we would never get
-# this far. Running coding policy checks on one platform *should* suffice...
-if [[ "$arch" == "Darwin" ]]
-then
-    # install the git-hooks dependencies in our virtualenv
-    python_cmd -m pip install -r "$(native_path "$git_hooks_checkout/requirements.txt")" || \
-        fatal "pip install git-hooks failed"
-    # validate the branch we're about to build
-    python_cmd "$git_hooks_checkout/coding_policy_git.py" --all_files || \
-        fatal "coding policy check failed"
-fi
+# install the git-hooks dependencies in our virtualenv
+python_cmd -m pip install -r "$(native_path "$git_hooks_checkout/requirements.txt")" || \
+    fatal "pip install git-hooks failed"
+# validate the branch we're about to build
+python_cmd "$git_hooks_checkout/coding_policy_git.py" --all_files || \
+    fatal "coding policy check failed"
 end_section "coding policy check"
-
-# Some build-time tests require llbase. Now that that's no longer implicitly
-# pulled in by autobuild, install it explicitly in our virtualenv.
-python_cmd -m pip install -U llbase==1.3.1b1
 
 # Now run the build
 succeeded=true
