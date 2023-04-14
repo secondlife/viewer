021b531129494910b7c866a68e8bd8ecd8a0d8e0
Cosmic Linden <cosmic@lindenlab.com>
2023-04-14 15:03:36 -0700

    SL-19561: Fix missing shadows on PBR alpha mask animeshes.
    TODO: Why did this work for the equivalent non-rigged prim?

--- indra/newview/pipeline.cpp
+++ indra/newview/pipeline.cpp
@@ -8758,6 +8758,8 @@ void LLPipeline::renderShadow(glh::matrix4f& view, glh::matrix4f& proj, LLCamera
     U32 saved_occlusion = sUseOcclusion;
     sUseOcclusion = 0;
 
+    // List of render pass types that use the prim volume as the shadow,
+    // ignoring textures.
     static const U32 types[] = {
         LLRenderPass::PASS_SIMPLE,
         LLRenderPass::PASS_FULLBRIGHT,
