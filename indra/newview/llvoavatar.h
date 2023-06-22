ca84e0e0c0b4b2844f478a549cb2cb1fbb52c898
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-06-22 22:09:42 +0300

    SL-5161 Avatars should stay hidden longer if they are waiting for meshes or skin data

--- indra/newview/llvoavatar.h
+++ indra/newview/llvoavatar.h
@@ -917,6 +917,7 @@ public:
 	virtual BOOL 		detachObject(LLViewerObject *viewer_object);
 	static bool		    getRiggedMeshID( LLViewerObject* pVO, LLUUID& mesh_id );
 	void				cleanupAttachedMesh( LLViewerObject* pVO );
+    bool                hasPendingAttachedMeshes();
 	static LLVOAvatar*  findAvatarFromAttachment(LLViewerObject* obj);
 	/*virtual*/ BOOL	isWearingWearableType(LLWearableType::EType type ) const;
 	LLViewerObject *	findAttachmentByID( const LLUUID & target_id ) const;
