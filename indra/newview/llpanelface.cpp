ef33902f220df35727ba54ee8bc3ed02bc53d6be
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-08-18 19:59:14 +0300

    SL-20145 GLTF specific feature notification

--- indra/newview/llpanelface.cpp
+++ indra/newview/llpanelface.cpp
@@ -492,6 +492,15 @@ LLPanelFace::~LLPanelFace()
     unloadMedia();
 }
 
+void LLPanelFace::onVisibilityChange(BOOL new_visibility)
+{
+    if (new_visibility)
+    {
+        gAgent.showLatestFeatureNotification("gltf");
+    }
+    LLPanel::onVisibilityChange(new_visibility);
+}
+
 void LLPanelFace::draw()
 {
     updateCopyTexButton();
