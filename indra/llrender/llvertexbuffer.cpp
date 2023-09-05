8d0af77d0b931781fd44e83e672f40fdb1ee458e
Alexander Gavriliuk <alexandrgproductengine@lindenlab.com>
2023-09-05 16:54:03 +0200

    SL-19709 Fix for fullbright shiny not factoring out exposure (update)

--- indra/llrender/llvertexbuffer.cpp
+++ indra/llrender/llvertexbuffer.cpp
@@ -703,7 +703,7 @@ bool LLVertexBuffer::validateRange(U32 start, U32 end, U32 count, U32 indices_of
 
         for (U32 i = start; i <= end; ++i)
         {
-            if (!v->isFinite3())
+            if (!v[i].isFinite3())
             {
                 LL_ERRS() << "Non-finite vertex position data detected." << LL_ENDL;
             }
