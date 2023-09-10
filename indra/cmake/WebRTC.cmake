007b2127b81168071fe44c1a91961ae75487c0bc
Roxie Linden <roxie@lindenlab.com>
2023-09-10 02:03:30 -0700

    more picky cmake

--- indra/cmake/WebRTC.cmake
+++ indra/cmake/WebRTC.cmake
@@ -6,29 +6,29 @@ if (WINDOWS)
     if( ADDRESS_SIZE EQUAL 32 )
         FetchContent_Declare(
               webrtc
-              URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.windows_x86.tar.bz2"
               FIND_PACKAGE_ARGS NAMES webrtc
               DOWNLOAD_EXTRACT_TIMESTAMP TRUE
               DOWNLOAD_DIR "${LIBS_PREBUILT_DIR}/webrtc/"
+              URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.windows_x86.tar.bz2"
               URL_HASH "MD5=0d55e58efceed3fb48085a5f0c58881c"
             )
     else ( ADDRESS_SIZE EQUAL 32 )
         FetchContent_Declare(
               webrtc
-              URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.windows_x86_64.tar.bz2"
               FIND_PACKAGE_ARGS NAMES webrtc
               DOWNLOAD_EXTRACT_TIMESTAMP TRUE
               DOWNLOAD_DIR "${LIBS_PREBUILT_DIR}/webrtc/"
+              URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.windows_x86_64.tar.bz2"
               URL_HASH "MD5=dfb692562770dc8c877ebfe4302e2881"
             )
     endif ( ADDRESS_SIZE EQUAL 32 )
 elseif (DARWIN)
     FetchContent_Declare(
       webrtc
-      URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.macos_x86_64.tar.bz2"
       FIND_PACKAGE_ARGS NAMES webrtc
       DOWNLOAD_EXTRACT_TIMESTAMP TRUE
       DOWNLOAD_DIR "${LIBS_PREBUILT_DIR}/webrtc/"
+      URL "https://webrtc-build-releases.s3.us-west-2.amazonaws.com/webrtc.macos_x86_64.tar.bz2"
       URL_HASH "MD5=cfbcac7da897a862f9791ea29330b814"
     )
 endif (WINDOWS)
