297897a609f62b9a75caff639d44226cc937ec8c
Leviathan Linden <leviathan@lindenlab.com>
2023-09-19 09:40:08 -0700

    add SDL2 as windows/macos dependency

---
+++ indra/cmake/SDL2.cmake
@@ -0,0 +1,18 @@
+# -*- cmake -*-
+cmake_minimum_required( VERSION 3.13 FATAL_ERROR )
+
+include( Prebuilt )
+include_guard()
+
+find_library( SDL2_LIBRARY
+    NAMES SDL2
+    PATHS "${LIBS_PREBUILT_DIR}/lib/release")
+if ( "${SDL2_LIBRARY}" STREQUAL "SDL2_LIBRARY-NOTFOUND" )
+    message( FATAL_ERROR "unable to find SDL2_LIBRARY" )
+endif()
+
+add_library( ll::SDL2 INTERFACE IMPORTED )
+
+target_link_libraries( ll::SDL2 INTERFACE "${SDL2_LIBRARY}" )
+target_include_directories( ll::SDL2 SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include")
+
