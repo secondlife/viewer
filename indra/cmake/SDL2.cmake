92e56c5a07da8db926b416ce9f3c12806029b30a
Leviathan Linden <leviathan@lindenlab.com>
2023-09-26 15:57:30 -0700

    use_prebuilt_binary() on SDL2

--- indra/cmake/SDL2.cmake
+++ indra/cmake/SDL2.cmake
@@ -4,6 +4,11 @@ cmake_minimum_required( VERSION 3.13 FATAL_ERROR )
 include( Prebuilt )
 include_guard()
 
+add_library( ll::SDL2 INTERFACE IMPORTED )
+
+use_system_binary( SDL2 )
+use_prebuilt_binary( SDL2 )
+
 find_library( SDL2_LIBRARY
     NAMES SDL2
     PATHS "${LIBS_PREBUILT_DIR}/lib/release")
@@ -11,8 +16,6 @@ if ( "${SDL2_LIBRARY}" STREQUAL "SDL2_LIBRARY-NOTFOUND" )
     message( FATAL_ERROR "unable to find SDL2_LIBRARY" )
 endif()
 
-add_library( ll::SDL2 INTERFACE IMPORTED )
-
 target_link_libraries( ll::SDL2 INTERFACE "${SDL2_LIBRARY}" )
-target_include_directories( ll::SDL2 SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include")
+target_include_directories( ll::SDL2 SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include" )
 
