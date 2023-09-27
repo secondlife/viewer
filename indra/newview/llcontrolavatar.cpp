fa0b611a9e8d533d3f35bbe1dcab977c5e231b7d
Alexander Gavriliuk <alexandrgproductengine@lindenlab.com>
2023-09-27 11:53:23 +0200

    SL-20345 Avoid of ASSERT (\!mRootVolp) in LLControlAvatar::~LLControlAvatar()

--- indra/newview/llcontrolavatar.cpp
+++ indra/newview/llcontrolavatar.cpp
@@ -362,6 +362,7 @@ void LLControlAvatar::idleUpdate(LLAgent &agent, const F64 &time)
 
 void LLControlAvatar::markDead()
 {
+    mRootVolp = NULL;
     super::markDead();
     mControlAVBridge = NULL;
 }
