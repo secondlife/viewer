4e727c9e874ec5e82d26ce107a9c136578964a35
Leviathan Linden <leviathan@lindenlab.com>
2023-10-02 09:48:23 -0700

    add LLGameControllerManager wrapper for SDL2

---
+++ indra/newview/llgamecontrollermanager.h
@@ -0,0 +1,60 @@
+/**
+ * @file llgamecontrollermanager.h
+ * @brief GameController detection and management
+ *
+ * $LicenseInfo:firstyear=2023&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2023, Linden Research, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "llerror.h"
+#include "llsingleton.h"
+#include "stdtypes.h"
+
+
+// LLGameControllerState is a minimal class for storing axes and buttons values
+class LLGameControllerState
+{
+public:
+    std::vector<S16> mAxes; // [ -32768, 32767 ]
+    std::vector<bool> mButtons;
+};
+
+// LLGameControllerManager is a singleton with pure static public interface
+class LLGameControllerManager : public LLSingleton<LLGameControllerManager>
+{
+	LLSINGLETON_EMPTY_CTOR(LLGameControllerManager);
+	virtual ~LLGameControllerManager();
+    LOG_CLASS(LLGameControllerManager);
+
+public:
+
+    static bool isInitialized();
+	static void init();
+	static void terminate();
+    static void processEvents();
+    static bool hasInput();
+    static const LLGameControllerState& getState();
+};
+
