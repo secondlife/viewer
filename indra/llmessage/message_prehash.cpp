dfb801e0ee8137424166f9aa345fd6350a7e82fd
Leviathan Linden <leviathan@lindenlab.com>
2023-10-04 08:15:32 -0700

    send GameControlInput message with data

--- indra/llmessage/message_prehash.cpp
+++ indra/llmessage/message_prehash.cpp
@@ -1402,3 +1402,5 @@ char const* const _PREHASH_HoverHeight = LLMessageStringTable::getInstance()->ge
 char const* const _PREHASH_Experience = LLMessageStringTable::getInstance()->getString("Experience");
 char const* const _PREHASH_ExperienceID = LLMessageStringTable::getInstance()->getString("ExperienceID");
 char const* const _PREHASH_LargeGenericMessage = LLMessageStringTable::getInstance()->getString("LargeGenericMessage");
+char const* const _PREHASH_GameControlInput = LLMessageStringTable::getInstance()->getString("GameControlInput");
+char const* const _PREHASH_AxisData = LLMessageStringTable::getInstance()->getString("AxisData");
