683e96ed61692d26b3eba2b08fe9978de998cb41
Maxim Nikolenko <maximnproductengine@lindenlab.com>
2023-10-09 12:35:53 +0300

    SL-20380 Add group notices button as hover button on Groups floater

--- indra/newview/llgrouplist.cpp
+++ indra/newview/llgrouplist.cpp
@@ -303,6 +303,7 @@ void LLGroupList::addNewItem(const LLUUID& id, const std::string& name, const LL
 
 	item->getChildView("info_btn")->setVisible( false);
 	item->getChildView("profile_btn")->setVisible( false);
+    item->getChildView("notices_btn")->setVisible(false);
 	item->setGroupIconVisible(mShowIcons);
     if (!mShowIcons)
     {
@@ -403,6 +404,7 @@ mGroupIcon(NULL),
 mGroupNameBox(NULL),
 mInfoBtn(NULL),
 mProfileBtn(NULL),
+mNoticesBtn(NULL),
 mVisibilityHideBtn(NULL),
 mVisibilityShowBtn(NULL),
 mGroupID(LLUUID::null),
@@ -434,6 +436,9 @@ BOOL  LLGroupListItem::postBuild()
 
     mProfileBtn = getChild<LLButton>("profile_btn");
     mProfileBtn->setClickedCallback([this](LLUICtrl *, const LLSD &) { onProfileBtnClick(); });
+
+    mNoticesBtn = getChild<LLButton>("notices_btn");
+    mNoticesBtn->setClickedCallback([this](LLUICtrl *, const LLSD &) { onNoticesBtnClick(); });
 
     mVisibilityHideBtn = findChild<LLButton>("visibility_hide_btn");
     if (mVisibilityHideBtn)
@@ -470,6 +475,7 @@ void LLGroupListItem::onMouseEnter(S32 x, S32 y, MASK mask)
 	{
 		mInfoBtn->setVisible(true);
         mProfileBtn->setVisible(true);
+        mNoticesBtn->setVisible(true);
         if (mForAgent && mVisibilityHideBtn)
         {
             LLGroupData agent_gdatap;
@@ -489,6 +495,7 @@ void LLGroupListItem::onMouseLeave(S32 x, S32 y, MASK mask)
 	getChildView("hovered_icon")->setVisible( false);
 	mInfoBtn->setVisible(false);
     mProfileBtn->setVisible(false);
+    mNoticesBtn->setVisible(false);
     if (mVisibilityHideBtn)
     {
         mVisibilityHideBtn->setVisible(false);
@@ -581,6 +588,11 @@ void LLGroupListItem::onInfoBtnClick()
 void LLGroupListItem::onProfileBtnClick()
 {
 	LLGroupActions::show(mGroupID);
+}
+
+void LLGroupListItem::onNoticesBtnClick()
+{
+    LLGroupActions::show(mGroupID, true);
 }
 
 void LLGroupListItem::onVisibilityBtnClick(bool new_visibility)
