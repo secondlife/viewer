40e568a3180f641fe0feb0e6ddd696c07ed662be
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-10-31 22:16:27 +0200

    SL-20549 Blank no-copy materials are not moved into scripted objects

--- indra/newview/llviewerobject.h
+++ indra/newview/llviewerobject.h
@@ -500,6 +500,7 @@ public:
 	void getInventoryContents(LLInventoryObject::object_list_t& objects);
 	LLInventoryObject* getInventoryRoot();
 	LLViewerInventoryItem* getInventoryItemByAsset(const LLUUID& asset_id);
+    LLViewerInventoryItem* getInventoryItemByAsset(const LLUUID& asset_id, LLAssetType::EType type);
 	S16 getInventorySerial() const { return mInventorySerialNum; }
 
 	// These functions does viewer-side only object inventory modifications
@@ -639,7 +640,7 @@ public:
 
 private:
     void setObjectCostStale();
-    bool isAssetInInventory(LLViewerInventoryItem* item);
+    bool isAssetInInventory(LLViewerInventoryItem* item, LLAssetType::EType type);
 
 	ExtraParameter* createNewParameterEntry(U16 param_type);
 	ExtraParameter* getExtraParameterEntry(U16 param_type) const;
