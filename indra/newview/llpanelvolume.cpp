551d6d7fdef1b0ccbd9305ad0c7426d2de6cf45b
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-11-08 02:05:33 +0200

    SL-20569 Notify user when reflection probe can't be selected #2

--- indra/newview/llpanelvolume.cpp
+++ indra/newview/llpanelvolume.cpp
@@ -746,21 +746,6 @@ void LLPanelVolume::sendIsLight()
 	LL_INFOS() << "update light sent" << LL_ENDL;
 }
 
-void notify_cant_select_reflection_probe()
-{
-    static bool show_notification = true;
-    if (show_notification)
-    {
-        // show only once per session if not ignored
-        show_notification = false;
-        if (!gSavedSettings.getBOOL("SelectReflectionProbes"))
-        {
-            LLNotificationsUtil::add("CantSelectReflectionProbe");
-        }
-        // else: user used the setting, don't show again this session
-    }
-}
-
 void LLPanelVolume::sendIsReflectionProbe()
 {
     LLViewerObject* objectp = mObject;
@@ -779,7 +764,7 @@ void LLPanelVolume::sendIsReflectionProbe()
     }
     else
     {
-        notify_cant_select_reflection_probe();
+        LLNotificationsUtil::add("CantSelectReflectionProbe");
         volobjp->setIsReflectionProbe(value);
     }
 }
@@ -796,7 +781,7 @@ void LLPanelVolume::doSendIsReflectionProbe(const LLSD & notification, const LLS
         }
         LLVOVolume* volobjp = (LLVOVolume*)objectp;
 
-        notify_cant_select_reflection_probe();
+        LLNotificationsUtil::add("CantSelectReflectionProbe");
         volobjp->setIsReflectionProbe(true);
 
         { // has become a reflection probe, slam to a 10m sphere and pop up a message
