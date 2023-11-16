d57c61d8c3138ee693683ca00ea37c760516b11b
Leviathan Linden <leviathan@lindenlab.com>
2023-11-16 13:53:37 -0800

    less verbose settings: GameControlInput --> GameControl

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1363,7 +1363,7 @@ bool packGameControlInput(LLMessageSystem* msg)
     {
         return false;
     }
-    if (!gSavedSettings.getBOOL("EnableGameControlInput"))
+    if (!gSavedSettings.getBOOL("EnableGameControl"))
     {
         LLGameControl::clearAllInput();
         return false;
@@ -1519,7 +1519,7 @@ bool LLAppViewer::doFrame()
                 gKeyboard->scanKeyboard();
                 gViewerInput.scanMouse();
 
-                LLGameControl::setIncludeKeyboardButtons(gSavedSettings.getBOOL("EnableGameControlKeyboardInput"));
+                LLGameControl::setIncludeKeyboardButtons(gSavedSettings.getBOOL("EnableGameControlKeyboard"));
                 LLGameControl::processEvents(gFocusMgr.getAppHasFocus());
                 // to help minimize lag we send GameInput packets immediately
                 // after getting the latest GameController input
