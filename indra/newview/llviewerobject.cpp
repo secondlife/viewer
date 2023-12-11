8c1aa6d6623995e56e1cf5de127befcf9033c8ca
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-12-12 00:15:29 +0200

    SL-20714 Crash accessing mControlAVBridge
    Looks like control avatar was recreated after cleanup then object was
    deleted

--- indra/newview/llviewerobject.cpp
+++ indra/newview/llviewerobject.cpp
@@ -391,6 +391,7 @@ LLViewerObject::~LLViewerObject()
 	sNumObjects--;
 	sNumZombieObjects--;
 	llassert(mChildList.size() == 0);
+    llassert(mControlAvatar.isNull()); // Should have been cleaned by now
 
 	clearInventoryListeners();
 }
@@ -3105,6 +3106,10 @@ void LLViewerObject::updateControlAvatar()
     {
         return;
     }
+
+    // caller isn't supposed to operate on a dead object,
+    // avatar was already cleaned up
+    llassert(!isDead());
 
     bool should_have_control_avatar = false;
     if (is_animated_object)
