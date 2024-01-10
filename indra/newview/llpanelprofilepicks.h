38a89c1fe66a7002b47e3801c074304f0893cba2
Maxim Nikolenko <maximnproductengine@lindenlab.com>
2024-01-10 17:16:00 +0200

    SL-20770 FIXED Picks location field not updating to different parcel/region

--- indra/newview/llpanelprofilepicks.h
+++ indra/newview/llpanelprofilepicks.h
@@ -140,7 +140,9 @@ public:
     void setParcelID(const LLUUID& parcel_id) override { mParcelId = parcel_id; }
     void setErrorStatus(S32 status, const std::string& reason) override {};
 
-protected:
+    void addLocationChangedCallbacks();
+
+  protected:
 
     /**
      * Sends remote parcel info request to resolve parcel name from its ID.
@@ -238,6 +240,9 @@ protected:
     LLUUID mPickId;
     LLUUID mRequestedId;
     std::string mPickNameStr;
+
+    boost::signals2::connection mRegionCallbackConnection;
+    boost::signals2::connection mParcelCallbackConnection;
 
     bool mLocationChanged;
     bool mNewPick;
