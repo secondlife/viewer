04ff8db90cca7a1e152e89fa7fb12f8561498f04
Alexander Gavriliuk <alexandrgproductengine@lindenlab.com>
2024-01-11 20:29:06 +0100

    SL-20756 The emoji in the profile floater is displayed schematically

--- indra/llui/lltextbase.cpp
+++ indra/llui/lltextbase.cpp
@@ -168,7 +168,7 @@ LLTextBase::Params::Params()
 	trusted_content("trusted_content", true),
 	always_show_icons("always_show_icons", false),
 	use_ellipses("use_ellipses", false),
-	use_color("use_color", false),
+	use_color("use_color", true),
 	parse_urls("parse_urls", false),
 	force_urls_external("force_urls_external", false),
 	parse_highlights("parse_highlights", false)
