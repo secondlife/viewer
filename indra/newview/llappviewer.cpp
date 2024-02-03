3f652df0d0ca1981b5d98626825d3ea0813d51bf
leviathan <leviathan@lindenlab.com>
2024-02-02 23:25:24 -0800

    GameControl preferences cleanup

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1121,7 +1121,11 @@ bool LLAppViewer::init()
 	{
 		LLViewerJoystick::getInstance()->init(false);
 	}
+
 	LLGameControl::init();
+    LLGameControl::enableSendToServer(gSavedSettings.getBOOL("GameControlToServer"));
+    LLGameControl::enableControlAvatar(gSavedSettings.getBOOL("GameControlToAvatar"));
+    LLGameControl::enableReceiveControlFromAvatar(gSavedSettings.getBOOL("AvatarToGameControl"));
 
 	try
     {
@@ -4782,9 +4786,6 @@ void LLAppViewer::idle()
 		}
 
 		static LLFrameTimer agent_update_timer;
-
-        // TODO?: move this LLGameControl feature-check to UI callback
-        LLGameControl::setInterpretControlActionsAsGameControl(gSavedSettings.getBOOL("InterpretControlActionsAsGameControl"));
 
         // Note: we process game_control before sending AgentUpdate
         // because it may translate to control flags that control avatar motion.
