0ee93f159c711c8dda701cc09a123464c64aa838
leviathan <leviathan@lindenlab.com>
2024-02-21 11:01:43 -0800

    add camera actions to GameControl preferences

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1125,8 +1125,8 @@ bool LLAppViewer::init()
 
 	LLGameControl::init();
     LLGameControl::enableSendToServer(gSavedSettings.getBOOL("GameControlToServer"));
-    LLGameControl::enableControlAvatar(gSavedSettings.getBOOL("GameControlToAvatar"));
-    LLGameControl::enableReceiveControlFromAvatar(gSavedSettings.getBOOL("AvatarToGameControl"));
+    LLGameControl::enableControlAgent(gSavedSettings.getBOOL("GameControlToAgent"));
+    LLGameControl::enableTranslateAgentActions(gSavedSettings.getBOOL("AgentToGameControl"));
 
 	try
     {
