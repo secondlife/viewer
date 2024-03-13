b6d195a7472f2e9860fb6ebe6a0dbc35200c0ddc
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-03-13 22:15:56 +0200

    SL-20288 Fix renaming
    getChangedIDs is only accurate in scope of observer's callback, don't
    use it onIdle.
    getObject call made no sense, item was warrantied to be
    LLViewerInventoryItem and would only be AT_CATEGORY if it is a link,
    making the following cast to a category dangerous

--- indra/newview/lloutfitslist.h
+++ indra/newview/lloutfitslist.h
@@ -110,6 +110,7 @@ public:
     virtual bool getHasExpandableFolders() = 0;
 
 protected:
+    void observerCallback(const LLUUID& category_id);
     virtual LLOutfitListGearMenuBase* createGearMenu() = 0;
     virtual void onHighlightBaseOutfit(LLUUID base_id, LLUUID prev_id) = 0;
     virtual void onSetSelectedOutfitByUUID(const LLUUID& outfit_uuid) = 0;
@@ -128,6 +129,7 @@ protected:
         uuid_vec_t::const_iterator	AddedIterator;
         uuid_vec_t::const_iterator	RemovedIterator;
     } mRefreshListState;
+    std::set<LLUUID>                mChangedItems;
 
     bool                            mIsInitialized;
     LLInventoryCategoriesObserver* 	mCategoriesObserver;
