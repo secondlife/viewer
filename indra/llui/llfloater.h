35f201f4cb4e8a8d729a1eca62cf4c635fd72fe3
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-03-22 23:04:56 +0200

    viewer#1033 Crash at syncFloaterTabOrder

--- indra/llui/llfloater.h
+++ indra/llui/llfloater.h
@@ -622,7 +622,7 @@ private:
 	S32				mMinimizePositionVOffset;
 	typedef std::vector<std::pair<LLHandle<LLFloater>, boost::signals2::connection> > hidden_floaters_t;
 	hidden_floaters_t mHiddenFloaters;
-	LLFloater *		mFrontChild;
+    LLHandle<LLFloater>	mFrontChildHandle;
 };
 
 //
