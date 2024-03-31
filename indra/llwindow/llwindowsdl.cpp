7fd3c854d35b1e0ce4ec99e2f6bd3fdda4b50a45
Nicky <nicky.dasmijn@posteo.nl>
2024-03-31 20:14:20 +0200

    Remove usage of ndGPU info, as of now there is no 3P for that

--- indra/llwindow/llwindowsdl.cpp
+++ indra/llwindow/llwindowsdl.cpp
@@ -382,12 +382,6 @@ LLWindowSDL::LLWindowSDL(LLWindowCallbacks* callbacks,
     mIsMinimized = -1;
     mFSAASamples = fsaa_samples;
 
-	mGPUInfo = ndgpuinfo::init();
-	if( !mGPUInfo )
-	{
-		LL_WARNS() << "GPUInfo could not be initialized" << LL_ENDL;
-	}
-
 #if LL_X11
     mSDL_XWindowID = None;
     mSDL_Display = NULL;
@@ -948,8 +942,6 @@ LLWindowSDL::~LLWindowSDL()
 {
     quitCursors();
     destroyContext();
-
-	ndgpuinfo::shutdown( mGPUInfo );
 
     if(mSupportedResolutions != NULL)
     {
@@ -2625,17 +2617,5 @@ void LLWindowSDL::toggleVSync(bool enable_vsync)
 
 U32 LLWindowSDL::getAvailableVRAMMegabytes()
 {
-	if( !mGPUInfo )
-	    return 4096;
-
-	auto totalVRAM = ndgpuinfo::getTotalMemory( mGPUInfo )/(1024*1024);
-	auto usedVRAM = ndgpuinfo::getUsedMemory( mGPUInfo )/(1024*1024);
-
-	if( totalVRAM == 0 )
-		return 4096;
-
-	if( totalVRAM <= usedVRAM )
-		return 0;
-
-	return totalVRAM - usedVRAM;
+    return 4096;
 }
--- indra/llwindow/llwindowsdl.h
+++ indra/llwindow/llwindowsdl.h
@@ -34,8 +34,6 @@
 
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_endian.h"
-#include "ndGPUInfo.h"
-
 
 #if LL_X11
 // get X11-specific headers for use in low-level stuff like copy-and-paste support
@@ -48,86 +46,137 @@
 #undef require
 
 
-class LLWindowSDL : public LLWindow
-{
+class LLWindowSDL : public LLWindow {
 public:
     /*virtual*/ void show();
+
     /*virtual*/ void hide();
+
     /*virtual*/ void close();
+
     /*virtual*/ BOOL getVisible();
+
     /*virtual*/ BOOL getMinimized();
+
     /*virtual*/ BOOL getMaximized();
+
     /*virtual*/ BOOL maximize();
+
     /*virtual*/ void minimize();
+
     /*virtual*/ void restore();
+
     /*virtual*/ BOOL getFullscreen();
+
     /*virtual*/ BOOL getPosition(LLCoordScreen *position);
+
     /*virtual*/ BOOL getSize(LLCoordScreen *size);
+
     /*virtual*/ BOOL getSize(LLCoordWindow *size);
+
     /*virtual*/ BOOL setPosition(LLCoordScreen position);
+
     /*virtual*/ BOOL setSizeImpl(LLCoordScreen size);
+
     /*virtual*/ BOOL setSizeImpl(LLCoordWindow size);
-    /*virtual*/ BOOL switchContext(BOOL fullscreen, const LLCoordScreen &size, BOOL disable_vsync, const LLCoordScreen * const posp = NULL);
+
+    /*virtual*/ BOOL switchContext(BOOL fullscreen, const LLCoordScreen &size, BOOL disable_vsync,
+                                   const LLCoordScreen *const posp = NULL);
+
     /*virtual*/ BOOL setCursorPosition(LLCoordWindow position);
+
     /*virtual*/ BOOL getCursorPosition(LLCoordWindow *position);
+
     /*virtual*/ void showCursor();
+
     /*virtual*/ void hideCursor();
+
     /*virtual*/ void showCursorFromMouseMove();
+
     /*virtual*/ void hideCursorUntilMouseMove();
+
     /*virtual*/ BOOL isCursorHidden();
+
     /*virtual*/ void updateCursor();
+
     /*virtual*/ void captureMouse();
+
     /*virtual*/ void releaseMouse();
-    /*virtual*/ void setMouseClipping( BOOL b );
-    /*virtual*/	void setMinSize(U32 min_width, U32 min_height, bool enforce_immediately = true);
+
+    /*virtual*/ void setMouseClipping(BOOL b);
+
+    /*virtual*/    void setMinSize(U32 min_width, U32 min_height, bool enforce_immediately = true);
 
     /*virtual*/ BOOL isClipboardTextAvailable();
+
     /*virtual*/ BOOL pasteTextFromClipboard(LLWString &dst);
-    /*virtual*/ BOOL copyTextToClipboard(const LLWString & src);
+
+    /*virtual*/ BOOL copyTextToClipboard(const LLWString &src);
 
     /*virtual*/ BOOL isPrimaryTextAvailable();
+
     /*virtual*/ BOOL pasteTextFromPrimary(LLWString &dst);
-    /*virtual*/ BOOL copyTextToPrimary(const LLWString & src);
+
+    /*virtual*/ BOOL copyTextToPrimary(const LLWString &src);
 
     /*virtual*/ void flashIcon(F32 seconds);
+
     /*virtual*/ F32 getGamma();
+
     /*virtual*/ BOOL setGamma(const F32 gamma); // Set the gamma
     /*virtual*/ U32 getFSAASamples();
+
     /*virtual*/ void setFSAASamples(const U32 samples);
-    /*virtual*/ BOOL restoreGamma();			// Restore original gamma table (before updating gamma)
+
+    /*virtual*/ BOOL restoreGamma();            // Restore original gamma table (before updating gamma)
     /*virtual*/ ESwapMethod getSwapMethod() { return mSwapMethod; }
+
     /*virtual*/ void processMiscNativeEvents();
+
     /*virtual*/ void gatherInput();
+
     /*virtual*/ void swapBuffers();
+
     /*virtual*/ void restoreGLContext() {};
 
-    /*virtual*/ void delayInputProcessing() { };
+    /*virtual*/ void delayInputProcessing() {};
 
     // handy coordinate space conversion routines
     /*virtual*/ BOOL convertCoords(LLCoordScreen from, LLCoordWindow *to);
+
     /*virtual*/ BOOL convertCoords(LLCoordWindow from, LLCoordScreen *to);
+
     /*virtual*/ BOOL convertCoords(LLCoordWindow from, LLCoordGL *to);
+
     /*virtual*/ BOOL convertCoords(LLCoordGL from, LLCoordWindow *to);
+
     /*virtual*/ BOOL convertCoords(LLCoordScreen from, LLCoordGL *to);
+
     /*virtual*/ BOOL convertCoords(LLCoordGL from, LLCoordScreen *to);
 
-    /*virtual*/ LLWindowResolution* getSupportedResolutions(S32 &num_resolutions);
-    /*virtual*/ F32	getNativeAspectRatio();
+    /*virtual*/ LLWindowResolution *getSupportedResolutions(S32 &num_resolutions);
+
+    /*virtual*/ F32 getNativeAspectRatio();
+
     /*virtual*/ F32 getPixelAspectRatio();
+
     /*virtual*/ void setNativeAspectRatio(F32 ratio) { mOverrideAspectRatio = ratio; }
 
     /*virtual*/ void beforeDialog();
+
     /*virtual*/ void afterDialog();
 
     /*virtual*/ BOOL dialogColorPicker(F32 *r, F32 *g, F32 *b);
 
     /*virtual*/ void *getPlatformWindow();
+
     /*virtual*/ void bringToFront();
 
-    /*virtual*/ void spawnWebBrowser(const std::string& escaped_url, bool async);
-    /*virtual*/ void openFile(const std::string& file_name);
+    /*virtual*/ void spawnWebBrowser(const std::string &escaped_url, bool async);
 
-    /*virtual*/ void setTitle(const std::string& title);
+    /*virtual*/ void openFile(const std::string &file_name);
+
+    /*virtual*/ void setTitle(const std::string &title);
 
     static std::vector<std::string> getDynamicFallbackFontList();
 
@@ -137,46 +186,59 @@ public:
     Window mSDL_XWindowID;
     Display *mSDL_Display;
 #endif
+
     void (*Lock_Display)(void);
+
     void (*Unlock_Display)(void);
 
 #if LL_GTK
     // Lazily initialize and check the runtime GTK version for goodness.
-	static bool ll_try_gtk_init(void);
+    static bool ll_try_gtk_init(void);
 #endif // LL_GTK
 
 #if LL_X11
+
     static Window get_SDL_XWindowID(void);
-    static Display* get_SDL_Display(void);
+
+    static Display *get_SDL_Display(void);
+
 #endif // LL_X11
 
-    void* createSharedContext() override;
-    void makeContextCurrent(void* context) override;
-    void destroySharedContext(void* context) override;
+    void *createSharedContext() override;
+
+    void makeContextCurrent(void *context) override;
+
+    void destroySharedContext(void *context) override;
+
     void toggleVSync(bool enable_vsync) override;
+
     U32 getAvailableVRAMMegabytes() override;
 
 protected:
-    LLWindowSDL(LLWindowCallbacks* callbacks,
-                const std::string& title, int x, int y, int width, int height, U32 flags,
+    LLWindowSDL(LLWindowCallbacks *callbacks,
+                const std::string &title, int x, int y, int width, int height, U32 flags,
                 BOOL fullscreen, BOOL clearBg, BOOL disable_vsync, BOOL use_gl,
                 BOOL ignore_pixel_depth, U32 fsaa_samples);
+
     ~LLWindowSDL();
 
-    /*virtual*/ BOOL	isValid();
-    /*virtual*/ LLSD    getNativeKeyData();
+    /*virtual*/ BOOL isValid();
 
-    void	initCursors();
-    void	quitCursors();
-    void	moveWindow(const LLCoordScreen& position,const LLCoordScreen& size);
+    /*virtual*/ LLSD getNativeKeyData();
+
+    void initCursors();
+
+    void quitCursors();
+
+    void moveWindow(const LLCoordScreen &position, const LLCoordScreen &size);
 
     // Changes display resolution. Returns true if successful
-    BOOL	setDisplayResolution(S32 width, S32 height, S32 bits, S32 refresh);
+    BOOL setDisplayResolution(S32 width, S32 height, S32 bits, S32 refresh);
 
     // Go back to last fullscreen display resolution.
-    BOOL	setFullscreenResolution();
+    BOOL setFullscreenResolution();
 
-    BOOL	shouldPostQuit() { return mPostQuit; }
+    BOOL shouldPostQuit() { return mPostQuit; }
 
 protected:
     //
@@ -185,41 +247,48 @@ protected:
 
     // create or re-create the GL context/window.  Called from the constructor and switchContext().
     BOOL createContext(int x, int y, int width, int height, int bits, BOOL fullscreen, BOOL disable_vsync);
+
     void destroyContext();
-    void setupFailure(const std::string& text, const std::string& caption, U32 type);
+
+    void setupFailure(const std::string &text, const std::string &caption, U32 type);
+
     void fixWindowSize(void);
+
     U32 SDLCheckGrabbyKeys(U32 keysym, BOOL gain);
+
     BOOL SDLReallyCaptureInput(BOOL capture);
 
     //
     // Platform specific variables
     //
-    U32             mGrabbyKeyFlags;
-    int			mReallyCapturedCount;
+    U32 mGrabbyKeyFlags;
+    int mReallyCapturedCount;
 
-    SDL_Window* mWindow;
-    SDL_Surface* mSurface;
+    SDL_Window *mWindow;
+    SDL_Surface *mSurface;
     SDL_GLContext mContext;
-    SDL_Cursor*	mSDLCursors[UI_CURSOR_COUNT];
+    SDL_Cursor *mSDLCursors[UI_CURSOR_COUNT];
 
     std::string mWindowTitle;
-    double		mOriginalAspectRatio;
-    BOOL		mNeedsResize;		// Constructor figured out the window is too big, it needs a resize.
-    LLCoordScreen   mNeedsResizeSize;
-    F32			mOverrideAspectRatio;
-    F32		mGamma;
-    U32		mFSAASamples;
+    double mOriginalAspectRatio;
+    BOOL mNeedsResize;        // Constructor figured out the window is too big, it needs a resize.
+    LLCoordScreen mNeedsResizeSize;
+    F32 mOverrideAspectRatio;
+    F32 mGamma;
+    U32 mFSAASamples;
 
-    int		mSDLFlags;
+    int mSDLFlags;
 
-    int             mHaveInputFocus; /* 0=no, 1=yes, else unknown */
-    int             mIsMinimized; /* 0=no, 1=yes, else unknown */
+    int mHaveInputFocus; /* 0=no, 1=yes, else unknown */
+    int mIsMinimized; /* 0=no, 1=yes, else unknown */
 
     friend class LLWindowManager;
 
 private:
 #if LL_X11
+
     void x11_set_urgent(BOOL urgent);
+
     BOOL mFlashing;
     LLTimer mFlashTimer;
 #endif //LL_X11
@@ -230,26 +299,32 @@ private:
 
 public:
 #if LL_X11
-    static Display* getSDLDisplay();
-    LLWString const& getPrimaryText() const { return mPrimaryClipboard; }
-    LLWString const& getSecondaryText() const { return mSecondaryClipboard; }
-    void clearPrimaryText()  { mPrimaryClipboard.clear(); }
+
+    static Display *getSDLDisplay();
+
+    LLWString const &getPrimaryText() const { return mPrimaryClipboard; }
+
+    LLWString const &getSecondaryText() const { return mSecondaryClipboard; }
+
+    void clearPrimaryText() { mPrimaryClipboard.clear(); }
+
     void clearSecondaryText() { mSecondaryClipboard.clear(); }
+
 private:
-    void tryFindFullscreenSize( int &aWidth, int &aHeight );
+    void tryFindFullscreenSize(int &aWidth, int &aHeight);
+
     void initialiseX11Clipboard();
 
-    bool getSelectionText(Atom selection, LLWString& text);
-    bool getSelectionText( Atom selection, Atom type, LLWString &text );
+    bool getSelectionText(Atom selection, LLWString &text);
 
-    bool setSelectionText(Atom selection, const LLWString& text);
+    bool getSelectionText(Atom selection, Atom type, LLWString &text);
+
+    bool setSelectionText(Atom selection, const LLWString &text);
+
 #endif
     LLWString mPrimaryClipboard;
     LLWString mSecondaryClipboard;
-
-	ndgpuinfo::gpu_info *mGPUInfo{nullptr};
 };
-
 
 class LLSplashScreenSDL : public LLSplashScreen
 {
