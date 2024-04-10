4f2f328f70e465d452bf33722f37e49f7d5813a7
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-04-11 00:34:54 +0300

    triage#171 Sometimes conversation.log remains empty

--- indra/newview/llimview.cpp
+++ indra/newview/llimview.cpp
@@ -1572,6 +1572,8 @@ bool LLIMModel::logToFile(const std::string& file_name, const std::string& from,
 	}
 	else
 	{
+        // will check KeepConversationLogTranscripts on its own
+        LLConversationLog::instance().cache();
 		return false;
 	}
 }
