33c7b9701de1589e8e3875656a6bab4f8710e7a8
Nicky Dasmijn <nicky.dasmijn@posteo.nl>
2024-04-12 14:15:01 +0200

    Chore/cmake pretty up (#1205)
    * - Add an option to with ASAN
    - Fix GCC -Werror option.
    
    * - find_program should not set REQUIRED if per logic a fallback to ld.bfd is fine
    - cmake idiomatic is to test Variables without dereferencing  them
    
    * Add -Wno-unknown-warning-option for clang or it will complain about some GCC only options
    
    ```
    /home/runner/work/viewer/viewer/indra/llcommon/llsdutil.cpp:39:32: error: unknown warning group '-Wstringop-truncation', ignored [-Werror,-Wunknown-warning-option]
    #pragma GCC diagnostic ignored "-Wstringop-truncation" // It's actually okay what happens here
    ```

--- indra/cmake/Linker.cmake
+++ indra/cmake/Linker.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
-if (${LINK_WITH_MOLD})
-  find_program(MOLD_BIN mold REQUIRED)
+if( LINK_WITH_MOLD )
+  find_program(MOLD_BIN mold)
   if(MOLD_BIN)
     message(STATUS "Mold linker found: ${MOLD_BIN}. Enabling mold as active linker.")
     add_link_options("-fuse-ld=${MOLD_BIN}")
