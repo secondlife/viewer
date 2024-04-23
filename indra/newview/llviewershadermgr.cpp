1bdbdfc484839794d44099fe15de853562d014e3
Brad Linden <brad@lindenlab.com>
2024-04-23 14:57:59 -0700

    Clean up LLGLSLShader::sIndexedTextureChannels usage and improve fix for SL-20652

--- indra/newview/llviewershadermgr.cpp
+++ indra/newview/llviewershadermgr.cpp
@@ -423,7 +423,7 @@ void LLViewerShaderMgr::setShaders()
     static LLCachedControl<U32> max_texture_index(gSavedSettings, "RenderMaxTextureIndex", 16);
     
     // when using indexed texture rendering, leave some texture units available for shadow and reflection maps
-    static LLCachedControl<S32> reserved_texture_units(gSavedSettings, "RenderReservedTextureIndices", 9);
+    static LLCachedControl<S32> reserved_texture_units(gSavedSettings, "RenderReservedTextureIndices", 14);
 
     LLGLSLShader::sIndexedTextureChannels =
         llclamp<S32>(max_texture_index, 1, gGLManager.mNumTextureImageUnits-reserved_texture_units);
