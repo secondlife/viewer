5f8a7374b9f18e0112d6749a9c845bd077a81acb
Andrey Lihatskiy <alihatskiy@productengine.com>
2024-05-15 15:08:37 +0300

    Temporarily disable the tests to unblock Maint B linux builds

--- build.sh
+++ build.sh
@@ -194,7 +194,6 @@ pre_build()
 
     "$autobuild" configure --quiet -c $variant \
      ${eval_autobuild_configure_parameters:---} \
-     -DLL_TESTS:BOOL=ON \
      -DPACKAGE:BOOL=ON \
      -DHAVOK:BOOL="$HAVOK" \
      -DRELEASE_CRASH_REPORTING:BOOL="$RELEASE_CRASH_REPORTING" \
