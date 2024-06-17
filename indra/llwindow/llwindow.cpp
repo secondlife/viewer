46355d52de448c39e1a965c1a05d4f4bc9ebe90f
Ansariel <ansariel.hiller@phoenixviewer.com>
2024-06-17 15:42:06 +0200

    Remove orphaned VRAM handling code

--- indra/llwindow/llwindow.cpp
+++ indra/llwindow/llwindow.cpp
@@ -409,7 +409,6 @@ LLWindow* LLWindowManager::createWindow(
     bool ignore_pixel_depth,
     U32 fsaa_samples,
     U32 max_cores,
-    U32 max_vram,
     F32 max_gl_version)
 {
     LLWindow* new_window;
@@ -427,11 +426,11 @@ LLWindow* LLWindowManager::createWindow(
 #elif LL_WINDOWS
         new_window = new LLWindowWin32(callbacks,
             title, name, x, y, width, height, flags,
-            fullscreen, clearBg, enable_vsync, use_gl, ignore_pixel_depth, fsaa_samples, max_cores, max_vram, max_gl_version);
+            fullscreen, clearBg, enable_vsync, use_gl, ignore_pixel_depth, fsaa_samples, max_cores, max_gl_version);
 #elif LL_DARWIN
         new_window = new LLWindowMacOSX(callbacks,
             title, name, x, y, width, height, flags,
-            fullscreen, clearBg, enable_vsync, use_gl, ignore_pixel_depth, fsaa_samples, max_vram);
+            fullscreen, clearBg, enable_vsync, use_gl, ignore_pixel_depth, fsaa_samples);
 #endif
     }
     else
