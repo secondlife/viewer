3fa6db9603a020402eea8c61622b0cab27dbcca3
Rye Mutt <rye@alchemyviewer.org>
2024-07-01 22:50:39 -0400

    Reduce string temporaries from LLTrans

--- indra/newview/lllogininstance.cpp
+++ indra/newview/lllogininstance.cpp
@@ -447,7 +447,7 @@ void LLLoginInstance::handleLoginFailure(const LLSD& event)
             gViewerWindow->setShowProgress(false);
         }
 
-        showMFAChallange(LLTrans::getString(response["message_id"]));
+        showMFAChallange(LLTrans::getString(response["message_id"].asString()));
     }
     else if(   reason_response == "key"
             || reason_response == "presence"
