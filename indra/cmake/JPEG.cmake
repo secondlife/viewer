e9a370fd2ab205e1ffcb6f3a981f0041fe640f0b
Rye Mutt <rye@alchemyviewer.org>
2024-08-01 16:24:31 -0400

    Update from jpeglib to libjpeg-turbo 3.0.3

--- indra/cmake/JPEG.cmake
+++ indra/cmake/JPEG.cmake
@@ -7,12 +7,14 @@ include_guard()
 add_library( ll::libjpeg INTERFACE IMPORTED )
 
 use_system_binary(libjpeg)
-use_prebuilt_binary(jpeglib)
+use_prebuilt_binary(libjpeg-turbo)
 if (LINUX)
-  target_link_libraries( ll::libjpeg INTERFACE jpeg)
+  target_link_libraries( ll::libjpeg INTERFACE ${ARCH_PREBUILT_DIRS_RELEASE}/libjpeg.a)
 elseif (DARWIN)
-  target_link_libraries( ll::libjpeg INTERFACE jpeg)
+  target_link_libraries( ll::libjpeg INTERFACE ${ARCH_PREBUILT_DIRS_RELEASE}/libjpeg.a)
 elseif (WINDOWS)
-  target_link_libraries( ll::libjpeg INTERFACE jpeglib)
+    target_link_libraries( ll::libjpeg INTERFACE
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/jpeg.lib
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/jpeg.lib)
 endif (LINUX)
 target_include_directories( ll::libjpeg SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include)
