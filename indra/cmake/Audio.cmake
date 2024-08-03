637d81b9dd4912624ba5f220bec0ef3c7d0245a9
Rye Mutt <rye@alchemyviewer.org>
2024-08-02 21:55:41 -0400

    Update ogg_vorbis to 1.3.5-1.3.7

--- indra/cmake/Audio.cmake
+++ indra/cmake/Audio.cmake
@@ -1,4 +1,5 @@
 # -*- cmake -*-
+include(Linking)
 include(Prebuilt)
 
 include_guard()
@@ -9,8 +10,22 @@ use_prebuilt_binary(ogg_vorbis)
 target_include_directories( ll::vorbis SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include )
 
 if (WINDOWS)
-  target_link_libraries(ll::vorbis INTERFACE ogg_static vorbis_static vorbisenc_static vorbisfile_static )
+  target_link_libraries(ll::vorbis INTERFACE
+        optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libogg.lib
+        debug ${ARCH_PREBUILT_DIRS_DEBUG}/libogg.lib
+        optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbisenc.lib
+        debug ${ARCH_PREBUILT_DIRS_DEBUG}/libvorbisenc.lib
+        optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbisfile.lib
+        debug ${ARCH_PREBUILT_DIRS_DEBUG}/libvorbisfile.lib
+        optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbis.lib
+        debug ${ARCH_PREBUILT_DIRS_DEBUG}/libvorbis.lib
+    )
 else (WINDOWS)
-  target_link_libraries(ll::vorbis INTERFACE ogg vorbis vorbisenc vorbisfile )
+  target_link_libraries(ll::vorbis INTERFACE
+        ${ARCH_PREBUILT_DIRS_RELEASE}/libogg.a
+        ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbisenc.a
+        ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbisfile.a
+        ${ARCH_PREBUILT_DIRS_RELEASE}/libvorbis.a
+        )
 endif (WINDOWS)
 
