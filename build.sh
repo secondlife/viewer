ece9cb8022d1c889bfd02ce52f0dd470460d5a21
Roxie Linden <roxie@lindenlab.com>
2024-08-06 18:41:46 -0700

    Build an xcarchive for mac symbol upload.

--- build.sh
+++ build.sh
@@ -526,8 +526,8 @@ then
     # nat 2016-12-22: without RELEASE_CRASH_REPORTING, we have no symbol file.
     if [ "${RELEASE_CRASH_REPORTING:-}" != "OFF" ]
     then
-        # e.g. build-darwin-x86_64/newview/Release/Second Life Test.sym.tar.gz
-        symbol_file="${build_dir}/newview/${variant}/${viewer_channel}.sym.tar.gz"
+        # e.g. build-darwin-x86_64/symbols/Release/Second Life Test.xarchive.zip
+        symbol_file="${build_dir}/symbols/${variant}/${viewer_channel}.xarchive.zip"
         if [[ ! -f "$symbol_file" ]]
         then
             # symbol tarball we prep for (e.g.) Breakpad
