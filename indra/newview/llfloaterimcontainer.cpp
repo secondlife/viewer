7cc3ff55b97b94a3b52daebfe072eb22192c710b
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-08-16 19:27:48 +0300

    viewer#2296 Don't show 'are you sure you want to leave the call' when shutting down

--- indra/newview/llfloaterimcontainer.cpp
+++ indra/newview/llfloaterimcontainer.cpp
@@ -2432,7 +2432,7 @@ void LLFloaterIMContainer::closeHostedFloater()
     onClickCloseBtn();
 }
 
-void LLFloaterIMContainer::closeAllConversations()
+void LLFloaterIMContainer::closeAllConversations(bool app_quitting)
 {
     std::vector<LLUUID> ids;
     for (conversations_items_map::iterator it_session = mConversationsItems.begin(); it_session != mConversationsItems.end(); it_session++)
@@ -2447,7 +2447,7 @@ void LLFloaterIMContainer::closeAllConversations()
     for (std::vector<LLUUID>::const_iterator it = ids.begin(); it != ids.end();     ++it)
     {
         LLFloaterIMSession *conversationFloater = LLFloaterIMSession::findInstance(*it);
-        LLFloater::onClickClose(conversationFloater);
+        LLFloater::onClickClose(conversationFloater, app_quitting);
     }
 }
 
@@ -2470,7 +2470,7 @@ void LLFloaterIMContainer::closeFloater(bool app_quitting/* = false*/)
 {
     if(app_quitting)
     {
-        closeAllConversations();
+        closeAllConversations(app_quitting);
         onClickCloseBtn(app_quitting);
     }
     else
