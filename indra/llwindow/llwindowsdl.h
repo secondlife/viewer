94367a8d827ff9d5432635e7c13a1d7e014ef25a
Ansariel <ansariel.hiller@phoenixviewer.com>
2024-09-04 21:07:38 +0200

    Fix name of vsync param in LLWindowSDL

--- indra/llwindow/llwindowsdl.h
+++ indra/llwindow/llwindowsdl.h
@@ -80,7 +80,7 @@ public:
 
     bool setSizeImpl(LLCoordWindow size) override;
 
-    bool switchContext(bool fullscreen, const LLCoordScreen &size, bool disable_vsync,
+    bool switchContext(bool fullscreen, const LLCoordScreen &size, bool enable_vsync,
                                    const LLCoordScreen *const posp = NULL) override;
 
     bool setCursorPosition(LLCoordWindow position) override;
@@ -212,7 +212,7 @@ public:
 protected:
     LLWindowSDL(LLWindowCallbacks *callbacks,
                 const std::string &title, int x, int y, int width, int height, U32 flags,
-                bool fullscreen, bool clearBg, bool disable_vsync, bool use_gl,
+                bool fullscreen, bool clearBg, bool enable_vsync, bool use_gl,
                 bool ignore_pixel_depth, U32 fsaa_samples);
 
     ~LLWindowSDL();
@@ -241,7 +241,7 @@ protected:
     //
 
     // create or re-create the GL context/window.  Called from the constructor and switchContext().
-    bool createContext(int x, int y, int width, int height, int bits, bool fullscreen, bool disable_vsync);
+    bool createContext(int x, int y, int width, int height, int bits, bool fullscreen, bool enable_vsync);
 
     void destroyContext();
 
