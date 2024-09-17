fd843d514a4e28f8e4a5d5595bba21ccad195e72
Dave Parks <davep@lindenlab.com>
2024-09-17 13:07:01 -0500

    Mac pass (#2587)

--- indra/cmake/Tracy.cmake
+++ indra/cmake/Tracy.cmake
@@ -6,7 +6,7 @@ add_library( ll::tracy INTERFACE IMPORTED )
 
 # default Tracy profiling on for test builds, but off for all others
 string(TOLOWER ${VIEWER_CHANNEL} channel_lower)
-if(WINDOWS AND channel_lower MATCHES "^second life test")
+if(channel_lower MATCHES "^second life test")
   option(USE_TRACY "Use Tracy profiler." ON)
 else()
     option(USE_TRACY "Use Tracy profiler." OFF)
