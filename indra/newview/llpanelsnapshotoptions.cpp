85cf1d7ebefe41eaad8b71cfd6d0d2fac72423ec
Ansariel <ansariel.hiller@phoenixviewer.com>
2024-09-25 23:12:27 +0200

    Remove incorrectly calculated upload fee from snapshot options button based on previously selected snapshot type and not necessarily snapshot to inventory

--- indra/newview/llpanelsnapshotoptions.cpp
+++ indra/newview/llpanelsnapshotoptions.cpp
@@ -30,11 +30,7 @@
 #include "llsidetraypanelcontainer.h"
 
 #include "llfloatersnapshot.h" // FIXME: create a snapshot model
-#include "llsnapshotlivepreview.h"
 #include "llfloaterreg.h"
-
-#include "llagentbenefits.h"
-
 
 /**
  * Provides several ways to save a snapshot.
@@ -46,12 +42,9 @@ class LLPanelSnapshotOptions
 
 public:
     LLPanelSnapshotOptions();
-    ~LLPanelSnapshotOptions();
     bool postBuild() override;
-    void onOpen(const LLSD& key) override;
 
 private:
-    void updateUploadCost();
     void openPanel(const std::string& panel_name);
     void onSaveToProfile();
     void onSaveToEmail();
@@ -71,39 +64,11 @@ LLPanelSnapshotOptions::LLPanelSnapshotOptions()
     mCommitCallbackRegistrar.add("Snapshot.SaveToComputer",     boost::bind(&LLPanelSnapshotOptions::onSaveToComputer,  this));
 }
 
-LLPanelSnapshotOptions::~LLPanelSnapshotOptions()
-{
-}
-
 // virtual
 bool LLPanelSnapshotOptions::postBuild()
 {
     mSnapshotFloater = getParentByType<LLFloaterSnapshotBase>();
     return LLPanel::postBuild();
-}
-
-// virtual
-void LLPanelSnapshotOptions::onOpen(const LLSD& key)
-{
-    updateUploadCost();
-}
-
-void LLPanelSnapshotOptions::updateUploadCost()
-{
-    S32 w = 0;
-    S32 h = 0;
-
-    if( mSnapshotFloater )
-    {
-        LLSnapshotLivePreview* preview = mSnapshotFloater->getPreviewView();
-        if( preview )
-        {
-            preview->getSize(w, h);
-        }
-    }
-
-    S32 upload_cost = LLAgentBenefitsMgr::current().getTextureUploadCost(w, h);
-    getChild<LLUICtrl>("save_to_inventory_btn")->setLabelArg("[AMOUNT]", llformat("%d", upload_cost));
 }
 
 void LLPanelSnapshotOptions::openPanel(const std::string& panel_name)
