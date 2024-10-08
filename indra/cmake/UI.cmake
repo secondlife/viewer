a1e1b7217d6e6c6f18a54547252f12b22ae83e84
AiraYumi <aira.youme@airanyumi.net>
2024-10-08 13:00:16 +0900

    Remove FLTK

--- indra/cmake/UI.cmake
+++ indra/cmake/UI.cmake
@@ -6,8 +6,7 @@ include(GLIB)
 add_library( ll::uilibraries INTERFACE IMPORTED )
 
 if (LINUX)
-  use_prebuilt_binary(fltk)
-  target_compile_definitions(ll::uilibraries INTERFACE LL_FLTK=1 LL_X11=1 )
+  target_compile_definitions(ll::uilibraries INTERFACE LL_X11=1 )
 
   if( USE_CONAN )
     return()
