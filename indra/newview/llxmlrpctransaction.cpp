a2c86616360fe51e8d766796bed80e73444823d5
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-10-29 23:02:46 +0200

    viewer#2972 Fix viewer stripping quotes off folder names

--- indra/newview/llxmlrpctransaction.cpp
+++ indra/newview/llxmlrpctransaction.cpp
@@ -313,6 +313,8 @@ bool LLXMLRPCTransaction::Impl::process()
     if (mHasResponse && !mResponseParsed)
     {
         LLXMLNodePtr root;
+        bool strip_escaped_strings = LLXMLNode::sStripEscapedStrings;
+        LLXMLNode::sStripEscapedStrings = false;
         if (!LLXMLNode::parseBuffer(mResponseText.data(), mResponseText.size(),
                                     root, nullptr))
         {
@@ -329,6 +331,7 @@ bool LLXMLRPCTransaction::Impl::process()
             LL_WARNS() << "XMLRPC response parsing failed; request URI: "
                        << mURI << LL_ENDL;
         }
+        LLXMLNode::sStripEscapedStrings = strip_escaped_strings;
         mResponseParsed = true;
     }
 
