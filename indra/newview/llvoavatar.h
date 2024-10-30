d1a4cbb8fa9058c4d3a8fc3ebe3a543626aac77e
Rye <rye@lindenlab.com>
2024-10-29 20:24:40 -0700

    Fixes to LLCachedControl usage in windowing and avatars

--- indra/newview/llvoavatar.h
+++ indra/newview/llvoavatar.h
@@ -668,8 +668,6 @@ private:
     LLVector3   mLastAnimExtents[2];
     LLVector3   mLastAnimBasePos;
 
-    LLCachedControl<bool> mRenderUnloadedAvatar;
-
     //--------------------------------------------------------------------
     // Wind rippling in clothes
     //--------------------------------------------------------------------
