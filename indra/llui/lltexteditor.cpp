677aa2aa024a548ff2927b07b2406ff70e47ca1c
Alexander Gavriliuk <alexandrgproductengine@lindenlab.com>
2024-11-26 16:13:35 +0100

    #3096 Shift+Tab selects extra lines on script edit on reduce indent

--- indra/llui/lltexteditor.cpp
+++ indra/llui/lltexteditor.cpp
@@ -581,7 +581,7 @@ S32 LLTextEditor::indentLine( S32 pos, S32 spaces )
             LLWString wtext = getWText();
             if (wtext[pos] == ' ')
             {
-                delta_spaces += remove( pos, 1, false );
+                delta_spaces -= remove( pos, 1, false );
             }
         }
     }
