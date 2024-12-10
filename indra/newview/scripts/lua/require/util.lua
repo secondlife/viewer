59f838762a46879d102f9d3c99e078fc15ec9f41
Nat Goodspeed <nat@lindenlab.com>
2024-12-10 15:35:11 -0500

    Add a number of functions to util.lua.
    util.bind(function, boundargs...) returns a function f which, when called as
    f(moreargs...), invokes function(boundargs..., moreargs...).
    
    util.bindclass(class, 'method') returns a function f which, when called as
    f(args...), invokes class.method(class, args...). This is more or less what
    you'd expect from 'class:method' when not actually calling it, except that Lua
    doesn't do that.
    
    util.callable(somefunc) tests whether somefunc is callable according to Lua
    rules: either a function or a table with a __call() metafunction.
    
    util.class() facilitates creating a Lua class, or a subclass. The data
    structures for this are a bit finicky, and what we've been using actually
    wasn't correct. Add test_class.lua to exercise util.class().
    
    util.fromvector(v) converts from a vector returned by the Luau vector library
    to an {x, y, z} table. vector doesn't (yet) implicitly convert to LLSD.
    
    util.notfunc(function) returns a function f which, when called as f(args...),
    returns (not function(args...)). A small but important special case is that
    when function(args...) returns a truthy value, f(args...) returns nil instead
    of false. Certain modules distinguish nil from non-nil, rather than truthy
    from falsey.
    
    util.tovector(x, y, z) converts the passed coordinates to a Luau vector, a la
    vector.create(). But you can also pass tovector() an existing {x, y, z} table,
    e.g. obtained via LLSD, to convert that to a Luau vector.

--- indra/newview/scripts/lua/require/util.lua
+++ indra/newview/scripts/lua/require/util.lua
@@ -2,6 +2,56 @@
 
 local util = {}
 
+-- bind(func, args...) returns a function f such that f(moreargs) calls
+-- func(args..., moreargs).
+function util.bind(func, ...)
+    local bound = table.pack(...)
+    return function(...)
+        -- What we want here is simply
+        -- return func(table.unpack(bound), ...)
+        -- But Lua only forwards a multi-valued expression as additional
+        -- arguments if that expression is LAST in the function call argument
+        -- list. Have to engage in more elaborate table manipulation.
+        local args = table.pack(...)
+        -- Don't modify the original bound arguments! Otherwise we'll impact
+        -- all subsequent calls!
+        local boundplus = table.clone(bound)
+        -- Append 'args' to 'boundplus'
+        table.move(args, 1, #args, #boundplus+1, boundplus)
+        -- Now we can expand boundplus as the full argument list.
+        return func(table.unpack(boundplus))
+    end
+end
+
+-- Lua defines the special syntax table:attribute(args...) as shorthand for
+-- table.attribute(table, args...). But what if we want to refer to
+-- table.attribute as a free function? That is, what if we want:
+-- f = table:attribute
+-- ... some code ...
+-- f(args) => calls table.attribute(table, args)
+-- Sadly, the special syntax table:attribute doesn't return a callable that
+-- implicitly binds 'table' as the first argument. table:attribute is defined
+-- only for function definitions and function calls, not for expressions in
+-- general.
+-- bindclass(table, 'attribute') returns a function f such that f(args) calls
+-- table.attribute(table, args). Note that 'attribute' must be passed as a
+-- string. Otherwise we'd have to require bindclass(table, table.attribute).
+-- Yes this is equivalent to bind(table.attribute, table), but much simpler.
+function util.bindclass(t, attribute)
+    local method = assert(t[attribute], `bindclass() class has no method '{attribute}'`)
+    return function(...)
+        return method(t, ...)
+    end
+end
+
+-- A Lua object can be callable in either of a couple fairly verbose ways.
+function util.callable(maybefunc)
+    return (type(maybefunc) == 'function' or
+            (type(maybefunc) == 'table' and
+             getmetatable(maybefunc) and
+             type(getmetatable(maybefunc).__call) == 'function'))
+end
+
 -- Check errors from pcall(), xpcall() or coroutine.resume().
 -- pcall() et al. are defined to return either:
 -- * true followed by all return values of the function or coroutine
@@ -17,6 +67,116 @@ function util.callok(ok, err, ...)
     end
     -- return everything except the initial bool 'ok'
     return err, ...
+end
+
+-- Define a "class" table.
+-- Pass the class name, the class constructor (a function accepting whatever
+-- arguments are appropriate) and any additional class attributes, e.g.
+-- additional methods or __tostring(self).
+-- Example: MyClass = util.class{
+--     'MyClass',
+--     function(self, _name)
+--         -- In a constructor function, 'self' is the *class table*.
+--         -- The constructor must create and return a new instance table.
+--         return {name=_name}
+--     end,
+--     __tostring = function(self)
+--         return `{self.classname}({self.name})`
+--     end}
+-- The class name is stored as classname, the constructor as new() -- and also
+-- as the __call() metafunction.
+-- MyClass = util.class('MyClass', function(self)...end) works too (i.e.
+-- positional arguments), though in that case any additional class attributes
+-- must be assigned explicitly.
+-- With table-argument syntax util.class{}, the class name may be passed
+-- either as the first positional argument or as classname=. Similarly, the
+-- constructor may be passed either as the second positional argument or as
+-- new=.
+-- If base=BaseClass is passed, the new class will be a subclass of BaseClass.
+-- If you omit the constructor without base=, the new object will be empty.
+-- If you omit the constructor with base=, the new class inherits the base-
+-- class constructor.
+function util.class(classname, new)
+    local nm = classname
+    local ctor = new
+    local args = {}
+    if type(classname) == 'table' then
+        assert(new == nil, 'Call util.class{...} or util.class(classname, new), not both')
+        -- clone the passed table because we're going to modify it
+        args = table.clone(classname)
+        assert(not (args[1] and args.classname),
+               "Call util.class{'Name', ...} or util.class{classname='Name', ...}, not both")
+        nm = args[1] or args.classname
+        args[1] = nil
+        assert(not (args[2] and args.new),
+               'Call util.class{name, ctor, ...} or util.class{name, new=ctor, ...}, not both')
+        ctor = args[2] or args.new
+        args[2] = nil
+        assert((not args.base) or (type(args.base) == 'table'),
+            'util.class{base=} must be a class table')
+    end
+    assert(type(nm) == 'string' and #nm ~= 0, 'util.class(classname) must be non-empty string')
+    if ctor then
+        assert(util.callable(ctor), 'util.class() constructor must be callable')
+        ctor = wrapnew(ctor)
+    else
+        if args.base and args.base.new then
+            -- Omitting the constructor inherits the base-class constructor.
+            ctor = args.base.new
+        else
+            -- If there's no base-class constructor, just return empty table.
+            ctor = wrapnew(function(self) return {} end)
+        end
+    end
+    -- Use 'args' as our new class table, to capture any additional class
+    -- attributes we were passed.
+    args.classname = nm
+    args.new = ctor
+    -- Are we deriving this new class from an existing base class?
+    if args.base then
+        -- The metatable __index assignment below takes care of inheriting
+        -- most class attributes. But remembering that the class table is the
+        -- metatable for each instance, our base class may have __metafunctions
+        -- in the class table. We expect a subclass object to share the base-
+        -- class metafunctions; but Lua only looks up a __metafunction name in
+        -- the immediate metatable. Copy those to the new subclass, avoiding
+        -- any explicitly set by our caller.
+        -- This must happen *before* we set args's metatable's __index,
+        -- otherwise we think args already has every attribute set in
+        -- args.base!
+        for attribute, value in pairs(args.base) do
+            if (string.sub(attribute, 1, 2) == '__' and #attribute > 2
+                and not args[attribute]) then
+                args[attribute] = value
+            end
+        end
+    end
+    -- Set ctor() as the __call() metafunction too.
+    -- Also, if we're creating a subclass, set its metatable's __index to the
+    -- base class so any class attributes not found in this class will be
+    -- looked up in the base class.
+    setmetatable(args, {__call=ctor, __index=args.base})
+    -- Since 'args' is the metatable for each new instance, set its __index to
+    -- the class table: if a referenced attribute isn't found on the object
+    -- itself, seek it in the class.
+    args.__index = args
+    return args
+end
+
+-- internal to util.class(), see usage above
+local function wrapnew(ctor)
+    -- Per https://www.lua.org/pil/16.1.html, ensure that a class constructor
+    -- function returns an object with the class table set as the new object's
+    -- metatable.
+    return function(self, ...)
+        -- Both new() and __call() expect initial 'self' argument, which in a
+        -- MyClass:new(...) or MyClass(...) call is the class table. It's
+        -- important to bind that 'self' parameter as the metatable for this
+        -- class, not the current class table. If a subclass inherits its
+        -- base-class constructor, calling the constructor on the subclass
+        -- should bind the subclass as the metatable, not the base class.
+        return setmetatable(ctor(self, ...), self)
+    end
 end
 
 -- Allow MyClass(ctor args...) equivalent to MyClass:new(ctor args...)
@@ -76,6 +236,35 @@ function util.equal(t1, t2)
     return util.empty(temp)
 end
 
+-- Return an {x, y, z} table from a vector. Vectors don't implicitly convert.
+function util.fromvector(v)
+    if type(v) == 'table' then
+        -- already a table
+        return v
+    elseif type(v) == 'vector' then
+        -- Empirically, the only way we've found so far to extract the values of a
+        -- vector is to name them individually.
+        return {v.x, v.y, v.z}
+    else
+        error(`fromvector({type(v)}) not supported`)
+    end
+end
+
+-- notfunc(function) returns a function that calls the specified function,
+-- then returns the boolean inverse of its return value. But since some
+-- modules distinguish nil versus non-nil callback returns, make our returned
+-- function return nil for any truthy value, otherwise true. For any caller
+-- that simply tests boolean truth, this is the same.
+function util.notfunc(func)
+    return function(...)
+        if func(...) then
+            return nil
+        else
+            return true
+        end
+    end
+end
+
 -- Find or create the metatable for a specified table (a new empty table if
 -- omitted), and to that metatable assign the specified keys.
 -- Setting multiple keys at once is more efficient than a function to set only
@@ -126,6 +315,19 @@ function util.submoduledir(module, path)
             return mod
         end
     }
+end
+
+-- Return a Luau vector from (x, y, z) coordinates or an {x, y, z} table.
+function util.tovector(x, y, z)
+    -- you can pass an {x, y, z} table too
+    if type(x) == 'table' then
+        return vector.create(table.unpack(x))
+    elseif type(x) == 'vector' then
+        -- already a vector
+        return x
+    else
+        return vector.create(x, y, z)
+    end
 end
 
 return util
