997eb91d1a89e1315236dede8404f60a77cab5d3
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2024-12-11 22:21:49 +0200

    #3265 Crash at getAllowEnvironmentOverride
    Check region pointer and make sure external callbacks get disconected

--- indra/newview/llfloaterregioninfo.h
+++ indra/newview/llfloaterregioninfo.h
@@ -320,7 +320,7 @@ public:
     bool onMessageCommit(const LLSD& notification, const LLSD& response);
 
     LLPanelEstateInfo();
-    ~LLPanelEstateInfo() {}
+    ~LLPanelEstateInfo();
 
     void updateControls(LLViewerRegion* region);
 
@@ -352,6 +352,8 @@ protected:
     bool checkSunHourSlider(LLUICtrl* child_ctrl);
 
     U32 mEstateID;
+    boost::signals2::connection mEstateInfoCommitConnection;
+    boost::signals2::connection mEstateInfoUpdateConnection;
 };
 
 /////////////////////////////////////////////////////////////////////////////
