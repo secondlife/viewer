fa837baf0cccd7ddbe6788d539514e97089ff071
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2025-01-09 21:26:01 +0200

    #3374 LLFolderViewFolder::updateHasFavorites

--- indra/newview/llinventorypanel.cpp
+++ indra/newview/llinventorypanel.cpp
@@ -668,7 +668,7 @@ void LLInventoryPanel::itemChanged(const LLUUID& item_id, U32 mask, const LLInve
 
             if (get_is_favorite(model_item))
             {
-                LLFolderViewFolder* new_parent = (LLFolderViewFolder*)getItemByID(model_item->getParentUUID());
+                LLFolderViewFolder* new_parent = getFolderByID(model_item->getParentUUID());
                 if (new_parent)
                 {
                     new_parent->updateHasFavorites(true);
@@ -728,8 +728,14 @@ void LLInventoryPanel::itemChanged(const LLUUID& item_id, U32 mask, const LLInve
 
                     if (view_item->isFavorite())
                     {
-                        old_parent->updateHasFavorites(false); // favorite was removed
-                        new_parent->updateHasFavorites(true); // favorite was added
+                        if (old_parent)
+                        {
+                            old_parent->updateHasFavorites(false); // favorite was removed
+                        }
+                        if (new_parent)
+                        {
+                            new_parent->updateHasFavorites(true); // favorite was added
+                        }
                     }
                 }
             }
