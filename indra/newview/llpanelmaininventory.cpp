2a3d69afb80f59121e1c84e51e4ab7c94d5392c5
Maxim Nikolenko <maximnproductengine@lindenlab.com>
2025-01-10 19:45:22 +0200

    #3377 Select 'All items' tab when creating a folder via '+' button

--- indra/newview/llpanelmaininventory.cpp
+++ indra/newview/llpanelmaininventory.cpp
@@ -592,7 +592,8 @@ void LLPanelMainInventory::doCreate(const LLSD& userdata)
     }
     else
     {
-        menu_create_inventory_item(getPanel(), NULL, userdata);
+        selectAllItemsPanel();
+        menu_create_inventory_item(mAllItemsPanel, NULL, userdata);
     }
 }
 
