bbd89662fc5fc957149d71896da3367442d1bc19
Maxim Nikolenko <maximnproductengine@lindenlab.com>
2025-01-13 14:15:46 +0200

    #3394 Fix colour change not applied to favourites in Outfit Gallery

--- indra/newview/lloutfitgallery.cpp
+++ indra/newview/lloutfitgallery.cpp
@@ -1067,7 +1067,7 @@ void LLOutfitGalleryItem::setOutfitFavorite(bool is_favorite)
     mFavorite = is_favorite;
 
     static LLCachedControl<bool> use_color(gSavedSettings, "InventoryFavoritesColorText");
-    mOutfitNameText->setReadOnlyColor((mFavorite && use_color()) ? sDefaultFavoriteColor.get() : sDefaultTextColor.get());
+    mOutfitNameText->setReadOnlyColor((mFavorite && use_color()) ? sDefaultFavoriteColor : sDefaultTextColor);
 }
 
 void LLOutfitGalleryItem::setOutfitWorn(bool value)
@@ -1082,7 +1082,7 @@ void LLOutfitGalleryItem::setOutfitWorn(bool value)
     mOutfitNameText->setText(mOutfitName); // refresh LLTextViewModel to pick up font changes
 
     static LLCachedControl<bool> use_color(gSavedSettings, "InventoryFavoritesColorText");
-    mOutfitNameText->setReadOnlyColor((mFavorite && use_color()) ? sDefaultFavoriteColor.get() : sDefaultTextColor.get());
+    mOutfitNameText->setReadOnlyColor((mFavorite && use_color()) ? sDefaultFavoriteColor : sDefaultTextColor);
 }
 
 void LLOutfitGalleryItem::setSelected(bool value)
