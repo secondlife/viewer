6dc819e22aaf35054764a57dc51bb0ed629c6703
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2025-01-15 18:28:09 +0200

    #3364 Fix update rate being stuck high due to bias
    if bias stays unchanged at 4.f, there is no reason to keep refreshing
    at high rate.

--- indra/newview/llviewertexture.h
+++ indra/newview/llviewertexture.h
@@ -220,6 +220,7 @@ public:
     static S32 sAuxCount;
     static LLFrameTimer sEvaluationTimer;
     static F32 sDesiredDiscardBias;
+    static U32 sBiasTexturesUpdated;
     static S32 sMaxSculptRez ;
     static U32 sMinLargeImageSize ;
     static U32 sMaxSmallImageSize ;
