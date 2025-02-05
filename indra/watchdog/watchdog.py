e0cf6b8207fc466b11d83da484877f912f91e41b
Nat Goodspeed <nat@lindenlab.com>
2025-02-05 11:18:51 -0500

    Assemble crash report for sending. Check previously unsent reports.
    Add to the viewer's logs directory a crashes.json file containing a JSON array
    of previous crash reports. If that file exists when the watchdog starts up,
    try to send its contents right away.
    
    Stop logging successful viewer termination.
    
    On detecting viewer crash, try to read static_debug_info.log as the basis of
    the new crash report blob. If we can find SecondLife.log, read its creation
    time, which we set as "Date", the start of the viewer run. Subtract from current
    time to get "Duration".
    
    Adapt logic from viewer-manager aka SLVersionChecker to get a hash of some
    sort for "MachineID", to group crash reports from the same computer.
    
    If we had trouble with any of the above, also record those "Errors".
    
    The new crash blob consists of an array of previous crashes plus the current
    report. Update crashes.json before attempting to send. If the send succeeded,
    delete crashes.json again.
    
    So far the "send" operation merely logs the JSON array of crashes.

--- indra/watchdog/watchdog.py
+++ indra/watchdog/watchdog.py
@@ -11,14 +11,19 @@ $/LicenseInfo$
 """
 
 import __main__
-from contextlib import suppress
+from datetime import datetime, timezone
+import hashlib
 import json
+import llsd
 import logging
 import os
 from pathlib import Path
 import platform
+import re
+import subprocess
 import sys
 from tkinter.messagebox import showerror
+from uuid import UUID
 
 mydir = Path(__file__).parent
 myname = Path(__main__.__file__).stem
@@ -26,21 +31,48 @@ myname = Path(__main__.__file__).stem
 class Error(Exception):
     pass
 
+class JSONLLSDEncoder(json.JSONEncoder):
+    """
+    The Python `llsd` module returns most exotic LLSD data types as Python
+    strings. But it does return:
+    * LLSD dates as Python datetime.datetime objects
+    * LLSD UUIDs as Python uuid.UUID objects
+    which the json module, by default, does not handle. Extend the encoder to
+    serialize these as string instead of crumping.
+    """
+    def default(self, obj):
+        if isinstance(obj, (datetime, UUID)):
+            return str(obj)
+        return super().default(obj)
+
 def main(logdir):
+    errors = []
+
 ##  from argparse import ArgumentParser
 ##  parser = ArgumentParser(description="%(prog)s")
 ##  #parser.add_argument()
 ##  args = parser.parse_args(raw_args)
 
-    with suppress(FileExistsError):
-        os.makedirs(logdir)
+    logdir = Path(logdir)
+    logdir.mkdir(parents=True, exist_ok=True)
 
-    # Set up logging
-    logging.basicConfig(filename=Path(logdir) / (myname + '.log'),
-                        format='{asctime} {message}', style='{',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        level=logging.INFO)
-    logging.info(72*'=')
+    # When we first wake up, check if there's a backlog of pending crash
+    # reports we couldn't previously send.
+    crashes_json = logdir / 'crashes.json'
+    try:
+        inf = crashes_json.open()
+    except FileNotFoundError:
+        crashes = []
+    else:
+        with inf:
+            crashes = json.load(inf)
+
+        if not send_report('backlog', crashes):
+            errors.append(f"Can't send backlog of {len(crashes)} crashes")
+        else:
+            # We were successful in clearing out the backlog, so start fresh.
+            crashes = []
+            crashes_json.unlink()
 
     # Our job is to notice when the viewer terminates. We spend most of our
     # lifetime hanging on a read of stdin. When the viewer terminates, in any
@@ -48,10 +80,154 @@ def main(logdir):
     # 'OK' first, it's intentional, a normal termination. If not -- well,
     # something went wrong.
     OK = sys.stdin.buffer.read(2)
-    if OK != b'OK':
-        raise Error('Viewer crashed!')
+    if OK == b'OK':
+        # Viewer shut down successfully.
+        return
 
-    logging.info("viewer terminated")
+    # All of what follows is when we believe the viewer crashed.
+    # Get timestamp in case our setup takes any real time.
+    viewerdown = datetime.now(timezone.utc)
+
+    # Set up logging
+    logging.basicConfig(filename=logdir / (myname + '.log'),
+                        format='{asctime} {message}', style='{',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.DEBUG)
+    logging.info(72*'=')
+    logging.error('Viewer crashed!')
+
+    info_log = logdir / 'static_debug_info.log'
+    try:
+        inf = open(info_log, 'rb')
+    except FileNotFoundError:
+        errors.append(f"Can't open {info_log}")
+        logging.error(errors[-1])
+        crash = {}
+    else:
+        # Start with the viewer's static_debug_info.log as the bulk of this
+        # new crash report.
+        with inf:
+            crash = llsd.parse(inf)
+
+    crashes.append(crash)
+
+    # Supplement by recording the timestamp and the duration of the crashed
+    # session. We use the timestamp of SecondLife.log to determine both.
+    SecondLife_log = logdir / 'SecondLife.log'
+    try:
+        stat = SecondLife_log.stat()
+    except FileNotFoundError:
+        # If the viewer didn't even get as far as writing SecondLife.log, we
+        # can't report its timestamp. We may be fooled if a prior viewer run
+        # does write SecondLife.log, then the one that just terminated neither
+        # renames SecondLife.log to SecondLife.old nor writes a new
+        # SecondLife.log. We'll pick up on the previous run's log file.
+        errors.append(f"Can't stat {SecondLife_log}")
+        logging.error(errors[-1])
+    else:
+        # st_mtime gives us the most recent time the file was written. We
+        # expect that SecondLife.log is continually flushed, therefore
+        # continually written. What we want is the time this most recent
+        # SecondLife.log file was *created*, to get the start time of the
+        # session that just crashed.
+        try:
+            # "This attribute is not always available, and may raise AttributeError."
+            # https://docs.python.org/3/library/os.html#os.stat_result
+            creation = stat.st_birthtime
+        except AttributeError:
+            creation = stat.st_ctime
+        logstart = datetime.fromtimestamp(creation, timezone.utc)
+        duration = viewerdown - logstart
+        minutes, seconds = divmod(int(duration.total_seconds()), 60)
+        hours,   minutes = divmod(minutes, 60)
+        crash['Date'] = logstart.isoformat(timespec='seconds')
+        crash['Duration'] = f'{hours:02}:{minutes:02}:{seconds:02}'
+
+    try:
+        machine_id = make_VVM_UUID_hash()
+    except Error as err:
+        err = str(err)
+        logging.error(err)
+        errors.append(err)
+    else:
+        crash['MachineID'] = machine_id
+
+    if errors:
+        crash['Errors'] = errors
+
+    # Capture pending crashes in case we need to try again next time. Write
+    # the file first, remove it if successful, to guard against crashing
+    # during the attempt to send.
+    with crashes_json.open('w') as outf:
+        json.dump(crashes, outf, indent=4, ensure_ascii=False, cls=JSONLLSDEncoder)
+    if send_report('new', crashes):
+        crashes_json.unlink()
+
+def send_report(desc, report):
+    # For diagnostic purposes, dump the report to our log file.
+    logging.debug(f'Sending {desc}:')
+    streport = json.dumps(report, indent=4, ensure_ascii=False, cls=JSONLLSDEncoder)
+    for line in streport.splitlines():
+        logging.debug(line)
+    # pretend we successfully sent
+    return True
+
+## Adapted from viewer-manager:
+## https://github.com/secondlife/viewer-manager/blob/master/src/update_manager.py#L263-L313
+## There's no compelling reason to pass the same MachineID as the machine ID
+## passed to the Viewer Version Manager, but we already have this code.
+def make_VVM_UUID_hash():
+    #NOTE: There is no python library support for a persistent machine
+    #      specific UUID (MUUID), AND all three platforms do this a different
+    #      way, so exec'ing out is really the best we can do
+    #for env without stdin, such as pythonw and pyinstaller, provide a legit empty handle, not the broken
+    #thing we get from the env.
+    if (platform.system() == 'Linux'):
+        muuid = get_output('/usr/bin/hostid')
+        logging.debug("result of subprocess call to get linux MUUID: %r" % muuid)
+
+    elif (platform.system() == 'Darwin'):
+        #this is absurdly baroque
+        #/usr/sbin/system_profiler SPHardwareDataType | fgrep 'Serial' | awk '{print $NF}'
+        #also note that this causes spurious messages about X86PlatformPlugin in the log from stderr
+        # ignoring per https://tickets.puppetlabs.com/browse/FACT-724, stdout is correct, stderr is noise
+        profiler_cmd=[]
+        muuid = get_output("/usr/sbin/system_profiler", "SPHardwareDataType")
+        #findall[0] does the grep for the value we are looking for: "Serial Number (system): XXXXXXXX"
+        #split(:)[1] gets us the XXXXXXX part
+        #lstrip shaves off the leading space that was after the colon
+        muuid = re.split(b":", re.findall(rb'Serial Number \(system\): \S*', muuid)[0])[1].lstrip()
+        logging.debug("result of subprocess call to get mac MUUID: %r" % muuid)
+
+    elif (platform.system() == 'Windows'):
+        try:
+            # pshell csproduct get UUID | grep -v UUID
+            muuid = pshell('-Command', r'"CimCmdlets\Get-CimInstance -ClassName Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID"')
+        except PShellError as err:
+            logging.warning(err)
+            muuid = None
+        else:
+            #outputs row:
+            #XXXXXXX-XXXX...
+            # but splitlines() produces a whole lot of empty strings.
+            muuid = [line for line in muuid.splitlines() if line][-1].rstrip()
+            logging.debug("result of subprocess call to get win MUUID: %r" % muuid)
+            
+    else:
+        #fake it
+        logging.info(f"Unable to get {platform.system()} system unique id; constructing a dummy")
+        muuid = str(uuid.uuid1()).encode('utf8')
+
+    # hashlib requires a bytes object, not a str
+    return hashlib.md5(muuid).hexdigest()
+
+def get_output(*command):
+    try:
+        # For this use case we want bytes, not str.
+        return subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                              check=True, text=False).stdout.rstrip()
+    except subprocess.CalledProcessError as err:
+        raise Error(f'{err}:\n{err.stderr.decode("utf8")}')
 
 if __name__ == "__main__":
     try:
