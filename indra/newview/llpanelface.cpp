cb9a5f7114c3f73993e53ca7f41e8bf4db016ab2
Mnikolenko Productengine <mnikolenko@productengine.com>
2025-03-20 19:36:40 +0200

    #3779 reset texture repeats when unchecking the Hide Water option

--- indra/newview/llpanelface.cpp
+++ indra/newview/llpanelface.cpp
@@ -3077,6 +3077,8 @@ void LLPanelFace::onCommitHideWater()
     {
         // reset texture to default plywood
         LLSelectMgr::getInstance()->selectionSetImage(DEFAULT_OBJECT_TEXTURE);
+        // reset texture repeats, that might be altered by invisiprim script from wiki
+        LLSelectMgr::getInstance()->selectionTexScaleAutofit(2.f);
     }
 }
 
