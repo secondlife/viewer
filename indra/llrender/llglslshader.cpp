d2517d0bab328b1d5f17694ebea08735ad742f40
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2025-04-29 17:09:19 +0300

    #3997 Crash in a gltf asset enabled region

--- indra/llrender/llglslshader.cpp
+++ indra/llrender/llglslshader.cpp
@@ -1076,8 +1076,8 @@ void LLGLSLShader::bind()
 
 void LLGLSLShader::bind(U8 variant)
 {
-    llassert(mGLTFVariants.size() == LLGLSLShader::NUM_GLTF_VARIANTS);
-    llassert(variant < LLGLSLShader::NUM_GLTF_VARIANTS);
+    llassert_always(mGLTFVariants.size() == LLGLSLShader::NUM_GLTF_VARIANTS);
+    llassert_always(variant < LLGLSLShader::NUM_GLTF_VARIANTS);
     mGLTFVariants[variant].bind();
 }
 
@@ -1085,7 +1085,7 @@ void LLGLSLShader::bind(bool rigged)
 {
     if (rigged)
     {
-        llassert(mRiggedVariant);
+        llassert_always(mRiggedVariant);
         mRiggedVariant->bind();
     }
     else
