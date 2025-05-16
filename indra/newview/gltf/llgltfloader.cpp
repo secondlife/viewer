47d95f1eae379f16ed0ae320c9bb1a6002186e32
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2025-05-16 19:10:23 -0400

    White space.

--- indra/newview/gltf/llgltfloader.cpp
+++ indra/newview/gltf/llgltfloader.cpp
@@ -174,24 +174,19 @@ bool LLGLTFLoader::parseMeshes()
                     LLMatrix4 rotation;
                     rotation.initRotation(90.0f * DEG_TO_RAD, 0.0f, 0.0f);
                     mTransform *= rotation;
-
                     transformation = mTransform;
-
                     // adjust the transformation to compensate for mesh normalization
                     LLVector3 mesh_scale_vector;
                     LLVector3 mesh_translation_vector;
                     pModel->getNormalizedScaleTranslation(mesh_scale_vector, mesh_translation_vector);
-
                     LLMatrix4 mesh_translation;
                     mesh_translation.setTranslation(mesh_translation_vector);
                     mesh_translation *= transformation;
                     transformation = mesh_translation;
-
                     LLMatrix4 mesh_scale;
                     mesh_scale.initScale(mesh_scale_vector);
                     mesh_scale *= transformation;
                     transformation = mesh_scale;
-
                     if (transformation.determinant() < 0)
                     { // negative scales are not supported
                         LL_INFOS() << "Negative scale detected, unsupported post-normalization transform.  domInstance_geometry: "
@@ -390,21 +385,18 @@ bool LLGLTFLoader::populateModelFromMesh(LLModel* pModel, const LL::GLTF::Mesh& 
             for (U32 i = 0; i < vertices.size(); i++)
             {
                 LLVolumeFace::VertexData vert;
-
                 if (i == 0 || vertices[i].position.x > max.x)
                     max.x = vertices[i].position.x;
                 if (i == 0 || vertices[i].position.y > max.y)
                     max.y = vertices[i].position.y;
                 if (i == 0 || vertices[i].position.z > max.z)
                     max.z = vertices[i].position.z;
-
                 if (i == 0 || vertices[i].position.x < min.x)
                     min.x = vertices[i].position.x;
                 if (i == 0 || vertices[i].position.y < min.y)
                     min.y = vertices[i].position.y;
                 if (i == 0 || vertices[i].position.z < min.z)
                     min.z = vertices[i].position.z;
-
                 LLVector4a position = LLVector4a(vertices[i].position.x, vertices[i].position.y, vertices[i].position.z);
                 LLVector4a normal = LLVector4a(vertices[i].normal.x, vertices[i].normal.y, vertices[i].normal.z);
                 vert.setPosition(position);
@@ -422,8 +414,6 @@ bool LLGLTFLoader::populateModelFromMesh(LLModel* pModel, const LL::GLTF::Mesh& 
                 weight_list.push_back(LLModel::JointWeight(vertices[i].joints.w, vertices[i].weights.w));
 
                 std::sort(weight_list.begin(), weight_list.end(), LLModel::CompareWeightGreater());
-
-                
                 std::vector<LLModel::JointWeight> wght;
                 F32                               total = 0.f;
 
