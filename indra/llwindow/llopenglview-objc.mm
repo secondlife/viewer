77e2aa5704b37f93af82e2fb0364284c8cc6e68b
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2025-06-10 06:31:57 -0400

    Fix for VLAs now emitting warnings on Xcode 26, and AGL being removed from the SDK.
    Note: there was no warning or notice provided for AGL's removal.

--- indra/llwindow/llopenglview-objc.mm
+++ indra/llwindow/llopenglview-objc.mm
@@ -657,7 +657,7 @@ attributedStringInfo getSegments(NSAttributedString *str)
         };
         
         int string_length = [aString length];
-        unichar text[string_length];
+        unichar *text = new unichar[string_length];
         attributedStringInfo segments;
         // I used 'respondsToSelector:@selector(string)'
         // to judge aString is an attributed string or not.
@@ -685,6 +685,8 @@ attributedStringInfo getSegments(NSAttributedString *str)
             // we must clear the marked text when aString is null.
             [self unmarkText];
         }
+        
+        delete [] text;
     } else {
         if (mHasMarkedText)
         {
