210f559efe81864c7581677882c867dc14b7f854
Darl <me@darl.cat>
2025-07-25 11:00:01 -0500

    Media first click interact self check fix (#4406, #4426)

--- indra/newview/lltoolpie.cpp
+++ indra/newview/lltoolpie.cpp
@@ -1541,12 +1541,6 @@ bool LLToolPie::shouldAllowFirstMediaInteraction(const LLPickInfo& pick, bool mo
         return false;
     }
 
-    // Own objects
-    if((FirstClickPref & MEDIA_FIRST_CLICK_OWN) && object->permYouOwner())
-    {
-        LL_DEBUGS_ONCE() << "FirstClickPref & MEDIA_FIRST_CLICK_OWN" << LL_ENDL;
-        return true;
-    }
     // HUD attachments
     if((FirstClickPref & MEDIA_FIRST_CLICK_HUD) && object->isHUDAttachment())
     {
@@ -1567,6 +1561,13 @@ bool LLToolPie::shouldAllowFirstMediaInteraction(const LLPickInfo& pick, bool mo
     {
         LL_WARNS() << "Owner information was not reliably obtained" << LL_ENDL;
         return false;
+    }
+
+    // Own objects
+    if((FirstClickPref & MEDIA_FIRST_CLICK_OWN) && owner_id == gAgent.getID())
+    {
+        LL_DEBUGS_ONCE() << "FirstClickPref & MEDIA_FIRST_CLICK_OWN" << LL_ENDL;
+        return true;
     }
 
     // Check if the object is owned by a friend of the agent
