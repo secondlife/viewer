971f131ba74f4024c6febf938294920771450d2e
Erik Kundiman <erik@megapahit.org>
2025-08-09 01:45:26 +0800

    Discord without auth & "Hidden Region" instead of blank (#4496)
    * Rich Presence w/o requiring access to friends list
    
    Thank you Signal Linden for the pointer from https://discord.com/developers/docs/social-sdk/classdiscordpp_1_1Client.html#af0a85e30f2b3d8a0b502fd23744ee58e
    "Note: On Desktop, rich presence can be set before calling Client::Connect, but it will be cleared if the Client connects. When Client is not connected, this sets the rich presence in the current user's Discord client when available."
    This lead me to setting the Application ID here https://discord.com/developers/docs/social-sdk/classdiscordpp_1_1Client.html#ad452335c06b28be0406dab824acccc49 in place of setting it on
    https://discord.com/developers/docs/social-sdk/classdiscordpp_1_1AuthorizationArgs.html which would lead Authorize, GetToken, UpdateToken, Connect, and so on.
    This means we don't even need the SecAPI saveCredential, loadCredential and deleteCredential parts now.
    
    * Discord integration is enabled by default per spec
    
    now that we don't need the user to authorise Discord SDK to have
    any access to the user's friends list, etc. (which are Discord
    Relationships related, and not needed just for Rich Presence).
    
    * "Hidden Region" if Discord location sharing is off
    
    instead of blank. The coords are hidden too, but the Party numbers
    are still shown, for consistency with TPVs' implementations.
    
    * Remove toggleDiscordIntegration declaration
    
    The definition had already been removed, I had forgotten to remove
    this one.

--- indra/newview/llappviewer.h
+++ indra/newview/llappviewer.h
@@ -253,7 +253,6 @@ public:
 
 #ifdef LL_DISCORD
     static void initDiscordSocial();
-    static void toggleDiscordIntegration(const LLSD& value);
     static void updateDiscordActivity();
     static void updateDiscordPartyCurrentSize(int32_t size);
     static void updateDiscordPartyMaxSize(int32_t size);
