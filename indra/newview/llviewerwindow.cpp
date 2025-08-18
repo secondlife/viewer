9fd5ecceb397ca77b0b4c1790eca71100d0a6d55
Callum Prentice <callum@lindenlab.com>
2025-08-18 15:19:22 -0700

    First phase of some work to replace certain UI web based floaters with a much more simple floater (no more browserish web-content-floater) and then pre-load content as login is progressing.  This means that after login, the floater can be opened much more rapidly than now.  This first commit does this process for the Search floater

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -2309,6 +2309,11 @@ void LLViewerWindow::initWorldUI()
             url = LLWeb::expandURLSubstitutions(url, LLSD());
             avatar_welcome_pack->navigateTo(url, HTTP_CONTENT_TEXT_HTML);
         }
+        LLMediaCtrl* search = LLFloaterReg::getInstance("search")->findChild<LLMediaCtrl>("search_contents");
+        if (search)
+        {
+            search->setErrorPageURL(gSavedSettings.getString("GenericErrorPageURL"));
+        }
     }
 }
 
