e629bf05d6212613083c8ccb2858085926f38902
Rye <rye@alchemyviewer.org>
2025-08-27 22:36:55 -0400

    Changes to support dullahan 1.21 undo/redo/delete/select all edit handlers

--- indra/llplugin/llpluginclassmedia.h
+++ indra/llplugin/llpluginclassmedia.h
@@ -201,6 +201,12 @@ public:
 
     LLPluginClassMediaOwner::EMediaStatus getStatus() const { return mStatus; }
 
+    void    undo();
+    bool    canUndo() const { return mCanUndo; };
+
+    void    redo();
+    bool    canRedo() const { return mCanRedo; };
+
     void    cut();
     bool    canCut() const { return mCanCut; };
 
@@ -209,6 +215,14 @@ public:
 
     void    paste();
     bool    canPaste() const { return mCanPaste; };
+
+    void    doDelete();
+    bool    canDoDelete() const { return mCanDoDelete; };
+
+    void    selectAll();
+    bool    canSelectAll() const { return mCanSelectAll; };
+
+    void    showPageSource();
 
     // These can be called before init(), and they will be queued and sent before the media init message.
     void    setUserDataPath(const std::string &user_data_path_cache, const std::string &username, const std::string &user_data_path_cef_log);
@@ -419,9 +433,13 @@ protected:
 
     F64             mSleepTime;
 
+    bool            mCanUndo;
+    bool            mCanRedo;
     bool            mCanCut;
     bool            mCanCopy;
     bool            mCanPaste;
+    bool            mCanDoDelete;
+    bool            mCanSelectAll;
 
     std::string     mMediaName;
     std::string     mMediaDescription;
