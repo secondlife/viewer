ac2cbdcc02efb8af88d91f02ec1b58022e4bf439
Andrey Kleshchev <117672381+akleshchev@users.noreply.github.com>
2025-09-11 22:32:03 +0300

    #4660 Unable to upload GLTF+bin with spaces in the name

--- indra/newview/gltf/accessor.cpp
+++ indra/newview/gltf/accessor.cpp
@@ -158,6 +158,11 @@ bool Buffer::prep(Asset& asset)
     {
         std::string dir = gDirUtilp->getDirName(asset.mFilename);
         std::string bin_file = dir + gDirUtilp->getDirDelimiter() + mUri;
+        if (!gDirUtilp->fileExists(bin_file))
+        {
+            // Characters might be escaped in the URI
+            bin_file = dir + gDirUtilp->getDirDelimiter() + LLURI::unescape(mUri);
+        }
 
         llifstream file(bin_file.c_str(), std::ios::binary);
         if (!file.is_open())
