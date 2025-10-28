ab91d44b1d73a3109d006b90f0ea3d501d31e12c
Rye <rye@alchemyviewer.org>
2025-10-28 05:24:24 -0400

    Reduce extra packages installed during autobuild configure
    Signed-off-by: Rye <rye@alchemyviewer.org>

--- indra/cmake/SDL3.cmake
+++ indra/cmake/SDL3.cmake
@@ -1,19 +1,26 @@
 # -*- cmake -*-
-cmake_minimum_required( VERSION 3.13 FATAL_ERROR )
-
-include(Linking)
-include( Prebuilt )
 include_guard()
+include(Linking)
+include(Prebuilt)
 
-add_library( ll::SDL3 INTERFACE IMPORTED )
+add_library(ll::SDL3 INTERFACE IMPORTED)
 
-use_system_binary( SDL3 )
-use_prebuilt_binary( SDL3 )
+if (LINUX)
+  set(USE_SDL_WINDOW ON CACHE BOOL "Build with SDL window backend")
+else()
+  set(USE_SDL_WINDOW OFF CACHE BOOL "Build with SDL window backend")
+endif (LINUX)
 
-find_library( SDL3_LIBRARY
-    NAMES SDL3 SDL3.lib libSDL3.so libSDL3.dylib
-    PATHS "${LIBS_PREBUILT_DIR}/lib/release" REQUIRED)
+if(USE_SDL_WINDOW)
+    use_system_binary(SDL3)
+    use_prebuilt_binary(SDL3)
 
-target_link_libraries( ll::SDL3 INTERFACE ${SDL3_LIBRARY})
-target_include_directories( ll::SDL3 SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include/" )
+    find_library( SDL3_LIBRARY
+        NAMES SDL3 SDL3.lib libSDL3.so libSDL3.dylib
+        PATHS "${LIBS_PREBUILT_DIR}/lib/release" REQUIRED)
+
+    target_link_libraries(ll::SDL3 INTERFACE ${SDL3_LIBRARY})
+    target_include_directories(ll::SDL3 SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include/")
+
+endif(USE_SDL_WINDOW)
 
