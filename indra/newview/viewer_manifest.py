0c796b0c429058b334ee8968c4f0019445c09d55
Rye <rye@alchemyviewer.org>
2025-11-01 07:30:04 -0400

    Remove dead linux crash logger
    Signed-off-by: Rye <rye@alchemyviewer.org>

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -1133,7 +1133,6 @@ class LinuxManifest(ViewerManifest):
 
         with self.prefix(dst="bin"):
             self.path("secondlife-bin","do-not-directly-run-secondlife-bin")
-            #self.path("../linux_crash_logger/linux-crash-logger","linux-crash-logger.bin")
             self.path2basename("../llplugin/slplugin", "SLPlugin")
             #this copies over the python wrapper script, associated utilities and required libraries, see SL-321, SL-322 and SL-323
             #with self.prefix(src="../viewer_components/manager", dst=""):
