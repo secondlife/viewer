4132a03bde40f09976501f01be788f9e4747f907
Andrey Kleshchev <117672381+akleshchev@users.noreply.github.com>
2026-01-14 06:51:52 +0200

    #5271 Update zlib-ng and libraries that depend on it

--- indra/cmake/Boost.cmake
+++ indra/cmake/Boost.cmake
@@ -20,48 +20,42 @@ if (WINDOWS)
 
     find_library(BOOST_CONTEXT_LIBRARY
         NAMES
+        libboost_context
         libboost_context-mt
         libboost_context-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_FIBER_LIBRARY
         NAMES
+        libboost_fiber
         libboost_fiber-mt
         libboost_fiber-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_FILESYSTEM_LIBRARY
         NAMES
+        libboost_filesystem
         libboost_filesystem-mt
         libboost_filesystem-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_PROGRAMOPTIONS_LIBRARY
         NAMES
+        libboost_program_options
         libboost_program_options-mt
         libboost_program_options-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
-    find_library(BOOST_REGEX_LIBRARY
-        NAMES
-        libboost_regex-mt
-        libboost_regex-mt${addrsfx}
-        PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
-
-    find_library(BOOST_SYSTEM_LIBRARY
-        NAMES
-        libboost_system-mt
-        libboost_system-mt${addrsfx}
-        PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
-
     find_library(BOOST_THREAD_LIBRARY
         NAMES
+        libboost_thread
         libboost_thread-mt
         libboost_thread-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_URL_LIBRARY
         NAMES
+        libboost_url
         libboost_url-mt
         libboost_url-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
@@ -70,48 +64,42 @@ else (WINDOWS)
 
     find_library(BOOST_CONTEXT_LIBRARY
        NAMES
+       boost_context
        boost_context-mt
        boost_context-mt${addrsfx}
        PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_FIBER_LIBRARY
         NAMES
+        boost_fiber
         boost_fiber-mt
         boost_fiber-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_FILESYSTEM_LIBRARY
         NAMES
+        boost_filesystem
         boost_filesystem-mt
         boost_filesystem-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_PROGRAMOPTIONS_LIBRARY
         NAMES
+        boost_program_options
         boost_program_options-mt
         boost_program_options-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
-    find_library(BOOST_REGEX_LIBRARY
-        NAMES
-        boost_regex-mt
-        boost_regex-mt${addrsfx}
-        PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
-
-    find_library(BOOST_SYSTEM_LIBRARY
-        NAMES
-        boost_system-mt
-        boost_system-mt${addrsfx}
-        PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
-
     find_library(BOOST_THREAD_LIBRARY
         NAMES
+        boost_thread
         boost_thread-mt
         boost_thread-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
 
     find_library(BOOST_URL_LIBRARY
         NAMES
+        boost_url
         boost_url-mt
         boost_url-mt${addrsfx}
         PATHS "${ARCH_PREBUILT_DIRS_RELEASE}" REQUIRED NO_DEFAULT_PATH)
@@ -123,8 +111,6 @@ target_link_libraries(ll::boost INTERFACE
     ${BOOST_CONTEXT_LIBRARY}
     ${BOOST_FILESYSTEM_LIBRARY}
     ${BOOST_PROGRAMOPTIONS_LIBRARY}
-    ${BOOST_REGEX_LIBRARY}
-    ${BOOST_SYSTEM_LIBRARY}
     ${BOOST_THREAD_LIBRARY}
     ${BOOST_URL_LIBRARY})
 
