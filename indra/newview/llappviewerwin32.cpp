9ae80a8392c8dae090da57b2c90343f729326225
Rye <rye@alchemyviewer.org>
2026-01-23 03:44:27 -0500

    Remove dead VisualLeakDetector support
    Signed-off-by: Rye <rye@alchemyviewer.org>

--- indra/newview/llappviewerwin32.cpp
+++ indra/newview/llappviewerwin32.cpp
@@ -26,9 +26,6 @@
 
 #include "llviewerprecompiledheaders.h"
 
-#ifdef INCLUDE_VLD
-#include "vld.h"
-#endif
 #include "llwin32headers.h"
 
 #include "llwindowwin32.h" // *FIX: for setting gIconResource.
@@ -437,7 +434,7 @@ int APIENTRY WINMAIN(HINSTANCE hInstance,
 
     LLWindowWin32::setDPIAwareness();
 
-#if WINDOWS_CRT_MEM_CHECKS && !INCLUDE_VLD
+#if WINDOWS_CRT_MEM_CHECKS
     _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF ); // dump memory leaks on exit
 #elif 0
     // Experimental - enable the low fragmentation heap
