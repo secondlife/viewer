f55dbadda7ee6315750e1cb3bc3d59a280ce927e
LamLe-BountyHunter <lamle@bountyhunter.dev>
2026-05-12 15:20:43 +0000

    fix: Viewer copyright dates need to be updated. (#5775)

--- indra/llphysicsextensionsos/llconvexdecompositionvhacd.cpp
+++ indra/llphysicsextensionsos/llconvexdecompositionvhacd.cpp
@@ -5,7 +5,7 @@
 *
 * $LicenseInfo:firstyear=2025&license=viewerlgpl$
 * Second Life Viewer Source Code
-* Copyright (C) 2025, Linden Research, Inc.
+* Copyright (C) 2026, Linden Research, Inc.
 *
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
