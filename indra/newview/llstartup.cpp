964c91bbafc43897a12f6e224766ff9da390eda2
Mnikolenko Productengine <mnikolenko@productengine.com>
2026-05-18 17:17:59 +0300

    #5785 fix login and world UI overlap

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1354,6 +1354,10 @@ bool idle_startup()
     {
         set_startup_status(0.30f, LLTrans::getString("LoginInitializingWorld"), gAgent.mMOTD);
         do_startup_frame();
+
+        // close login UI before world UI is initialized, if it is still visible
+        LLPanelLogin::closePanel();
+
         // We should have an agent id by this point.
         llassert(!(gAgentID == LLUUID::null));
 
