fc3b34f0a6b403819db8ed53e7b451da7a3d9993
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2026-06-17 11:06:00 -0400

    Update llwindowmacosx_iokit.h

--- indra/llwindow/llwindowmacosx_iokit.h
+++ indra/llwindow/llwindowmacosx_iokit.h
@@ -28,8 +28,8 @@
 #include <IOKit/IOKitLib.h>
 
 // kIOMainPortDefault is the macOS 12+ rename of kIOMasterPortDefault.
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 120000
-static constexpr mach_port_t kLLIOMainPort = kIOMainPortDefault;
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 120000
+static const mach_port_t kLLIOMainPort = kIOMainPortDefault;
 #else
-static constexpr mach_port_t kLLIOMainPort = kIOMasterPortDefault;
+static const mach_port_t kLLIOMainPort = kIOMasterPortDefault;
 #endif
