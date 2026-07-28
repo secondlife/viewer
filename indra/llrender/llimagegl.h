ac91515c115d0405a2dcb5438d1703bd9f624123
Andrey Kleshchev <117672381+akleshchev@users.noreply.github.com>
2026-07-28 19:26:11 +0300

    #6055 Optimize LLImageGL::analyzeAlpha

--- indra/llrender/llimagegl.h
+++ indra/llrender/llimagegl.h
@@ -111,6 +111,7 @@ public:
 protected:
     virtual ~LLImageGL();
 
+    static bool analyzeAlphaData(const void* data_in, U32 w, U32 h, S8 alpha_offset, S8 alpha_stride);
     void analyzeAlpha(const void* data_in, U32 w, U32 h);
     void calcAlphaChannelOffsetAndStride();
 
@@ -242,6 +243,7 @@ private:
 
     bool mIsMask;
     bool mNeedsAlphaAndPickMask;
+    LLAtomicU32 mAlphaAnalysisSerial; // for request tracking.
     S8   mAlphaStride ;
     S8   mAlphaOffset ;
 
