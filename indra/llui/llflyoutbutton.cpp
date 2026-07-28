a80c33606cf5688b77172b5430c87bc1272a44d3
Ansariel <ansariel.hiller@phoenixviewer.com>
2026-07-28 19:10:48 +0200

    #5907 Fix the actual button width calculation for LLFlyoutButton

--- indra/llui/llflyoutbutton.cpp
+++ indra/llui/llflyoutbutton.cpp
@@ -31,7 +31,7 @@
 
 //static LLDefaultChildRegistry::Register<LLFlyoutButton> r2("flyout_button");
 
-const S32 FLYOUT_BUTTON_ARROW_WIDTH = 24;
+constexpr S32 FLYOUT_BUTTON_ARROW_WIDTH = 22;
 
 LLFlyoutButton::LLFlyoutButton(const Params& p)
 :   LLComboBox(p),
@@ -43,7 +43,7 @@ LLFlyoutButton::LLFlyoutButton(const Params& p)
     LLButton::Params bp(p.action_button);
     bp.name(p.label);
     bp.label(p.label);
-    bp.rect.left(0).bottom(0).width(getRect().getWidth() - FLYOUT_BUTTON_ARROW_WIDTH).height(getRect().getHeight());
+    bp.rect.left(0).bottom(0).width(getRect().getWidth() - FLYOUT_BUTTON_ARROW_WIDTH - 2 * BTN_DROP_SHADOW).height(getRect().getHeight());
     bp.click_callback.function(boost::bind(&LLFlyoutButton::onActionButtonClick, this, _2));
     bp.follows.flags(FOLLOWS_ALL);
     if (p.font.isProvided())
@@ -83,10 +83,10 @@ bool LLFlyoutButton::postBuild()
     bool result = LLComboBox::postBuild();
 
     LLRect rect = getLocalRect();
-    S32 arrow_width = mArrowImage ? mArrowImage->getWidth() : 0;
-    S32 shadow_size = BTN_DROP_SHADOW;
-    mButton->setRect(LLRect(getRect().getWidth() - llmax(8, arrow_width) - shadow_size,
+    S32 arrow_width = llmax(FLYOUT_BUTTON_ARROW_WIDTH, mArrowImage ? mArrowImage->getWidth() : 0);
+    mButton->setRect(LLRect(getRect().getWidth() - llmax(8, arrow_width) - 2 * BTN_DROP_SHADOW,
         rect.mTop, rect.mRight, rect.mBottom));
+    mButton->setFollows(FOLLOWS_BOTTOM | FOLLOWS_TOP | FOLLOWS_RIGHT);
 
     return result;
 }
