4884f59bc9b43d70060c5a0f640ac233b19b99ac
Frederick Martian <fredmartian@gmail.com>
2026-09-02 22:11:04 +0200

    Improve LLDir::getFilesInDir() to optionally also return symlinks in a directory if they point to entries outside the directory

--- indra/llfilesystem/lldir.cpp
+++ indra/llfilesystem/lldir.cpp
@@ -89,10 +89,9 @@ LLDir::~LLDir()
 {
 }
 
-std::vector<std::string> LLDir::getFilesInDir(const std::string &dirname)
+std::vector<std::string> LLDir::getFilesInDir(const std::filesystem::path &dir_path, bool includeExternalSymlinks) const
 {
     // Returns a vector of filenames in the directory.
-    fsyspath dir_path(dirname);
     std::vector<std::string> v;
     std::error_code ec;
     if (std::filesystem::is_directory(dir_path, ec))
@@ -106,10 +105,22 @@ std::vector<std::string> LLDir::getFilesInDir(const std::string &dirname)
             {
                 v.push_back(dir_itr->path().filename().string());
             }
+            else if (includeExternalSymlinks && std::filesystem::is_symlink(dir_itr->status()))
+            {
+                if (dir_path != std::filesystem::read_symlink(dir_itr->path(), ec).parent_path())
+                {
+                    v.push_back(dir_itr->path().filename().string());
+                }
+            }
         }
     }
     return v;
 }
+
+std::vector<std::string> LLDir::getFilesInDir(const std::string &dirname, bool includeExternalSymlinks) const
+{
+    return getFilesInDir(fsyspath(dirname), includeExternalSymlinks);
+};
 
 S32 LLDir::deleteFilesInDir(const std::string &dirname, const std::string &mask)
 {
--- indra/llfilesystem/lldir.h
+++ indra/llfilesystem/lldir.h
@@ -68,7 +68,11 @@ class LLDir
 
     virtual S32 deleteFilesInDir(const std::string &dirname, const std::string &mask);
     U32 deleteDirAndContents(const std::string& dir_name);
-    std::vector<std::string> getFilesInDir(const std::string &dirname);
+    // get a list of file names inside the directory
+    // includeExternalSymlinks also adds any simlinks to the list that do refer to files in other locations.
+    // symlinks to files located in the same directory are excluded as that file is already added to the list.
+    std::vector<std::string> getFilesInDir(const std::filesystem::path &dirname, bool includeExternalSymlinks = false) const;
+    std::vector<std::string> getFilesInDir(const std::string &dirname, bool includeExternalSymlinks = false) const;
 // pure virtual functions
     virtual std::string getCurPath() = 0;
     virtual bool fileExists(const std::string &filename) const;
