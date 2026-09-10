0f7c18e4ef748d34c611941b66fe22d646aafc4f
trish-sl <sl.trish.ace@gmail.com>
2026-09-10 12:31:59 -0400

    #3850 Preserve followcam through momentary avatar loss (#6237)

--- indra/newview/llagentcamera.h
+++ indra/newview/llagentcamera.h
@@ -168,6 +168,7 @@ private:
     //--------------------------------------------------------------------
 public:
     bool            isfollowCamLocked();
+    void            notifyFollowCamParamsCleared();
 private:
     LLFollowCam     mFollowCam;             // Ventrella
 
@@ -227,6 +228,7 @@ private:
     LLVector3d      mCameraFocusOffsetTarget;       // Target towards which we are lerping the camera's focus offset
     bool            mFocusOnAvatar;
     bool            mAllowChangeToFollow;
+    F64             mLastValidFollowCamParamsTime;
     LLVector3d      mFocusGlobal;
     LLVector3d      mFocusTargetGlobal;
     LLPointer<LLViewerObject> mFocusObject;
