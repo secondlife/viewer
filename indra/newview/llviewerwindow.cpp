b6c9f91e9ce9a79c68459b22d7b52ba5b42bee8c
Kyler Eastridge <felix.wolfz@gmail.com>
2026-09-10 13:25:32 -0400

    Fix Timecode crash

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -2874,6 +2874,12 @@ void LLViewerWindow::draw()
         LLView::sDirtyRect = getWindowRectScaled();
     }
 
+    // Draw all nested UI views.
+    // No translation needed, this view is glued to 0,0
+
+    gUIProgram.bind();
+    gGL.color4f(1, 1, 1, 1);
+
     // HACK for timecode debugging
     if (gSavedSettings.getBOOL("DisplayTimecode"))
     {
@@ -2890,12 +2896,6 @@ void LLViewerWindow::draw()
             LLColor4( 1.f, 1.f, 1.f, 1.f ),
             LLFontGL::LEFT, LLFontGL::TOP);
     }
-
-    // Draw all nested UI views.
-    // No translation needed, this view is glued to 0,0
-
-    gUIProgram.bind();
-    gGL.color4f(1, 1, 1, 1);
 
     gGL.pushMatrix();
     LLUI::pushMatrix();
