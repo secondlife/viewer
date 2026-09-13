893c748376c4ad0621eb682caaab805d7e5cd625
Kyler "Félix" Eastridge <felix.wolfz@gmail.com>
2026-09-13 12:08:23 -0400

    #6311 Fix LLWorld SpaceTime updating

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -5311,7 +5311,7 @@ void LLAppViewer::idle()
     {
         LL_PROFILE_ZONE_NAMED_CATEGORY_NETWORK("network"); //LL_RECORD_BLOCK_TIME(FTM_NETWORK);
         // Update spaceserver timeinfo
-        LLWorld::getInstance()->setSpaceTimeUSec(LLWorld::getInstance()->getSpaceTimeUSec() + LLUnits::Seconds::fromValue(dt_raw));
+        LLWorld::getInstance()->setSpaceTimeUSec(LLWorld::getInstance()->getSpaceTimeUSec() + (U64)(dt_raw * USEC_PER_SEC));
 
 
         //////////////////////////////////////
