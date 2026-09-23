818c337afa685221611a1beb67ca171437c6ec1d
Andrey Lihatskiy <alihatskiy@productengine.com>
2026-09-24 00:49:40 +0300

    Fix Luau license packaging and formatting

--- indra/cmake/BuildPackagesInfo.cmake
+++ indra/cmake/BuildPackagesInfo.cmake
@@ -15,7 +15,7 @@ add_custom_command(OUTPUT packages-info.txt
           ${CMAKE_SOURCE_DIR}/newview/luau.txt
           ${CMAKE_SOURCE_DIR}/../autobuild.xml
   COMMAND ${PYTHON_EXECUTABLE}
-          ${CMAKE_SOURCE_DIR}/cmake/run_build_test.py -DAUTOBUILD_ADDRSIZE=${ADDRESS_SIZE} -DAUTOBUILD=${AUTOBUILD_EXECUTABLE}
+          ${CMAKE_SOURCE_DIR}/cmake/run_build_test.py -DAUTOBUILD_ADDRSIZE=${ADDRESS_SIZE} -DAUTOBUILD=${AUTOBUILD_EXECUTABLE} -DPYTHONIOENCODING=utf-8
           ${PYTHON_EXECUTABLE}
           ${CMAKE_SOURCE_DIR}/../scripts/packages-formatter.py "${VIEWER_CHANNEL}" "${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}" "${AUTOBUILD_INSTALL_DIR}" > packages-info.txt
   )
